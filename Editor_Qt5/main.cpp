9b59eb7706707874afb78c9159dea9a3b918aea8
Wohlstand <admin@wohlnet.ru>
2014-04-19 03:48:01 +0400

    added logger function

--- Editor_Qt5/main.cpp
+++ Editor_Qt5/main.cpp
@@ -18,15 +18,43 @@
 
 #include <QtWidgets>
 #include <QFile>
+#include <QtDebug>
+#include <QTextStream>
 #include <QTranslator>
 #include <QLocale>
 #include <QFileDialog>
-
 #include "mainwindow.h"
+
+#include "logger.h"
+void WriteToLog(QtMsgType type, QString msg)
+    {
+        QString txt;
+        switch (type) {
+        case QtDebugMsg:
+        txt = QString("Debug: %1").arg(msg);
+        break;
+        case QtWarningMsg:
+        txt = QString("Warning: %1").arg(msg);
+        break;
+        case QtCriticalMsg:
+        txt = QString("Critical: %1").arg(msg);
+        break;
+        case QtFatalMsg:
+        txt = QString("Fatal: %1").arg(msg);
+        abort();
+    }
+    QFile outFile("log.txt");
+    outFile.open(QIODevice::WriteOnly | QIODevice::Append);
+    QTextStream ts(&outFile);
+    ts << txt << endl;
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    WriteToLog(QtDebugMsg, "--> Application started <--");
+
     MainWindow w;
     w.show();
     return a.exec();
