b148c8d5689e729f5dbb8f9dde24961312a32efb
Wohlstand <admin@wohlnet.ru>
2014-05-11 01:19:36 +0400

    Moved from mainwindow.cpp

---
+++ Editor/main_window/edit_control.cpp
@@ -0,0 +1,129 @@
+/*
+ * Platformer Game Engine by Wohlstand, a free platform for game making
+ * Copyright (c) 2014 Vitaly Novichkov <admin@wohlnet.ru>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */
+
+
+#include "../ui_mainwindow.h"
+#include "../mainwindow.h"
+
+
+/////////////////Switch edit mode////////////////////////////////////////////
+
+void MainWindow::resetEditmodeButtons()
+{
+    ui->actionSelect->setChecked(0);
+    ui->actionSelectOnly->setChecked(0);
+    ui->actionEriser->setChecked(0);
+    ui->actionHandScroll->setChecked(0);
+
+    ui->actionSetFirstPlayer->setChecked(0);
+    ui->actionSetSecondPlayer->setChecked(0);
+    ui->actionDrawWater->setChecked(0);
+    ui->actionDrawSand->setChecked(0);
+}
+
+
+void MainWindow::on_actionSelect_triggered()
+{
+    resetEditmodeButtons();
+    ui->actionSelect->setChecked(1);
+
+    if ((activeChildWindow()==1) && (ui->actionSelect->isChecked()))
+    {
+       activeLvlEditWin()->changeCursor(0);
+       activeLvlEditWin()->scene->EditingMode = 0;
+       activeLvlEditWin()->scene->disableMoveItems=false;
+       activeLvlEditWin()->scene->EraserEnabled = false;
+    }
+}
+
+
+
+void MainWindow::on_actionSelectOnly_triggered()
+{
+    resetEditmodeButtons();
+    ui->actionSelectOnly->setChecked(1);
+
+    if ((activeChildWindow()==1) && (ui->actionSelectOnly->isChecked()))
+    {
+       activeLvlEditWin()->changeCursor(0);
+       activeLvlEditWin()->scene->EditingMode = 0;
+       activeLvlEditWin()->scene->disableMoveItems=true;
+       activeLvlEditWin()->scene->EraserEnabled = false;
+    }
+}
+
+
+void MainWindow::on_actionEriser_triggered()
+{
+    resetEditmodeButtons();
+    ui->actionEriser->setChecked(1);
+
+    if ((activeChildWindow()==1) && (ui->actionEriser->isChecked()))
+    {
+       activeLvlEditWin()->changeCursor(1);
+       activeLvlEditWin()->scene->EditingMode = 1;
+       activeLvlEditWin()->scene->disableMoveItems=false;
+       activeLvlEditWin()->scene->EraserEnabled = false;
+    }
+}
+
+
+void MainWindow::on_actionHandScroll_triggered()
+{
+    resetEditmodeButtons();
+    ui->actionHandScroll->setChecked(1);
+
+    if ((activeChildWindow()==1) && (ui->actionHandScroll->isChecked()))
+    {
+       activeLvlEditWin()->scene->clearSelection();
+       activeLvlEditWin()->changeCursor(-1);
+       activeLvlEditWin()->scene->EditingMode = 0;
+       activeLvlEditWin()->scene->disableMoveItems=false;
+       activeLvlEditWin()->scene->EraserEnabled = false;
+    }
+}
+
+
+void MainWindow::on_actionSetFirstPlayer_triggered()
+{
+    resetEditmodeButtons();
+    ui->actionSetFirstPlayer->setChecked(1);
+}
+
+void MainWindow::on_actionSetSecondPlayer_triggered()
+{
+    resetEditmodeButtons();
+    ui->actionSetSecondPlayer->setChecked(1);
+
+}
+
+void MainWindow::on_actionDrawWater_triggered()
+{
+    resetEditmodeButtons();
+    ui->actionDrawWater->setChecked(1);
+
+}
+
+void MainWindow::on_actionDrawSand_triggered()
+{
+    resetEditmodeButtons();
+    ui->actionDrawSand->setChecked(1);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+
