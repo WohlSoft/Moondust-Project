101c64b00a0649d08295e9d3c12e23fe41e38fe4
Wohlstand <admin@wohlnet.ru>
2014-05-14 23:53:20 +0400

    Spell check, NPC configuration improvement

--- Editor/data_configs/conf_lvl_npc.cpp
+++ Editor/data_configs/conf_lvl_npc.cpp
@@ -22,7 +22,7 @@ void dataconfigs::loadLevelNPC()
 {
     unsigned int i;
 
-    obj_bgo snpc;
+    obj_npc snpc;
     unsigned long npc_total=0;
     QString npc_ini = config_dir + "lvl_blocks.ini";
     QSettings npcset(npc_ini, QSettings::IniFormat);
@@ -53,124 +53,241 @@ void dataconfigs::loadLevelNPC()
             index_npc.push_back(npcIndex);
         }
 
+        //Buffers
+        int defGFX_h = 0;
+        int combobox_size = 0;
+
+
+        for(i=1; i<=npc_total; i++)
+        {
+            npcset.beginGroup( QString("npc-"+QString::number(i)) );
         //    //    [npc-1]
         //        unsigned long id;
+            snpc.id = i;
         //    //    name="Goomba"
         //        QString name;
+            snpc.name = npcset.value("name", "").toString();
+
         //    //    category="Enemy"		;The sort category
         //        QString category;
+            snpc.category = npcset.value("category", "Other").toString();
         //    //    image="npc-1.gif"		;NPC Image file
-        //        QString image_n;
-        //        QString mask_n;
-        //        QPixmap image;
-        //        QBitmap mask;
-        //    //    algotytm="0"			;NPC's alhorytm. Alhoritm have states and events (onDie, onTail, onCollisionWithFlyBlock...)
-        //        int algorythm;
+
+            //        QString image_n;
+            //        QString mask_n;
+            //        QPixmap image;
+            //        QBitmap mask;
+
+            imgFile = npcset.value("image", "").toString();
+            snpc.image_n = imgFile;
+            if( (imgFile!="") )
+            {
+                tmp = imgFile.split(".", QString::SkipEmptyParts);
+                if(tmp.size()==2)
+                    imgFileM = tmp[0] + "m." + tmp[1];
+                else
+                    imgFileM = "";
+                snpc.mask_n = imgFileM;
+                if(tmp.size()==2) mask = QBitmap(blockPath + imgFileM);
+                snpc.mask = mask;
+                snpc.image = QPixmap(blockPath + imgFile);
+                if(tmp.size()==2) snpc.image.setMask(mask);
+            }
+            else
+            {
+                snpc.image = QPixmap(QApplication::applicationDirPath() + "/" + "data/unknown_npc.gif");
+                snpc.mask = QBitmap(QApplication::applicationDirPath() + "/" + "data/unknown_npc.gif");
+                snpc.image.setMask(mask);
+            }
+
+        //    //    algotitm="0"			;NPC's alhorytm. Alhoritm have states and events (onDie, onTail, onCollisionWithFlyBlock...)
+        //        int algorithm;
+            snpc.algorithm = npcset.value("algorithm", "0").toInt();
         //        //    ;If alhorythm = 0, will using basic parametric alhorythm.
         //    //    ;Else, get alhorythm from list
         //    //    default-effect=2		;Spawn effect ID on jump-die
         //        unsigned long effect_1;
+            snpc.effect_1 = npcset.value("default-effect", "10").toInt();
         //    //    shell-effect=4			;effect on kick by shell or other NPC
         //        unsigned long effect_2;
+            snpc.effect_2 = npcset.value("shell-effect", "10").toInt();
 
         //    //    ; graphics
         //        int gfx_offset_x;
         //    //    gfx-offst-x=0
+            snpc.gfx_offset_x = npcset.value("gfx-offst-x", "0").toInt();
         //        int gfx_offset_y;
         //    //    gfx-offst-y=2
-        //        int gfx_h;
-        //    //    gfx-height-y=32
-        //        int gfx_w;
-        //    //    gfx-width-y=32
+            snpc.gfx_offset_y = npcset.value("gfx-offst-y", "0").toInt();
         //        int grid;
         //    //    grid=32
+            snpc.grid = npcset.value("grid", "32").toInt();
         //    //    grid-offset-x=0
         //        int grid_offset_x;
+            snpc.grid_offset_x = npcset.value("grid-offset-x", "0").toInt();
         //    //    grid-offset-y=0
         //        int grid_offset_y;
+            snpc.grid_offset_y = npcset.value("grid-offset-y", "0").toInt();
         //    //    frame-style=0	; (0-2) This option in some alhoritmes can be ignored
         //        int framestyle;
+            snpc.framestyle = npcset.value("frame-style", "0").toInt();
         //    //    frames=2
         //        unsigned int frames;
+            snpc.frames = npcset.value("frames", "1").toInt();
+
+        //        int gfx_h;
+        //    //    gfx-height-y=32
+            switch(snpc.framestyle)
+            {
+            case 0:
+                defGFX_h = (int)round(snpc.image.height()/snpc.frames);
+                break;
+            case 1:
+                defGFX_h = (int)round(snpc.image.height()/snpc.frames)/2;
+                break;
+            case 2:
+                defGFX_h = (int)round(snpc.image.height()/snpc.frames)/4;
+                break;
+            case 3:
+                defGFX_h = (int)round(snpc.image.height()/snpc.frames)/4;
+                break;
+            case 4:
+                defGFX_h = (int)round(snpc.image.height()/snpc.frames)/8;
+                break;
+            default:
+                break;
+            }
+
+            snpc.gfx_h = npcset.value("gfx-height-y", QString::number(defGFX_h) ).toInt();
+        //        int gfx_w;
+        //    //    gfx-width-y=32
+            snpc.gfx_w = npcset.value("gfx-width-y", QString::number(snpc.image.width()) ).toInt();
+
         //    //    frame-speed=128
         //        unsigned int framespeed;
-        //    //    foregound=0
+            snpc.framespeed = npcset.value("frame-speed", "128").toInt();
+        //    //    foreground=0
         //        bool foreground;
+            snpc.foreground = npcset.value("foreground", "0").toBool();
         //    //    ; for editor
         //    //    custom-animation=0
         //        bool custom_animate;
+            snpc.custom_animate = npcset.value("custom-animation", "0").toBool();
         //    //    ; this option useful for non-standart algorithmic sprites (for example, bosses)
 
         //    //    ;custom-animation-alg=0		; Custom animation algorithm - 0 simple frame range, 1 - frame Jump
         //        int custom_ani_alg;
+            snpc.custom_ani_alg = npcset.value("custom-animation-alg", "0").toInt();
         //    //    ;custom-animation-fl=0		; First frame for LEFT
         //        int custom_ani_fl;
+            snpc.custom_ani_fl = npcset.value("custom-animation-fl", "0").toInt();
         //    //    ;custom-animation-el=0		; end frame for LEFT / Jump step
         //        int custom_ani_el;
+            snpc.custom_ani_el = npcset.value("custom-animation-el", "-1").toInt();
         //    //    ;custom-animation-fr=0		; first frame for RIGHT
         //        int custom_ani_fr;
+            snpc.custom_ani_fr = npcset.value("custom-animation-fr", "0").toInt();
         //    //    ;custom-animation-er=0		; end frame for RIGHT / Jump step
         //        int custom_ani_er;
+            snpc.custom_ani_er = npcset.value("custom-animation-er", "-1").toInt();
+
 
         //    //    container=0			; NPC can containing inside other NPC (need enable special option type 2)
         //        bool container;
+            snpc.container = npcset.value("container", "0").toBool();
 
         //    //    ; Special option
         //    //    have-special=0			; Special NPC's option, what can used by NPC's algorithm
         //        bool special_option;
+            snpc.special_option = npcset.value("have-special", "0").toBool();
+
         //    //    ;special-name="Cheep-cheep"	; 60
         //        QString special_name;
+            snpc.special_name = npcset.value("special-name", "Special option value").toString();
         //    //    ;special-type=0			; 61 0 combobox, 1 - spin, 2 - npc-id
         //        int special_type;
+            snpc.special_type = npcset.value("special-type", "1").toInt();
+
         //    //    ;special-combobox-size=3		; 62 quantity of options
-        //        QStringList special_combobox_opts;
-        //    //    ;special-option-0="Swim"		; 63 set title for 0 option combobox
-        //    //    ;special-option-1="Jump"		; 64 set title for 1 option combobox
-        //    //    ;special-option-2="Projective"	; 65 set title for 2 option combobox
+
+            combobox_size = npcset.value("special-combobox-size", "0").toInt();
+
+            //        QStringList special_combobox_opts;
+            //    //    ;special-option-0="Swim"		; 63 set title for 0 option combobox
+            //    //    ;special-option-1="Jump"		; 64 set title for 1 option combobox
+            //    //    ;special-option-2="Projective"	; 65 set title for 2 option combobox
+
+            snpc.special_combobox_opts.clear();
+            for(int j=0; j<combobox_size; j++)
+            {
+                snpc.special_combobox_opts.push_back(
+                            npcset.value(QString("special-option-%1").arg(j), "0").toString()
+                            );
+            }
+
         //        int special_spin_min;
         //    //    ;special-spin-min=0		; 66 milimal value of spin
+            snpc.special_spin_min = npcset.value("special-spin-min", "0").toInt();
         //        int special_spin_max;
         //    //    ;special-spin-max=25		; 67 maximal value of spin
+            snpc.special_spin_max = npcset.value("special-spin-max", "1").toInt();
 
         //    //    ;game process
         //    //    score=2				; Add scores to player (value 0-13)
         //    //    ; 0, 10, 100, 200, 400, 800, 1000, 2000, 4000, 8000, 1up, 2up, 5up, 3up
         //        int score;
+            snpc.score = npcset.value("score", "0").toInt();
+
         //    //    speed=64			; Default movement speed in px/s
         //        int speed;
+            snpc.speed = npcset.value("speed", "64").toInt();
         //    //    moving=1			; NPC simply moving right/left
-
         //        bool movement;
+            snpc.movement = npcset.value("moving", "1").toBool();
         //    //    scenery=0			; NPC as block
         //        bool scenery;
+            snpc.scenery = npcset.value("scenery", "0").toBool();
         //    //    immortal=0			; NPC Can't be destroy
         //        bool immortal;
+            snpc.immortal = npcset.value("immortal", "0").toBool();
         //    //    yoshicaneat=1			; NPC can be eaten by yoshi
         //        bool can_be_eaten;
+            snpc.can_be_eaten = npcset.value("yoshicaneat", "0").toBool();
         //    //    takeble=0			; NPC destroyble on contact with player
         //        bool takable;
+            snpc.takeble = npcset.value("takable", "0").toBool();
         //    //    grab-side=0			; NPC can be grabbed on side
         //        bool grab_side;
+            snpc.grab_side = npcset.value("grab-side", "0").toBool();
         //    //    grab-top=0			; NPC can be grabbed on top
         //        bool grab_top;
+            snpc.grab_top = npcset.value("grab-top", "0").toBool();
         //    //    grab-any=0			; NPC can be grabbed on any collisions
         //        bool grab_any;
+            snpc.grab_any = npcset.value("grab-any", "0").toBool();
         //    //    default-health=1		; NPC's health value
         //        int health;
+            snpc.health = npcset.value("default-health", "0").toInt();
         //    //    hurtpalyer=1			; Hurt player on contact
         //        bool hurt_player;
+            snpc.hurt_player = npcset.value("hurtpalyer", "0").toBool();
         //    //    hurtnpc=0			; Hurt other npc on contact
         //        bool hurt_npc;
+            snpc.hurt_npc = npcset.value("hurtnpc", "0").toBool();
 
         //    //    ;Editor featured
         //    //    allow-bubble=1			; Allow packable into the bubble
         //        bool allow_buble;
+            snpc.allow_buble = npcset.value("allow-bubble", "0").toBool();
         //    //    allow-egg=1			; Allow packable into the egg
         //        bool allow_egg;
+            snpc.allow_egg = npcset.value("allow-egg", "0").toBool();
         //    //    allow-lakitu=1			; Allow packable into the SMW Lakitu
         //        bool allow_lakitu;
+            snpc.allow_lakitu = npcset.value(" allow-lakitu", "0").toBool();
         //    //    allow-burred=1			; Allow packable under the herb
         //        bool allow_burred;
+            snpc.allow_burred = npcset.value(" allow-burred", "0").toBool();
 
 
         //    //    ; Physics
@@ -224,5 +341,13 @@ void dataconfigs::loadLevelNPC()
         //        bool turn_on_cliff_detect;
         //    //    lava-protection=0	; NPC will not be burn in lava
         //        bool lava_protect;
+            main_npc.push_back(snpc);
+
+            //Add to Index
+            if(i < (unsigned int)index_npc.size())
+                index_npc[i].i = i;
+
+        npcset.endGroup();
+        }
 
 }
