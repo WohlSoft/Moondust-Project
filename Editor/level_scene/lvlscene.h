95161fd71f111afd5a99336238efa44f2fb92a75
Wohlstand <admin@wohlnet.ru>
2014-05-15 23:55:34 +0400

    Layer's visible functions NPC's features

--- Editor/level_scene/lvlscene.h
+++ Editor/level_scene/lvlscene.h
@@ -60,9 +60,12 @@ struct UserBlocks
 
 struct UserNPCs
 {
+    bool withImg;
     QPixmap image;
     QBitmap mask;
     unsigned long id;
+
+    bool withTxt;
     NPCConfigFile sets;
 };
 
@@ -126,6 +129,8 @@ public:
     void setDoors(LevelData FileData, QProgressDialog &progress);
     void setPlayerPoints();
 
+    void applyLayersVisible();
+
     void startBlockAnimation();
     void stopAnimation();
 
@@ -141,6 +146,8 @@ public:
     QVector<UserBGs > uBGs;
     QVector<UserBGOs > uBGOs;
     QVector<UserBlocks > uBlocks;
+    QVector<UserNPCs > uNPCs;
+
     QGraphicsItem * itemCollidesWith(QGraphicsItem * item);
 
     LevelData  * LvlData;
@@ -152,6 +159,7 @@ public:
     //Object Indexing:
     QVector<blocksIndexes > index_blocks;
     QVector<bgoIndexes > index_bgo;
+    QVector<npcIndexes > index_npc;
 
     bool lock_bgo;
     bool lock_block;
