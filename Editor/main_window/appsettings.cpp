b054005365c44d83cef59d3353366a01c194df22
Wohlstand <admin@wohlnet.ru>
2014-05-20 23:58:59 +0400

    Small AppSettings dialog  improvement

--- Editor/main_window/appsettings.cpp
+++ Editor/main_window/appsettings.cpp
@@ -1,5 +1,8 @@
 #include "appsettings.h"
 #include "../ui_appsettings.h"
+#include <QFileDialog>
+
+//#include "../common_features/logger_sets.h"
 
 AppSettings::AppSettings(QWidget *parent) :
     QDialog(parent),
@@ -11,4 +14,28 @@ AppSettings::AppSettings(QWidget *parent) :
 AppSettings::~AppSettings()
 {
     delete ui;
+}
+
+void AppSettings::applySettings()
+{
+    //set settings into GUI fields
+    ui->autoPlayMusic->setChecked(autoPlayMusic);
+    ui->Animations->setChecked(Animation);
+    ui->itemsLisit->setValue(AnimationItemLimit);
+
+    ui->Collisions->setChecked(Collisions);
+
+    ui->logLevel->setCurrentIndex(loglevel);
+    ui->logFileName->setText(logfile);
+}
+
+void AppSettings::on_setLogFile_clicked()
+{
+    QString fileName = QFileDialog::getSaveFileName(this, tr("Set log file"),
+        logfile, tr("Text files (*.txt *.log)"));
+    if (fileName.isEmpty())
+        return;
+
+    logfile = fileName;
+    ui->logFileName->setText(fileName);
 }
