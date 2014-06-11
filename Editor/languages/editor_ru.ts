19ee08c67b0ec11df5d2212181908fd131564972
KevinW1998 <kevin.waldock@gmail.com>
2014-06-11 22:00:34 +0200

    Update Translations

--- Editor/languages/editor_ru.ts
+++ Editor/languages/editor_ru.ts
@@ -463,6 +463,107 @@ Message limits: max line lenth is 28 characters</source>
         <source>Search User NPCs %1</source>
         <translation>Поиск польз. нИП %1</translation>
     </message>
+    <message>
+        <location filename="../level_scene/HistoryManager.cpp" line="502"/>
+        <source>Undone: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../level_scene/HistoryManager.cpp" line="762"/>
+        <source>Redone: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../level_scene/HistoryManager.cpp" line="1603"/>
+        <source>Remove</source>
+        <translation type="unfinished">Удалить</translation>
+    </message>
+    <message>
+        <location filename="../level_scene/HistoryManager.cpp" line="1604"/>
+        <source>Place</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../level_scene/HistoryManager.cpp" line="1605"/>
+        <source>Move</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../level_scene/HistoryManager.cpp" line="1606"/>
+        <source>Changed Setting [%1]</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../level_scene/HistoryManager.cpp" line="1607"/>
+        <source>Change Layer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../level_scene/HistoryManager.cpp" line="1608"/>
+        <source>Resize Block</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../level_scene/HistoryManager.cpp" line="1609"/>
+        <source>Place Door</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../level_scene/HistoryManager.cpp" line="1610"/>
+        <source>Add Warp</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../level_scene/HistoryManager.cpp" line="1611"/>
+        <source>Remove Warp</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../level_scene/HistoryManager.cpp" line="1613"/>
+        <location filename="../level_scene/HistoryManager.cpp" line="1629"/>
+        <source>Unknown</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../level_scene/HistoryManager.cpp" line="1620"/>
+        <source>Slippery</source>
+        <translation type="unfinished">Скользкий</translation>
+    </message>
+    <message>
+        <location filename="../level_scene/HistoryManager.cpp" line="1621"/>
+        <source>Friendly</source>
+        <translation type="unfinished">Дружественный</translation>
+    </message>
+    <message>
+        <location filename="../level_scene/HistoryManager.cpp" line="1622"/>
+        <source>Boss</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../level_scene/HistoryManager.cpp" line="1623"/>
+        <source>Not Moveable</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../level_scene/HistoryManager.cpp" line="1624"/>
+        <source>Message</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../level_scene/HistoryManager.cpp" line="1625"/>
+        <source>Direction</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../level_scene/HistoryManager.cpp" line="1626"/>
+        <source>Included NPC</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../level_scene/HistoryManager.cpp" line="1627"/>
+        <source>Water Type</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>MainWindow</name>
