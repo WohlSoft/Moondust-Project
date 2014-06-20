45d9268f5783ccb1888823e42a9c8505309fac41
Wohlstand <admin@wohlnet.ru>
2014-06-20 12:13:48 +0400

    Translations refresh

--- Editor/languages/editor_it.ts
+++ Editor/languages/editor_it.ts
@@ -551,23 +551,35 @@ Message limits: max line lenth is 28 characters</source>
 <context>
     <name>LvlScene</name>
     <message>
-        <location filename="../level_scene/lvl_usergfx.cpp" line="48"/>
+        <location filename="../level_scene/lvl_usergfx.cpp" line="50"/>
         <source>Search User Backgrounds %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../level_scene/lvl_usergfx.cpp" line="115"/>
+        <location filename="../level_scene/lvl_usergfx.cpp" line="122"/>
         <source>Search User Blocks %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../level_scene/lvl_usergfx.cpp" line="178"/>
+        <location filename="../level_scene/lvl_usergfx.cpp" line="187"/>
         <source>Search User BGOs %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../level_scene/lvl_usergfx.cpp" line="241"/>
+        <location filename="../level_scene/lvl_usergfx.cpp" line="252"/>
         <source>Search User NPCs %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../level_scene/lvl_usergfx.cpp" line="397"/>
+        <source>Wrong custom images</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../level_scene/lvl_usergfx.cpp" line="398"/>
+        <source>This level have a wrong custom graphics files.
+You will see &apos;ghosties&apos; or other dummy images instead custom GFX of items, what used broken images. It occurred because, for example, the BMP format with GIF extension was used.
+Please, reconvert your images to valid format and try to reload this level.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
