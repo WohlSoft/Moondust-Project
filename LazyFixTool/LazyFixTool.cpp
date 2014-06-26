14bedfe60410d35401734ab853cda5c49df292d9
KevinW1998 <kevin.waldock@gmail.com>
2014-06-26 16:51:16 +0200

    Add Gif Writer

--- LazyFixTool/LazyFixTool.cpp
+++ LazyFixTool/LazyFixTool.cpp
@@ -55,6 +55,103 @@ QImage setAlphaMask(QImage image, QImage mask)
     return target;
 }
 
+bool toGif(QImage& img, QString& path){
+    int errcode;
+    GifFileType* t = EGifOpenFileName(path.toStdString().c_str(),true, &errcode);
+    if(!t){
+        EGifCloseFile(t, &errcode);
+        return false;
+    }
+
+    EGifSetGifVersion(t, true);
+
+    GifColorType* colorArr = new GifColorType[256];
+    ColorMapObject* cmo = GifMakeMapObject(256, colorArr);
+
+    bool unfinished = false;
+    QImage tarQImg(img.width(), img.height(), QImage::Format_Indexed8);
+    QVector<QRgb> table;
+    for(int y = 0; y < img.height(); y++){
+        for(int x = 0; x < img.width(); x++){
+            if(table.size() >= 256){
+                unfinished = true;
+                break;
+            }
+            QRgb pix;
+            if(!table.contains(pix = img.pixel(x,y))){
+                table.push_back(pix);
+                tarQImg.setColor(tarQImg.colorCount(), pix);
+            }
+            tarQImg.setPixel(x,y,table.indexOf(pix));
+        }
+        if(table.size() >= 256){
+            unfinished = true;
+            break;
+        }
+    }
+
+    if(unfinished){
+        EGifCloseFile(t, &errcode);
+        return false;
+    }
+
+
+    for(int l = tarQImg.colorCount(); l < 256; l++){
+        tarQImg.setColor(l,0);
+    }
+
+    if(tarQImg.colorTable().size() != 256){
+        EGifCloseFile(t, &errcode);
+        return false;
+    }
+
+    QVector<QRgb> clTab = tarQImg.colorTable();
+
+    for(int i = 0; i < 255; i++){
+        QRgb rgb = clTab[i];
+        colorArr[i].Red = qRed(rgb);
+        colorArr[i].Green = qGreen(rgb);
+        colorArr[i].Blue = qBlue(rgb);
+    }
+    cmo->Colors = colorArr;
+
+    errcode = EGifPutScreenDesc(t, img.width(), img.height(), 256, 0, cmo);
+    if(errcode != GIF_OK){
+        EGifCloseFile(t, &errcode);
+        return false;
+    }
+
+    errcode = EGifPutImageDesc(t, 0, 0, img.width(), img.height(), false, 0);
+    if(errcode != GIF_OK){
+        EGifCloseFile(t, &errcode);
+        return false;
+    }
+
+    //gen byte array
+    GifByteType* byteArr = tarQImg.bits();
+
+    for(int h = 0; h < tarQImg.height(); h++){
+        errcode = EGifPutLine(t, byteArr, tarQImg.width());
+        if(errcode != GIF_OK){
+            EGifCloseFile(t, &errcode);
+            return false;
+        }
+
+        byteArr += tarQImg.width();
+        byteArr += ((tarQImg.width() % 4)!=0 ? 4 - (tarQImg.width() % 4) : 0);
+    }
+
+
+
+
+
+    EGifCloseFile(t, &errcode);
+    if(errcode != GIF_OK){
+        return false;
+    }
+    return true;
+}
+
 QImage fromBMP(QString &file)
 {
     QImage errImg;
