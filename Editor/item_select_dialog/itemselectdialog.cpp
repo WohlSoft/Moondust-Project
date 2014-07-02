dbcde75b25b0ae169e4b0f96da7a441833228977
KevinW1998 <kevin.waldock@gmail.com>
2014-07-02 09:56:41 +0200

    Some progress to item selection dialog

--- Editor/item_select_dialog/itemselectdialog.cpp
+++ Editor/item_select_dialog/itemselectdialog.cpp
@@ -41,14 +41,17 @@ ItemSelectDialog::ItemSelectDialog(dataconfigs *configs,
         switch (npcExtraData.toInt()) {
         case 1:
         {
-            QRadioButton* npcFromList = new QRadioButton(tr("NPC from List"));
-            QRadioButton* npcCoins = new QRadioButton(tr("Coins"));
-            QSpinBox* npcCoinsSel = new QSpinBox();
+            npcFromList = new QRadioButton(tr("NPC from List"));
+            npcCoins = new QRadioButton(tr("Coins"));
+            npcCoinsSel = new QSpinBox();
             npcCoinsSel->setMinimum(1);
+            npcCoinsSel->setEnabled(false);
             extraNPCWid << npcFromList << npcCoins << npcCoinsSel;
             addExtraDataControl(npcFromList);
             addExtraDataControl(npcCoins);
             addExtraDataControl(npcCoinsSel);
+            npcFromList->setChecked(true);
+            connect(npcFromList, SIGNAL(toggled(bool)), this, SLOT(npcTypeChange(bool)));
             break;
         }
         default:
@@ -56,7 +59,51 @@ ItemSelectDialog::ItemSelectDialog(dataconfigs *configs,
         }
     }
 
-    on_Sel_TabCon_ItemType_currentChanged(0);
+    if(blockTab){
+        foreach(obj_block blockItem, conf->main_block)
+        {
+            //Add category
+            QPixmap tmpI = blockItem.image;
+
+            QListWidgetItem* item = new QListWidgetItem( blockItem.name );
+            item->setIcon( QIcon( tmpI ) );
+            item->setData(3, QString::number(blockItem.id) );
+            item->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEnabled );
+            ui->Sel_List_Block->addItem(item);
+        }
+    }
+
+    if(bgoTab){
+        foreach(obj_bgo bgoItem, conf->main_bgo)
+        {
+            //Add category
+            QPixmap tmpI = bgoItem.image;
+
+            QListWidgetItem* item = new QListWidgetItem( bgoItem.name );
+            item->setIcon( QIcon( tmpI ) );
+            item->setData(3, QString::number(bgoItem.id) );
+            item->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEnabled );
+            ui->Sel_List_BGO->addItem(item);
+        }
+    }
+
+    if(npcTab){
+        foreach(obj_npc npcItem, conf->main_npc)
+        {
+            //Add category
+            QPixmap tmpI = npcItem.image.copy(0,0, npcItem.image.width(), npcItem.gfx_h );
+
+            QListWidgetItem* item = new QListWidgetItem( npcItem.name );
+            item->setIcon( QIcon( tmpI ) );
+            item->setData(3, QString::number(npcItem.id) );
+            item->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEnabled );
+            ui->Sel_List_NPC->addItem(item);
+        }
+    }
+
+
+
+    on_Sel_TabCon_ItemType_currentChanged(ui->Sel_TabCon_ItemType->currentIndex());
 }
 
 ItemSelectDialog::~ItemSelectDialog()
@@ -86,4 +133,37 @@ void ItemSelectDialog::on_Sel_TabCon_ItemType_currentChanged(int index)
             ((QWidget*)(*it))->setVisible((ui->Sel_TabCon_ItemType->indexOf(ui->Sel_Tab_NPC)!=-1 ? ui->Sel_TabCon_ItemType->indexOf(ui->Sel_Tab_NPC)==index : false));
         }
     }
+}
+
+void ItemSelectDialog::npcTypeChange(bool /*toggled*/)
+{
+    ui->Sel_List_NPC->setEnabled(npcFromList->isChecked());
+    ui->Sel_Text_NPC->setEnabled(npcFromList->isChecked());
+    ui->Sel_Combo_CategoryNPC->setEnabled(npcFromList->isChecked());
+    ui->Sel_Combo_FiltertypeNPC->setEnabled(npcFromList->isChecked());
+    ui->Sel_Combo_GroupsNPC->setEnabled(npcFromList->isChecked());
+    npcCoinsSel->setEnabled(npcCoins->isChecked());
+}
+
+void ItemSelectDialog::on_Sel_DiaButtonBox_accepted()
+{
+    if(ui->Sel_TabCon_ItemType->indexOf(ui->Sel_Tab_Block)!=-1)
+        blockID = ui->Sel_List_Block->selectedItems()[0]->data(3).toInt();
+
+    if(ui->Sel_TabCon_ItemType->indexOf(ui->Sel_Tab_BGO)!=-1)
+        bgoID = ui->Sel_List_BGO->selectedItems()[0]->data(3).toInt();
+
+    if(ui->Sel_TabCon_ItemType->indexOf(ui->Sel_Tab_NPC)!=-1){
+        if(npcCoins!=0){
+            if(npcCoins->isChecked()){
+                npcID = npcCoinsSel->value();
+            }else{
+                npcID = ui->Sel_List_NPC->selectedItems()[0]->data(3).toInt()+1000;
+            }
+        }else{
+            npcID = ui->Sel_List_NPC->selectedItems()[0]->data(3).toInt()+1000;
+        }
+    }
+
+    this->accept();
 }
