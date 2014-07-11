6a652a4cac11d6d786156697e720efa6c7e21fba
Wohlstand <admin@wohlnet.ru>
2014-07-12 00:14:31 +0400

    Updated copyright header inside Calibrator sources

--- PlayableCalibrator/frame_matrix/MatrixScene.cpp
+++ PlayableCalibrator/frame_matrix/MatrixScene.cpp
@@ -1,10 +1,12 @@
 /*
- * SMBX64 Playble Character Sprite Calibrator, a free tool for playble srite design
+ * SMBX64 Playble Character Sprite Calibrator, a free tool for playable srite design
+ * This is a part of Platformer Game Engine by Wohlstand, a free platform for game making
  * Copyright (c) 2014 Vitaly Novichkov <admin@wohlnet.ru>
  *
- * This program is free software; you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 2 of the License
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -12,8 +14,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "MatrixScene.h"
