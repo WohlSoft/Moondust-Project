539796abe0bfc005c23b6b16f5f2c0c17249c11e
Wohlstand <admin@wohlnet.ru>
2014-07-30 16:29:24 +0400

    Added global project version file Each PGE's component can have own version number

--- pge_version.h
+++ pge_version.h
@@ -16,34 +16,37 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "../pge_version.h" //Global Project version file
+#ifndef GLOBAL_PGE_VERSION
+#define GLOBAL_PGE_VERSION
 
-#ifndef EDITOR_VERSION_H
-#define EDITOR_VERSION_H
+//Version of whole project
+#define _VERSION "0.2.0"
+#define _RELEASE " Alpha" //Developing state (for release this field must be empty)
 
-//Version of this program
-#define _FILE_VERSION "0.1.2.3-dev"
-#define _FILE_RELEASE " Alpha"
-
-#define _VF1 0
-#define _VF2 1
-#define _VF3 2
-#define _VF4 3
+#define _VP1 0
+#define _VP2 2
+#define _VP3 0
+#define _VP4 0
 
 
-#define _FILE_DESC "Platformer Game Engine - Editor"
+#define _COPYRIGHT "2014 by Wohlstand"
 
-#define _PRODUCT_NAME "Platformer Game Engine"
+#define _COMPANY "Wohlhabend Network System"
 
-#define _INTERNAL_NAME "pge_editor"
 
-#ifdef _WIN32
-	#define _ORIGINAL_NAME "pge_editor.exe" // for Windows platforms
+#ifdef _WIN64
+	#define OPERATION_SYSTEM "Windows"
+#elif  _WIN32
+	#define OPERATION_SYSTEM "Windows x64"
+#elif defined __APPLE__
+	#define OPERATION_SYSTEM "MacOSX"
+#elif defined __FreeBSD__
+	#define OPERATION_SYSTEM "FreeBSD"
+#elif defined __gnu_linux__
+	#define OPERATION_SYSTEM "GNU/Linux"
 #else
-	#define _ORIGINAL_NAME "pge_editor" // for any other platforms
+	#define OPERATION_SYSTEM "Other"
 #endif
 
-//Uncomment this for enable detal logging
-//#define DEBUG
 
 #endif
