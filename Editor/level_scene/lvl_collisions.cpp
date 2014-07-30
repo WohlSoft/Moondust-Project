7178be98a5ee94c3db3f210d2ab123478a07207e
Wohlstand <admin@wohlnet.ru>
2014-07-31 03:23:23 +0400

    Collision function update - Escape of using class types in collision check (for safe check with cursor or other abstract item) - Impossible place blocks over NPC's with enabled "block collision"

--- Editor/level_scene/lvl_collisions.cpp
+++ Editor/level_scene/lvl_collisions.cpp
@@ -28,22 +28,25 @@
 
 QGraphicsItem * LvlScene::itemCollidesWith(QGraphicsItem * item)
 {
-    qlonglong leftA, leftB;
-    qlonglong rightA, rightB;
-    qlonglong topA, topB;
-    qlonglong bottomA, bottomB;
+    qreal leftA, leftB;
+    qreal rightA, rightB;
+    qreal topA, topB;
+    qreal bottomA, bottomB;
     //qreal betweenZ;
 
-    QList<QGraphicsItem *> collisions = this->items(
-                item->pos().x(),item->pos().y(),
-                item->data(9).toReal(),item->data(10).toReal(),
-                Qt::IntersectsItemBoundingRect, Qt::AscendingOrder);
-        //collidingItems(item, Qt::IntersectsItemBoundingRect);
+//    QList<QGraphicsItem *> collisions = this->items(
+//                QRectF(item->scenePos().x(),item->scenePos().y(),
+//                item->data(9).toReal(),item->data(10).toReal() ),
+//                Qt::IntersectsItemBoundingRect, Qt::AscendingOrder);
+
+    QList<QGraphicsItem *> collisions = collidingItems(item, Qt::IntersectsItemBoundingRect);
+
     foreach (QGraphicsItem * it, collisions)
     {
             if (it == item)
                  continue;
-            if(!it->isVisible()) continue;
+            if(!it->isVisible())
+                continue;
             if(item->data(0).toString()=="Water")
                 return NULL;
             if(item->data(0).toString()=="Door_exit")
@@ -51,76 +54,90 @@ QGraphicsItem * LvlScene::itemCollidesWith(QGraphicsItem * item)
             if(item->data(0).toString()=="Door_enter")
                 return NULL;
 
-            leftA = item->scenePos().x();
-            rightA = item->scenePos().x()+item->data(9).toLongLong();
-            topA = item->scenePos().y();
-            bottomA = item->scenePos().y()+item->data(10).toLongLong();
+            if(
+               (it->data(0).toString()!="Block")&&
+               (it->data(0).toString()!="BGO")&&
+               (it->data(0).toString()!="NPC")
+                    ) continue;
 
-            leftB = it->scenePos().x();
-            rightB = it->scenePos().x()+it->data(9).toLongLong();
-            topB = it->scenePos().y();
-            bottomB = it->scenePos().y()+it->data(10).toLongLong();
+        leftA = item->scenePos().x();
+        rightA = item->scenePos().x()+item->data(9).toReal();
+        topA = item->scenePos().y();
+        bottomA = item->scenePos().y()+item->data(10).toReal();
+
+        leftB = it->scenePos().x();
+        rightB = it->scenePos().x()+it->data(9).toReal();
+        topB = it->scenePos().y();
+        bottomB = it->scenePos().y()+it->data(10).toReal();
 
             #ifdef _DEBUG_
             if(it->data(0).toString()=="Block")
                 WriteToLog(QtDebugMsg, QString(" >>Collision with block detected"));
             #endif
 
-
-          if((item->data(0).toString()=="Block")||(item->data(0).toString()=="NPC")||(item->data(0).toString()=="BGO"))
+          if(item->data(0).toString()=="NPC")
           {
-              if(item->data(0).toString()=="NPC")
+              if( item->data(8).toBool() ) // Disabled collisions with other NPCs
               {
-                  if( item->data(8).toBool() ) // Disabled collisions with other NPCs
-                  {
-                      if(item->data(1).toInt()!=it->data(1).toInt()) continue;
-                  }
-                  else
-                  {
-                      if(
-                              (
-                               (it->data(0).toString()=="Block")
-                               &&(!((ItemNPC *)item)->localProps.collision_with_blocks)
-                               )
-                              ||
-                              (
-                               (it->data(0).toString()=="NPC")
-                               &&(it->data(8).toBool())
-                               )
-                              ||
-                              ((it->data(0).toString()!="NPC")&&(it->data(0).toString()!="Block"))
-                         )
-                                continue;
-                  }
-
+                  if(item->data(1).toInt()!=it->data(1).toInt()) continue;
               }
               else
-                    if(item->data(0).toString()!=it->data(0).toString()) continue;
-
-              if(item->data(3).toString()=="sizable")
-              {//sizable Block
-                  #ifdef _DEBUG_
-                  WriteToLog(QtDebugMsg, QString("sizable block") );
-                  #endif
-                  continue;
+              {
+                  if(
+                          (
+                           (it->data(0).toString()=="Block")
+                           &&(!item->data(7).toBool()) //BlockCollision
+                           )
+                          ||
+                          (
+                           (it->data(0).toString()=="NPC")
+                           &&(it->data(8).toBool()) //NpcCollision
+                           )
+                          ||
+                          ((it->data(0).toString()!="NPC")&&(it->data(0).toString()!="Block"))
+                     )
+                            continue;
               }
 
-              if(item->data(0).toString()=="BGO")
-                if(item->data(1).toInt()!=it->data(1).toInt()) continue;
+          }
+          else
+          if(item->data(0).toString()=="Block")
+          {
+              if(
+                      (
+                       (it->data(0).toString()=="NPC")
+                       &&(!it->data(7).toBool()) //BlockCollision
+                       ) ||
+                      ((it->data(0).toString()!="NPC")&&(it->data(0).toString()!="Block"))
+                 )
+                        continue;
 
-                 if( bottomA <= topB )
-                 { continue; }
-                 if( topA >= bottomB )
-                 { continue; }
-                 if( rightA <= leftB )
-                 { continue; }
-                 if( leftA >= rightB )
-                 { continue; }
+          }
+          else
+                if(item->data(0).toString()!=it->data(0).toString()) continue;
 
-                 if(it->data(3).toString()!="sizable")
-                    return it;
+          if(item->data(3).toString()=="sizable")
+          {//sizable Block
+              #ifdef _DEBUG_
+              WriteToLog(QtDebugMsg, QString("sizable block") );
+              #endif
+              continue;
           }
 
+          if(item->data(0).toString()=="BGO")
+            if(item->data(1).toInt()!=it->data(1).toInt()) continue;
+
+             if( bottomA < topB )
+             { continue; }
+             if( topA > bottomB )
+             { continue; }
+             if( rightA < leftB )
+             { continue; }
+             if( leftA > rightB )
+             { continue; }
+
+             if(it->data(3).toString()!="sizable")
+                return it;
     }
     return NULL;
 }
