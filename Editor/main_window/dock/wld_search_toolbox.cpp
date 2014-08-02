48a658d251e902a4d43291ee3a612513d6fcf4f1
KevinW1998 <kevin.waldock@gmail.com>
2014-08-02 16:20:02 +0200

    Some work on extending ItemSelectDialog & World Search

--- Editor/main_window/dock/wld_search_toolbox.cpp
+++ Editor/main_window/dock/wld_search_toolbox.cpp
@@ -18,6 +18,16 @@
 
 #include "../../ui_mainwindow.h"
 #include "../../mainwindow.h"
+#include "../../item_select_dialog/itemselectdialog.h"
 
 bool lockResetWorld = false;
 
+void MainWindow::on_FindStartLevel_clicked()
+{
+    ItemSelectDialog* sel = new ItemSelectDialog(&configs, ItemSelectDialog::TAB_TILE | ItemSelectDialog::TAB_SCENERY |
+                                                 ItemSelectDialog::TAB_PATH | ItemSelectDialog::TAB_LEVEL | ItemSelectDialog::TAB_MUSIC);
+    sel->setWindowFlags (Qt::Window | Qt::WindowTitleHint | Qt::WindowCloseButtonHint);
+    sel->setGeometry(QStyle::alignedRect(Qt::LeftToRight, Qt::AlignCenter, sel->size(), qApp->desktop()->availableGeometry()));
+    sel->exec();
+    delete sel;
+}
