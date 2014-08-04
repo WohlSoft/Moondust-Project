44d1d350fc859a3fc408ea72df097a67f349c2d4
Wohlstand <admin@wohlnet.ru>
2014-08-04 17:54:11 +0400

    Tiny changes / added slots

--- Editor/tilesets/tilesetgroupeditor.h
+++ Editor/tilesets/tilesetgroupeditor.h
@@ -15,6 +15,19 @@ public:
     explicit TilesetGroupEditor(QWidget *parent = 0);
     ~TilesetGroupEditor();
 
+private slots:
+
+
+    void on_addTileset_clicked();
+
+    void on_RemoveTileset_clicked();
+
+    void on_Close_clicked();
+
+    void on_Open_clicked();
+
+    void on_Save_clicked();
+
 private:
     Ui::TilesetGroupEditor *ui;
 };
