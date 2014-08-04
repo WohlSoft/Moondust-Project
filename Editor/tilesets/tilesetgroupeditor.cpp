5701a0ead96c11329ad1425e54700383e6f30753
Wohlstand <admin@wohlnet.ru>
2014-08-04 17:54:11 +0400

    Tiny changes / added slots

--- Editor/tilesets/tilesetgroupeditor.cpp
+++ Editor/tilesets/tilesetgroupeditor.cpp
@@ -1,14 +1,43 @@
 #include "tilesetgroupeditor.h"
 #include "ui_tilesetgroupeditor.h"
 
+#include "tilesetitembutton.h"
+#include "../common_features/mainwinconnect.h"
+
 TilesetGroupEditor::TilesetGroupEditor(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::TilesetGroupEditor)
 {
     ui->setupUi(this);
+    //ui->PreviewBox->layout()->addWidget(new TilesetItemButton(&(MainWinConnect::pMainWin->configs)));
 }
 
 TilesetGroupEditor::~TilesetGroupEditor()
 {
     delete ui;
+}
+
+void TilesetGroupEditor::on_Close_clicked()
+{
+    this->close();
+}
+
+void TilesetGroupEditor::on_addTileset_clicked()
+{
+
+}
+
+void TilesetGroupEditor::on_RemoveTileset_clicked()
+{
+
+}
+
+void TilesetGroupEditor::on_Open_clicked()
+{
+
+}
+
+void TilesetGroupEditor::on_Save_clicked()
+{
+
 }
