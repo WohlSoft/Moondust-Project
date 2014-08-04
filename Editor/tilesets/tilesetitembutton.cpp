bdb8180520191e38754aefad83afccfde46cdc92
KevinW1998 <kevin.waldock@gmail.com>
2014-08-04 13:22:42 +0200

    More work on TilesetButton

--- Editor/tilesets/tilesetitembutton.cpp
+++ Editor/tilesets/tilesetitembutton.cpp
@@ -1,6 +1,46 @@
+/*
+ * Platformer Game Engine by Wohlstand, a free platform for game making
+ * Copyright (c) 2014 Vitaly Novichkov <admin@wohlnet.ru>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "tilesetitembutton.h"
 
-TilesetItemButton::TilesetItemButton(QObject *parent) :
+
+TilesetItemButton::TilesetItemButton(dataconfigs *conf, QObject *parent) :
     QFrame(parent)
 {
+    m_config = conf;
 }
+
+
+
+dataconfigs *TilesetItemButton::config() const
+{
+    return m_config;
+}
+
+void TilesetItemButton::setConfig(dataconfigs *config)
+{
+    m_config = config;
+}
+
+void TilesetItemButton::paintEvent(QPaintEvent *ev)
+{
+
+
+    QFrame::paintEvent(ev);
+}
+
--- Editor/tilesets/tilesetitembutton.h
+++ Editor/tilesets/tilesetitembutton.h
@@ -1,18 +1,46 @@
+/*
+ * Platformer Game Engine by Wohlstand, a free platform for game making
+ * Copyright (c) 2014 Vitaly Novichkov <admin@wohlnet.ru>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef TILESETITEMBUTTON_H
 #define TILESETITEMBUTTON_H
 
 #include <QFrame>
+#include "../data_configs/data_configs.h"
 
 class TilesetItemButton : public QFrame
 {
     Q_OBJECT
 public:
-    explicit TilesetItemButton(QObject *parent = 0);
+    explicit TilesetItemButton(dataconfigs* conf, QObject *parent = 0);
+
+    dataconfigs *config() const;
+    void setConfig(dataconfigs *config);
 
 signals:
 
 public slots:
 
+protected:
+    void paintEvent(QPaintEvent* ev);
+
+
+private:
+    dataconfigs* m_config;
 };
 
 #endif // TILESETITEMBUTTON_H
