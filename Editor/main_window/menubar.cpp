9881eca06d3603898a4d171a30ae27561871385a
Wohlstand <admin@wohlnet.ru>
2014-09-19 01:02:35 +0400

    Fixed item toolboxes re-updating on windows switch

--- Editor/main_window/menubar.cpp
+++ Editor/main_window/menubar.cpp
@@ -23,9 +23,23 @@
 #include "music_player.h"
 #include "../common_features/graphicsworkspace.h"
 
+namespace mainwindowMenuBar
+{
+    QMdiSubWindow *LastActiveSubWindow = NULL;
+}
+
 void MainWindow::updateMenus(bool force)
 {
-    if(!force) if(!this->isActiveWindow()) return;
+    using namespace mainwindowMenuBar;
+    if(!force)
+    {
+        //Don't update if window is not active
+        if(!this->isActiveWindow()) return;
+         //Don't update if this is - same subWindow
+        if(LastActiveSubWindow==ui->centralWidget->activeSubWindow()) return;
+    }
+
+    LastActiveSubWindow = ui->centralWidget->activeSubWindow();
 
     WriteToLog(QtDebugMsg, QString("Update menus"));
 
