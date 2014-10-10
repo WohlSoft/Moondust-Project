52599db55f64507c76f088e5c05e5478096c51b8
Wohlstand <admin@wohlnet.ru>
2014-10-11 00:38:23 +0400

    OUCH! forgot upload

---
+++ PlayableCalibrator/image_calibration/image_calibrator.h
@@ -0,0 +1,61 @@
+#ifndef IMAGE_CALIBRATOR_H
+#define IMAGE_CALIBRATOR_H
+
+#include <QDialog>
+#include <QPixmap>
+#include <QImage>
+#include <QGraphicsRectItem>
+#include <QGraphicsPixmapItem>
+#include "../main/globals.h"
+
+namespace Ui {
+class ImageCalibrator;
+}
+
+class ImageCalibrator : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit ImageCalibrator(QWidget *parent = 0);
+    ~ImageCalibrator();
+
+    bool init(QString imgPath);
+    SpriteScene * Scene;
+    QString targetPath;
+    QString pngPath;
+    QString gifPath;
+    QString gifPathM;
+    QString iniPath;
+
+private slots:
+    void on_FrameX_valueChanged(int arg1);
+    void on_FrameY_valueChanged(int arg1);
+    void on_OffsetX_valueChanged(int arg1);
+    void on_OffsetY_valueChanged(int arg1);
+
+    void on_Matrix_clicked();
+    void on_Reset_clicked();
+    void on_WritePNG_GIF_clicked();
+    void on_WritePNG_clicked();
+    void on_WriteGIF_clicked();
+    void updateScene();
+    void saveCalibrates();
+    void loadCalibrates();
+    QPixmap generateTarget();
+    QPixmap getFrame(int x, int y, int oX, int oY);
+
+private:
+    Ui::ImageCalibrator *ui;
+
+    int frmX;
+    int frmY;
+
+    QPixmap sprite;
+    QGraphicsPixmapItem *imgFrame;
+    QGraphicsRectItem *phsFrame;
+    QGraphicsRectItem *physics;
+
+};
+
+#endif // IMAGE_CALIBRATOR_H
