fdc82aafedaab8e320ea74ca67221452714a387c
Wohlstand <admin@wohlnet.ru>
2014-10-16 02:16:25 +0400

    Keyboard controller was added into level scene

--- Engine/controls/controller_keyboard.cpp
+++ Engine/controls/controller_keyboard.cpp
@@ -1,5 +1,51 @@
 #include "controller_keyboard.h"
 
 KeyboardController::KeyboardController()
+{}
+
+void KeyboardController::update(SDL_Event &event)
 {
+    switch(event.type)
+    {
+        case SDL_KEYDOWN: // If pressed key
+          switch(event.key.keysym.sym)
+          {
+            case SDLK_z:
+                keys.jump=true;
+            break;
+
+            case SDLK_x:
+                keys.run=true;
+            break;
+
+            case SDLK_RIGHT:
+                keys.right=true;
+            break;
+
+            case SDLK_LEFT:
+                keys.left=true;
+            break;
+          }
+        break;
+
+        case SDL_KEYUP:
+            switch(event.key.keysym.sym)
+            {
+                case SDLK_z:
+                    keys.jump=false;
+                break;
+
+                case SDLK_x:
+                    keys.run=false;
+                break;
+
+                case SDLK_RIGHT:
+                    keys.right=false;
+                    break;
+                case SDLK_LEFT:
+                    keys.left=false;
+                    break;
+            }
+        break;
+    }
 }
--- Engine/controls/controller_keyboard.h
+++ Engine/controls/controller_keyboard.h
@@ -2,11 +2,14 @@
 #define CONTROLLER_KEYBOARD_H
 
 #include "controller.h"
+#include <SDL2/SDL.h>
 
 class KeyboardController : public Controller
 {
 public:
     KeyboardController();
+    void update(SDL_Event &event);
+
 };
 
 #endif // CONTROLLER_KEYBOARD_H
