8b68eaef3e80b7e8a9c1d01b165d3eb369e9b22d
Wohlstand <admin@wohlnet.ru>
2014-10-16 19:50:04 +0400

    Fixed slowing of speed at time, no more using sensor

--- Engine/graphics/lvl_camera.cpp
+++ Engine/graphics/lvl_camera.cpp
@@ -18,12 +18,12 @@ PGE_LevelCamera::~PGE_LevelCamera()
 
     qDebug() << "Destroy camera";
 
-    if(sensor && worldPtr)
-    {
-        worldPtr->DestroyBody(sensor);
-        sensor->SetUserData(NULL);
-        sensor = NULL;
-    }
+//    if(sensor && worldPtr)
+//    {
+//        worldPtr->DestroyBody(sensor);
+//        sensor->SetUserData(NULL);
+//        sensor = NULL;
+//    }
 
 }
 
@@ -43,24 +43,24 @@ void PGE_LevelCamera::init(float x, float y, float w, float h)
     width = w;
     height = h;
 
-    if(!sensor)
-    {
-        b2BodyDef bodyDef;
-        bodyDef.type = b2_dynamicBody;
-        bodyDef.position.Set(PhysUtil::pix2met(x + (w/2)),
-                             PhysUtil::pix2met(y + (h/2) ) );
-        bodyDef.fixedRotation = true;
-        bodyDef.gravityScale = 0;
-        sensor = worldPtr->CreateBody(&bodyDef);
+//    if(!sensor)
+//    {
+//        b2BodyDef bodyDef;
+//        bodyDef.type = b2_dynamicBody;
+//        bodyDef.position.Set(PhysUtil::pix2met(x + (w/2)),
+//                             PhysUtil::pix2met(y + (h/2) ) );
+//        bodyDef.fixedRotation = true;
+//        bodyDef.gravityScale = 0;
+//        sensor = worldPtr->CreateBody(&bodyDef);
 
-        b2PolygonShape shape;
-        shape.SetAsBox(PhysUtil::pix2met(w)/2, PhysUtil::pix2met(h)/2);
-        b2FixtureDef fixtureDef;
-        fixtureDef.shape = &shape;
-        fixtureDef.isSensor = true;
-        fixtureDef.density = 0.0f; fixtureDef.friction = 0.0f;
-        sensor->CreateFixture(&fixtureDef);
-    }
+//        b2PolygonShape shape;
+//        shape.SetAsBox(PhysUtil::pix2met(w)/2, PhysUtil::pix2met(h)/2);
+//        b2FixtureDef fixtureDef;
+//        fixtureDef.shape = &shape;
+//        fixtureDef.isSensor = true;
+//        fixtureDef.density = 0.0f; fixtureDef.friction = 0.0f;
+//        sensor->CreateFixture(&fixtureDef);
+//    }
 
 }
 
@@ -100,9 +100,8 @@ void PGE_LevelCamera::setPos(float x, float y)
     if((pos_y+height) > s_bottom)
         pos_y = s_bottom-height;
 
-    sensor->SetTransform(b2Vec2( PhysUtil::pix2met(pos_x+width/2),
-                                 PhysUtil::pix2met(pos_y+height/2)), 0);
-    sensor->SetLinearVelocity(b2Vec2(0,0));
+    //    sensor->SetTransform(b2Vec2( PhysUtil::pix2met(pos_x+width/2),
+    //                                 PhysUtil::pix2met(pos_y+height/2)), 0);
 
     pos_x = round(pos_x);
     pos_y = round(pos_y);
@@ -112,31 +111,47 @@ void PGE_LevelCamera::setSize(int w, int h)
 {
     width = w;
     height = h;
-    b2PolygonShape* shape = (b2PolygonShape*)(sensor->GetFixtureList()->GetShape());
-    shape->m_vertices[0].Set(-PhysUtil::pix2met(w)/2, -PhysUtil::pix2met(h)/2);
-    shape->m_vertices[1].Set( PhysUtil::pix2met(w)/2, -PhysUtil::pix2met(h)/2);
-    shape->m_vertices[2].Set( PhysUtil::pix2met(w)/2,  PhysUtil::pix2met(h)/2);
-    shape->m_vertices[3].Set(-PhysUtil::pix2met(w)/2,  PhysUtil::pix2met(h)/2);
+//    b2PolygonShape* shape = (b2PolygonShape*)(sensor->GetFixtureList()->GetShape());
+//    shape->m_vertices[0].Set(-PhysUtil::pix2met(w)/2, -PhysUtil::pix2met(h)/2);
+//    shape->m_vertices[1].Set( PhysUtil::pix2met(w)/2, -PhysUtil::pix2met(h)/2);
+//    shape->m_vertices[2].Set( PhysUtil::pix2met(w)/2,  PhysUtil::pix2met(h)/2);
+//    shape->m_vertices[3].Set(-PhysUtil::pix2met(w)/2,  PhysUtil::pix2met(h)/2);
 }
 
+//subclass b2QueryCallback
+class CollidablesInRegionQueryCallback : public b2QueryCallback
+{
+public:
+  QVector<b2Body*> foundBodies;
+
+  bool ReportFixture(b2Fixture* fixture) {
+      foundBodies.push_back( fixture->GetBody() );
+      return true;//keep going to find all fixtures in the query area
+  }
+};
 
 void PGE_LevelCamera::update()
 {
     objects_to_render.clear();
 
-    if(!sensor) return;
-    //int contacts = 0;
+//    if(!sensor) return;
 
-    for(b2ContactEdge* ce = sensor->GetContactList(); ce; ce = ce->next)
+    CollidablesInRegionQueryCallback cb = CollidablesInRegionQueryCallback();
+    b2AABB aabb;
+    aabb.lowerBound.Set(PhysUtil::pix2met(pos_x), PhysUtil::pix2met(pos_y));
+    aabb.upperBound.Set(PhysUtil::pix2met(pos_x+width), PhysUtil::pix2met(pos_y+height));
+    worldPtr->QueryAABB(&cb, aabb);
+
+    int contacts = 0;
+
+    //for(b2ContactEdge* ce = sensor->GetContactList(); ce; ce = ce->next)
+    for(int i=0; i<cb.foundBodies.size();i++)
     {
-        b2Contact* c = ce->contact;
-        //contacts++;
+        //        b2Contact* c = ce->contact;
+        contacts++;
+         PGE_Phys_Object * visibleBody;
 
-        PGE_Phys_Object * visibleBody;
-        visibleBody = static_cast<PGE_Phys_Object *>(c->GetFixtureA()->GetBody()->GetUserData());
-
-        if(visibleBody==NULL)
-            visibleBody = static_cast<PGE_Phys_Object *>(c->GetFixtureB()->GetBody()->GetUserData());
+        visibleBody = static_cast<PGE_Phys_Object *>(cb.foundBodies[i]->GetUserData());
 
         if(visibleBody==NULL)
             continue;
@@ -151,7 +166,7 @@ void PGE_LevelCamera::update()
             objects_to_render.push_back(visibleBody);
         }
     }
-    //qDebug() << "Contacts" << contacts;
+    //qDebug() << "VisibleItems" << objects_to_render.size()  << contacts;
 }
 
 
