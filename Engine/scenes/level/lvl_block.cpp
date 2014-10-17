1c51a6d04821b4fcc62c498857a7499e20e5c142
Wohlstand <admin@wohlnet.ru>
2014-10-18 01:32:14 +0400

    Added shape definer for blocks (box or triangle (slope))

--- Engine/scenes/level/lvl_block.cpp
+++ Engine/scenes/level/lvl_block.cpp
@@ -39,9 +39,74 @@ void LVL_Block::init()
         PhysUtil::pix2met(data->y + posY_coefficient ) );
     bodyDef.userData = (void*)dynamic_cast<PGE_Phys_Object *>(this);
     physBody = worldPtr->CreateBody(&bodyDef);
+
     b2PolygonShape shape;
-    shape.SetAsBox(PhysUtil::pix2met(posX_coefficient), PhysUtil::pix2met(posY_coefficient) );
+
+    switch(setup->phys_shape)
+    {
+    //triangles
+    case 1: //up-left
+        {
+          b2Vec2 vertices[3];
+          //Left-top
+          vertices[0].Set(PhysUtil::pix2met(-posX_coefficient),  PhysUtil::pix2met(-posY_coefficient));
+          //right-bottom
+          vertices[1].Set(PhysUtil::pix2met(posX_coefficient),  PhysUtil::pix2met(posY_coefficient));
+          //left-bottom
+          vertices[2].Set(PhysUtil::pix2met(-posX_coefficient), PhysUtil::pix2met(posY_coefficient));
+          shape.Set(vertices, 3);
+          break;
+        }
+    case -1: // Up-right
+        {
+          b2Vec2 vertices[3];
+          //Left-bottom
+          vertices[0].Set(PhysUtil::pix2met(-posX_coefficient),  PhysUtil::pix2met(posY_coefficient));
+          //right-top
+          vertices[1].Set(PhysUtil::pix2met(posX_coefficient),  PhysUtil::pix2met(-posY_coefficient));
+          //right-bottom
+          vertices[2].Set(PhysUtil::pix2met(posX_coefficient), PhysUtil::pix2met(posY_coefficient));
+          shape.Set(vertices, 3);
+          break;
+        }
+    case 2: //right
+        {
+          b2Vec2 vertices[3];
+          //Left-top
+          vertices[0].Set(PhysUtil::pix2met(-posX_coefficient),  PhysUtil::pix2met(-posY_coefficient));
+          //right-top
+          vertices[1].Set(PhysUtil::pix2met(posX_coefficient),  PhysUtil::pix2met(-posY_coefficient));
+          //left-bottom
+          vertices[2].Set(PhysUtil::pix2met(-posX_coefficient), PhysUtil::pix2met(posY_coefficient));
+          shape.Set(vertices, 3);
+          break;
+        }
+    case -2: //left
+        {
+          b2Vec2 vertices[3];
+          //Left-top
+          vertices[0].Set(PhysUtil::pix2met(-posX_coefficient),  PhysUtil::pix2met(-posY_coefficient));
+          //right-top
+          vertices[1].Set(PhysUtil::pix2met(posX_coefficient),  PhysUtil::pix2met(-posY_coefficient));
+          //right-bottom
+          vertices[2].Set(PhysUtil::pix2met(posX_coefficient), PhysUtil::pix2met(posY_coefficient));
+          shape.Set(vertices, 3);
+          break;
+        }
+    case 3:// map shape from texture
+        //Is not implemented, create the box:
+
+    //rectangle box
+    default: //Box shape
+        shape.SetAsBox(PhysUtil::pix2met(posX_coefficient), PhysUtil::pix2met(posY_coefficient) );
+    }
+
     b2Fixture * block = physBody->CreateFixture(&shape, 1.0f);
+
+    if((setup->sizable) || (setup->collision==0))
+        block->SetSensor(true);
+
+
     block->SetFriction(data->slippery? 0.04f : 0.25f );
 
 }
