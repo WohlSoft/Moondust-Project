03b788031b9cd9a3a3e8a755f7a5cc30afda6806
Wohlstand <admin@wohlnet.ru>
2014-11-04 20:25:14 +0300

    Added template of "Set Point" mode

---
+++ Editor/world_scene/edit_modes/wld_mode_setpoint.cpp
@@ -0,0 +1,42 @@
+#include "wld_mode_setpoint.h"
+
+
+WLD_ModeSetPoint::WLD_ModeSetPoint(QGraphicsScene *parentScene, QObject *parent)
+    : EditMode("SetPoint", parentScene, parent)
+{}
+
+WLD_ModeSetPoint::~WLD_ModeSetPoint()
+{
+
+}
+
+void WLD_ModeSetPoint::set()
+{
+
+}
+
+void WLD_ModeSetPoint::mousePress(QGraphicsSceneMouseEvent *mouseEvent)
+{
+
+}
+
+void WLD_ModeSetPoint::mouseMove(QGraphicsSceneMouseEvent *mouseEvent)
+{
+
+}
+
+void WLD_ModeSetPoint::mouseRelease(QGraphicsSceneMouseEvent *mouseEvent)
+{
+
+}
+
+void WLD_ModeSetPoint::keyPress(QKeyEvent *keyEvent)
+{
+
+}
+
+void WLD_ModeSetPoint::keyRelease(QKeyEvent *keyEvent)
+{
+
+}
+
