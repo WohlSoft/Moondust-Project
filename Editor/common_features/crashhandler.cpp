91c25d8f72de352941f27c4e43c755d1cf8cfd01
KevinW1998 <kevin.waldock@gmail.com>
2014-11-05 18:04:42 +0100

    Some new crash handler test...

--- Editor/common_features/crashhandler.cpp
+++ Editor/common_features/crashhandler.cpp
@@ -4,14 +4,17 @@
 
 //temp
 #include <QMessageBox>
+#ifdef _WIN32
+#include <windows.h>
+#include <dbghelp.h>
+#endif
 
-
-
-CrashHandler::CrashHandler(QWidget *parent) :
+CrashHandler::CrashHandler(QString &crashText, QWidget *parent) :
     QDialog(parent),
     ui(new Ui::CrashHandler)
 {
     ui->setupUi(this);
+    ui->crashData->setText(crashText);
 }
 
 
@@ -22,9 +25,41 @@ CrashHandler::~CrashHandler()
 
 void CrashHandler::crashByFlood()
 {
-    QMessageBox::warning(nullptr, QApplication::tr("Crash"), QApplication::tr("We're sorry, but PGE has crashed. Reason: Out of memory! :(\n"
-                                                                              "To prevent this, try closing other uneccessary programs to free up more memory."));
+    QString crashMsg = QApplication::tr("We're sorry, but PGE has crashed. Reason: Out of memory! :(\n"
+                                        "To prevent this, try closing other uneccessary programs to free up more memory.");
+#ifdef _WIN32
+    unsigned int   i;
+    void         * stack[ 100 ];
+    unsigned short frames;
+    SYMBOL_INFO  * symbol;
+    HANDLE         process;
 
+    process = GetCurrentProcess();
+
+    SymInitialize( process, NULL, TRUE );
+
+    frames               = CaptureStackBackTrace( 0, 100, stack, NULL );
+    symbol               = ( SYMBOL_INFO * )calloc( sizeof( SYMBOL_INFO ) + 256 * sizeof( char ), 1 );
+    symbol->MaxNameLen   = 255;
+    symbol->SizeOfStruct = sizeof( SYMBOL_INFO );
+
+    QString bkTrace("");
+    for( i = 0; i < frames; i++ )
+    {
+        char buf[500];
+        SymFromAddr( process, ( DWORD64 )( stack[ i ] ), 0, symbol );
+
+        sprintf(buf, "%i: %s - 0x%0X\n", frames - i - 1, symbol->Name, symbol->Address );
+        bkTrace += buf;
+    }
+    crashMsg = crashMsg + QString("\n\n") + bkTrace;
+    CrashHandler* crsh = new CrashHandler(crashMsg);
+    crsh->exec();
+#else
+    QMessageBox::warning(nullptr, QApplication::tr("Crash"), crashMsg);
+
+
+#endif
     std::exit(1);
 }
 
@@ -39,7 +74,38 @@ void CrashHandler::crashByUnhandledException()
         std::string s2 = e.what();
         std::string s3 = " :(\nPlease inform our forum staff so we can try to fix this problem, Thank you\n\nForum link: engine.wohlnet.ru/forum";
 
+        QString crashMsg = (s1 + s2 + s3).c_str();
+#ifdef _WIN32
+        unsigned int   i;
+        void         * stack[ 100 ];
+        unsigned short frames;
+        SYMBOL_INFO  * symbol;
+        HANDLE         process;
+
+        process = GetCurrentProcess();
+
+        SymInitialize( process, NULL, TRUE );
+
+        frames               = CaptureStackBackTrace( 0, 100, stack, NULL );
+        symbol               = ( SYMBOL_INFO * )calloc( sizeof( SYMBOL_INFO ) + 256 * sizeof( char ), 1 );
+        symbol->MaxNameLen   = 255;
+        symbol->SizeOfStruct = sizeof( SYMBOL_INFO );
+
+        QString bkTrace("");
+        for( i = 0; i < frames; i++ )
+        {
+            char buf[500];
+            SymFromAddr( process, ( DWORD64 )( stack[ i ] ), 0, symbol );
+
+            sprintf(buf, "%i: %s - 0x%0X\n", frames - i - 1, symbol->Name, symbol->Address );
+            bkTrace += buf;
+        }
+        crashMsg = crashMsg + QString("\n\n") + bkTrace;
+        CrashHandler* crsh = new CrashHandler(crashMsg);
+        crsh->exec();
+#else
         QMessageBox::warning(nullptr, QApplication::tr("Crash"), QApplication::tr((s1 + s2 + s3).c_str()));
+#endif
     }
 
     std::exit(2);
