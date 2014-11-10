641057ac274ab53ae3afac910060a86f6ef9b646
KevinW1998 <kevin.waldock@gmail.com>
2014-11-10 20:28:51 +0100

    Improving crash handler + adding segserv handler

--- Editor/common_features/crashhandler.cpp
+++ Editor/common_features/crashhandler.cpp
@@ -44,12 +44,83 @@ CrashHandler::~CrashHandler()
 
 void CrashHandler::crashByFlood()
 {
-    QString crashMsg = QApplication::tr("We're sorry, but PGE Editor has crashed. Reason: Out of memory! :(\n"
+    QString crashMsg = QApplication::tr("We're sorry, but PGE Editor has crashed. \nReason: Out of memory!\n\n"
                                         "To prevent this, try closing other uneccessary programs to free up more memory.");
 #ifdef _WIN32
     if(DevConsole::isConsoleShown())
         DevConsole::closeIfPossible();
 
+    crashMsg = crashMsg + QString("\n\n") + getStacktrace();
+    CrashHandler* crsh = new CrashHandler(crashMsg);
+    crsh->exec();
+#else
+    QMessageBox::warning(nullptr, QApplication::tr("Crash"), crashMsg);
+
+
+#endif
+    exit(EXIT_FAILURE);
+}
+
+void CrashHandler::crashByUnhandledException()
+{
+    std::exception_ptr unhandledException = std::current_exception();
+    try{
+        std::rethrow_exception(unhandledException);
+    }
+    catch(const std::exception& e)
+    {
+        QString crashMsg = QApplication::tr("We're sorry, but PGE Editor has crashed. \nReason: %1\n\nPlease inform our forum staff so we can try to fix this problem, Thank you\n\nForum link: engine.wohlnet.ru/forum").arg(e.what());
+#ifdef _WIN32
+        if(DevConsole::isConsoleShown())
+            DevConsole::closeIfPossible();
+
+        crashMsg = crashMsg + QString("\n\n") + getStacktrace();
+        CrashHandler* crsh = new CrashHandler(crashMsg);
+        crsh->exec();
+#else
+        QMessageBox::warning(nullptr, QApplication::tr("Crash"), crashMsg);
+#endif
+    }
+
+    exit(EXIT_FAILURE);
+}
+
+void CrashHandler::crashBySIGSERV(int /*signalid*/)
+{
+    QString crashMsg = QApplication::tr("We're sorry, but PGE Editor has crashed. \nReason: Signal Segmentation Violation [SIGSERV]\n\n");
+#ifdef _WIN32
+    if(DevConsole::isConsoleShown())
+        DevConsole::closeIfPossible();
+
+    crashMsg = crashMsg + QString("\n\n") + getStacktrace();
+    CrashHandler* crsh = new CrashHandler(crashMsg);
+    crsh->exec();
+#else
+    QMessageBox::warning(nullptr, QApplication::tr("Crash"), crashMsg);
+#endif
+    exit(EXIT_FAILURE);
+}
+
+void CrashHandler::initCrashHandlers()
+{
+    std::set_new_handler(&crashByFlood);
+    std::set_terminate(&crashByUnhandledException);
+    signal(SIGSEGV, &crashBySIGSERV);
+}
+
+void CrashHandler::on_pgeForumButton_clicked()
+{
+    QDesktopServices::openUrl(QUrl("http://engine.wohlnet.ru/forum/"));
+}
+
+void CrashHandler::on_exitButton_clicked()
+{
+    this->close();
+}
+
+QString CrashHandler::getStacktrace()
+{
+#ifdef _WIN32
     unsigned int   i;
     void         * stack[ 100 ];
     unsigned short frames;
@@ -74,76 +145,8 @@ void CrashHandler::crashByFlood()
         sprintf(buf, "%i: %s - 0x%0llx\n", frames - i - 1, symbol->Name, symbol->Address );
         bkTrace += buf;
     }
-    crashMsg = crashMsg + QString("\n\n") + bkTrace;
-    CrashHandler* crsh = new CrashHandler(crashMsg);
-    crsh->exec();
+    return bkTrace;
 #else
-    QMessageBox::warning(nullptr, QApplication::tr("Crash"), crashMsg);
-
-
+    return QString("");
 #endif
-    exit(1);
-}
-
-void CrashHandler::crashByUnhandledException()
-{
-    std::exception_ptr unhandledException = std::current_exception();
-    try{
-        std::rethrow_exception(unhandledException);
-    }
-    catch(const std::exception& e)
-    {
-        QString crashMsg = QApplication::tr("We're sorry, but PGE Editor has crashed. Reason: \n%1\n :(\nPlease inform our forum staff so we can try to fix this problem, Thank you\n\nForum link: engine.wohlnet.ru/forum");
-#ifdef _WIN32
-        if(DevConsole::isConsoleShown())
-            DevConsole::closeIfPossible();
-        unsigned int   i;
-        void         * stack[ 100 ];
-        unsigned short frames;
-        SYMBOL_INFO  * symbol;
-        HANDLE         process;
-
-        process = GetCurrentProcess();
-
-        SymInitialize( process, NULL, TRUE );
-
-        frames               = CaptureStackBackTrace( 0, 100, stack, NULL );
-        symbol               = ( SYMBOL_INFO * )calloc( sizeof( SYMBOL_INFO ) + 256 * sizeof( char ), 1 );
-        symbol->MaxNameLen   = 255;
-        symbol->SizeOfStruct = sizeof( SYMBOL_INFO );
-
-        QString bkTrace("");
-        for( i = 0; i < frames; i++ )
-        {
-            char buf[500];
-            SymFromAddr( process, ( DWORD64 )( stack[ i ] ), 0, symbol );
-
-            sprintf(buf, "%i: %s - 0x%0llx\n", frames - i - 1, symbol->Name, symbol->Address );
-            bkTrace += buf;
-        }
-        crashMsg = crashMsg + QString("\n\n") + bkTrace;
-        CrashHandler* crsh = new CrashHandler(crashMsg);
-        crsh->exec();
-#else
-        QMessageBox::warning(nullptr, QApplication::tr("Crash"), QApplication::tr((s1 + s2 + s3).c_str()));
-#endif
-    }
-
-    exit(2);
-}
-
-void CrashHandler::initCrashHandlers()
-{
-    std::set_new_handler(&crashByFlood);
-    std::set_terminate(&crashByUnhandledException);
-}
-
-void CrashHandler::on_pgeForumButton_clicked()
-{
-    QDesktopServices::openUrl(QUrl("http://engine.wohlnet.ru/forum/"));
-}
-
-void CrashHandler::on_exitButton_clicked()
-{
-    this->close();
 }
