2b29a93f12b4a8038acee613cbfcfd0b7d65d862
Wohlstand <admin@wohlnet.ru>
2014-11-11 02:24:30 +0300

    Added stacktrace for Linux

--- Editor/common_features/crashhandler.cpp
+++ Editor/common_features/crashhandler.cpp
@@ -17,18 +17,67 @@
  */
 
 #include "crashhandler.h"
-#include "ui_crashhandler.h"
+#include <ui_crashhandler.h>
 #include "dev_console/devconsole.h"
 #include <QDesktopServices>
 
 //temp
-#include <QMessageBox>
-#ifdef _WIN32
-#include <windows.h>
-#include <dbghelp.h>
-#endif
+//#include <QMessageBox>
 
+#ifdef _WIN32
+    #include <windows.h>
+    #include <dbghelp.h>
+#elif __linux__
+    #include <execinfo.h>
+    #include <unistd.h>
+#endif
 #include <signal.h>
+
+QString CrashHandler::getStacktrace()
+{
+    #ifdef _WIN32
+        unsigned int   i;
+        void         * stack[ 100 ];
+        unsigned short frames;
+        SYMBOL_INFO  * symbol;
+        HANDLE         process;
+
+        process = GetCurrentProcess();
+
+        SymInitialize( process, NULL, TRUE );
+
+        frames               = CaptureStackBackTrace( 0, 100, stack, NULL );
+        symbol               = ( SYMBOL_INFO * )calloc( sizeof( SYMBOL_INFO ) + 256 * sizeof( char ), 1 );
+        symbol->MaxNameLen   = 255;
+        symbol->SizeOfStruct = sizeof( SYMBOL_INFO );
+
+        QString bkTrace("");
+        for( i = 0; i < frames; i++ )
+        {
+            char buf[500];
+            SymFromAddr( process, ( DWORD64 )( stack[ i ] ), 0, symbol );
+
+            sprintf(buf, "%i: %s - 0x%0llx\n", frames - i - 1, symbol->Name, symbol->Address );
+            bkTrace += buf;
+        }
+        return bkTrace;
+    #elif __linux__
+        void *array[400];
+        size_t size;
+
+        char **strings;
+        size = backtrace(array, 10);
+
+        strings = backtrace_symbols(array, size);
+
+        QString bkTrace("");
+        for(int j = 0; j < size; j++)
+             bkTrace += QString(strings[j])+"\n";
+        return bkTrace;
+    #else
+        return QString("");
+    #endif
+}
 
 CrashHandler::CrashHandler(QString &crashText, QWidget *parent) :
     QDialog(parent),
@@ -48,18 +97,15 @@ void CrashHandler::crashByFlood()
 {
     QString crashMsg = QApplication::tr("We're sorry, but PGE Editor has crashed. \nReason: Out of memory!\n\n"
                                         "To prevent this, try closing other uneccessary programs to free up more memory.");
-#ifdef _WIN32
+
     if(DevConsole::isConsoleShown())
         DevConsole::closeIfPossible();
 
-    crashMsg = crashMsg + QString("\n\n") + getStacktrace();
+    crashMsg += QString("\n\n") + getStacktrace();
+
     CrashHandler* crsh = new CrashHandler(crashMsg);
     crsh->exec();
-#else
-    QMessageBox::warning(nullptr, QApplication::tr("Crash"), crashMsg);
 
-
-#endif
     exit(EXIT_FAILURE);
 }
 
@@ -72,16 +118,14 @@ void CrashHandler::crashByUnhandledException()
     catch(const std::exception& e)
     {
         QString crashMsg = QApplication::tr("We're sorry, but PGE Editor has crashed. \nReason: %1\n\nPlease inform our forum staff so we can try to fix this problem, Thank you\n\nForum link: engine.wohlnet.ru/forum").arg(e.what());
-#ifdef _WIN32
+
         if(DevConsole::isConsoleShown())
             DevConsole::closeIfPossible();
 
-        crashMsg = crashMsg + QString("\n\n") + getStacktrace();
+        crashMsg += QString("\n\n") + getStacktrace();
+
         CrashHandler* crsh = new CrashHandler(crashMsg);
         crsh->exec();
-#else
-        QMessageBox::warning(nullptr, QApplication::tr("Crash"), crashMsg);
-#endif
     }
 
     exit(EXIT_FAILURE);
@@ -90,16 +134,15 @@ void CrashHandler::crashByUnhandledException()
 void CrashHandler::crashBySIGSERV(int /*signalid*/)
 {
     QString crashMsg = QApplication::tr("We're sorry, but PGE Editor has crashed. \nReason: Signal Segmentation Violation [SIGSERV]\n\n");
-#ifdef _WIN32
+
     if(DevConsole::isConsoleShown())
         DevConsole::closeIfPossible();
 
-    crashMsg = crashMsg + QString("\n\n") + getStacktrace();
+    crashMsg += QString("\n\n") + getStacktrace();
+
     CrashHandler* crsh = new CrashHandler(crashMsg);
     crsh->exec();
-#else
-    QMessageBox::warning(nullptr, QApplication::tr("Crash"), crashMsg);
-#endif
+
     exit(EXIT_FAILURE);
 }
 
@@ -118,37 +161,4 @@ void CrashHandler::on_pgeForumButton_clicked()
 void CrashHandler::on_exitButton_clicked()
 {
     this->close();
-}
-
-QString CrashHandler::getStacktrace()
-{
-#ifdef _WIN32
-    unsigned int   i;
-    void         * stack[ 100 ];
-    unsigned short frames;
-    SYMBOL_INFO  * symbol;
-    HANDLE         process;
-
-    process = GetCurrentProcess();
-
-    SymInitialize( process, NULL, TRUE );
-
-    frames               = CaptureStackBackTrace( 0, 100, stack, NULL );
-    symbol               = ( SYMBOL_INFO * )calloc( sizeof( SYMBOL_INFO ) + 256 * sizeof( char ), 1 );
-    symbol->MaxNameLen   = 255;
-    symbol->SizeOfStruct = sizeof( SYMBOL_INFO );
-
-    QString bkTrace("");
-    for( i = 0; i < frames; i++ )
-    {
-        char buf[500];
-        SymFromAddr( process, ( DWORD64 )( stack[ i ] ), 0, symbol );
-
-        sprintf(buf, "%i: %s - 0x%0llx\n", frames - i - 1, symbol->Name, symbol->Address );
-        bkTrace += buf;
-    }
-    return bkTrace;
-#else
-    return QString("");
-#endif
 }
