423996903b8fed9fb23de3f70b74c30702a67b7f
Wohlstand <admin@wohlnet.ru>
2014-11-12 01:24:55 +0300

    Added flip functions for SDL_Surface

--- Engine/common_features/graphics_funcs.cpp
+++ Engine/common_features/graphics_funcs.cpp
@@ -206,3 +206,109 @@ SDL_Surface* GraphicsHelps::QImage_toSDLSurface(const QImage &sourceImage)
         image.width(), image.height(), image.depth(), image.bytesPerLine(),
         rmask, gmask, bmask, amask);
 }
+
+void GraphicsHelps::flipHorizontally( SDL_Surface*& image )
+{
+    // create a copy of the image
+    SDL_Surface* flipped_image = SDL_CreateRGBSurface( SDL_SWSURFACE, image->w, image->h, image->format->BitsPerPixel,
+        image->format->Rmask, image->format->Gmask, image->format->Bmask, image->format->Amask );
+
+    // loop through pixels
+    for( int y=0; y<image->h; y++ )
+    {
+        for( int x=0; x<image->w; x++ )
+        {
+            // copy pixels, but reverse the x pixels!
+            putpixel( flipped_image, x, y, getpixel(image, image->w - x - 1, y) );
+        }
+    }
+
+    // free original and assign flipped to it
+    SDL_FreeSurface( image );
+    image = flipped_image;
+}
+
+void GraphicsHelps::flipVertically( SDL_Surface*& image )
+{
+    // create a copy of the image
+    SDL_Surface* flipped_image = SDL_CreateRGBSurface( SDL_SWSURFACE, image->w, image->h, image->format->BitsPerPixel,
+        image->format->Rmask, image->format->Gmask, image->format->Bmask, image->format->Amask );
+
+    // loop through pixels
+    for( int y=0; y<image->h; y++ )
+    {
+        for( int x=0; x<image->w; x++ )
+        {
+            // copy pixels, but reverse the x pixels!
+            putpixel( flipped_image, x, y, getpixel(image, x, image->h - y-1) );
+        }
+    }
+
+    // free original and assign flipped to it
+    SDL_FreeSurface( image );
+    image = flipped_image;
+}
+
+Uint32 GraphicsHelps::getpixel(SDL_Surface *surface, int x, int y)
+{
+    int bpp = surface->format->BytesPerPixel;
+    /* Here p is the address to the pixel we want to retrieve */
+    Uint8 *p = (Uint8 *)surface->pixels + y * surface->pitch + x * bpp;
+
+    switch(bpp) {
+    case 1:
+        return *p;
+        break;
+
+    case 2:
+        return *(Uint16 *)p;
+        break;
+
+    case 3:
+        if(SDL_BYTEORDER == SDL_BIG_ENDIAN)
+            return p[0] << 16 | p[1] << 8 | p[2];
+        else
+            return p[0] | p[1] << 8 | p[2] << 16;
+        break;
+
+    case 4:
+        return *(Uint32 *)p;
+        break;
+
+    default:
+        return 0;       /* shouldn't happen, but avoids warnings */
+    }
+}
+
+void GraphicsHelps::putpixel(SDL_Surface *surface, int x, int y, Uint32 pixel)
+{
+    int bpp = surface->format->BytesPerPixel;
+    /* Here p is the address to the pixel we want to set */
+    Uint8 *p = (Uint8 *)surface->pixels + y * surface->pitch + x * bpp;
+
+    switch(bpp) {
+    case 1:
+        *p = pixel;
+        break;
+
+    case 2:
+        *(Uint16 *)p = pixel;
+        break;
+
+    case 3:
+        if(SDL_BYTEORDER == SDL_BIG_ENDIAN) {
+            p[0] = (pixel >> 16) & 0xff;
+            p[1] = (pixel >> 8) & 0xff;
+            p[2] = pixel & 0xff;
+        } else {
+            p[0] = pixel & 0xff;
+            p[1] = (pixel >> 8) & 0xff;
+            p[2] = (pixel >> 16) & 0xff;
+        }
+        break;
+
+    case 4:
+        *(Uint32 *)p = pixel;
+        break;
+    }
+}
