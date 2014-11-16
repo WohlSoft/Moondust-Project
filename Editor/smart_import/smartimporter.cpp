c97d3eb5f118d5586ae015c165af4559718b5eae
KevinW1998 <kevin.waldock@gmail.com>
2014-11-16 11:43:56 +0100

    Add SmartImport class template

---
+++ Editor/smart_import/smartimporter.cpp
@@ -0,0 +1,25 @@
+#include "smartimporter.h"
+
+SmartImporter::SmartImporter(QObject *parent) :
+    QObject(parent)
+{}
+
+SmartImporter::SmartImporter(QString importPath, QObject *parent)
+{
+    this->importPath = importPath;
+}
+
+bool SmartImporter::attemptFastImport()
+{
+    return false;
+}
+QString SmartImporter::getImportPath() const
+{
+    return importPath;
+}
+
+void SmartImporter::setImportPath(const QString &value)
+{
+    importPath = value;
+}
+
