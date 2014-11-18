1b840450b1446596c78e9acf229ec39e7e9771e2
Wohlstand <admin@wohlnet.ru>
2014-11-18 14:16:29 +0300

    Added entries into changelog

--- pge_version.h
+++ pge_version.h
@@ -20,13 +20,13 @@
 #define GLOBAL_PGE_VERSION
 
 //Version of whole project
-#define _VERSION "0.2.1"
+#define _VERSION "0.2.2"
 #define _RELEASE " Alpha" //Developing state (for release this field must be empty)
 
 #define _VP1 0
 #define _VP2 2
 #define _VP3 0
-#define _VP4 1
+#define _VP4 2
 
 
 #define _COPYRIGHT "2014 by Wohlstand"
