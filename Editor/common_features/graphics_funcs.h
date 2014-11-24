85c76808f6a810c9ba5db66307f93138a4f668a4
Wohlstand <admin@wohlnet.ru>
2014-11-24 04:34:00 +0300

    Added "Enable GFX fix" flag into View menu This flag enabling alternate alpha-blending algorithm which can fix render of lazily-made graphics.

--- Editor/common_features/graphics_funcs.h
+++ Editor/common_features/graphics_funcs.h
@@ -27,6 +27,9 @@ class GraphicsHelps
 {
 public:
     static QPixmap setAlphaMask(QPixmap image, QPixmap mask);
+    static QImage setAlphaMask_VB(QImage image, QImage mask);
+    static bool EnableVBEmulate;
+
     static QImage fromBMP(QString& file);
     static QPixmap loadPixmap(QString file);
     static QImage loadQImage(QString file);
