8f8547b1f8e575338fe6f5662b595d8d1c41b3b0
Wohlstand <admin@wohlnet.ru>
2014-12-11 17:45:53 +0300

    Added new folder into copypasting script

--- _Misc/dev_scripts/_copydata.bat
+++ _Misc/dev_scripts/_copydata.bat
@@ -162,6 +162,7 @@ call :ApplyDir editing\_dialogs
 call :ApplyDir editing\_scenes
 call :ApplyDir editing\_scenes\level
 call :ApplyDir editing\_scenes\level\edit_modes
+call :ApplyDir editing\_scenes\level\items
 call :ApplyDir editing\_scenes\world
 call :ApplyDir editing\_scenes\world\edit_modes
 call :ApplyDir editing\edit_level
