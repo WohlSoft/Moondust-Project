3283806d513a1e428fc6428e87443f8e3ee285d7
KevinW1998 <kevin.waldock@gmail.com>
2014-12-12 17:41:05 +0100

    Added History for flip and rotate

--- Editor/defines.h
+++ Editor/defines.h
@@ -5,16 +5,20 @@
 class ItemTypes
 {
 public:
+    //Multiselectable
     enum itemTypes
     {
-        LVL_Block   = 0,
-        LVL_BGO,
-        LVL_NPC,
-        WLD_Tile,
-        WLD_Scenery,
-        WLD_Path,
-        WLD_Level,
-        WLD_MusicBox
+        LVL_Block       = 1<<0,
+        LVL_BGO         = 1<<1,
+        LVL_NPC         = 1<<2,
+        LVL_PhysEnv     = 1<<3,
+        LVL_Door        = 1<<4,
+        LVL_Player      = 1<<5,
+        WLD_Tile        = 1<<6,
+        WLD_Scenery     = 1<<7,
+        WLD_Path        = 1<<8,
+        WLD_Level       = 1<<9,
+        WLD_MusicBox    = 1<<10
     };
 };
 
