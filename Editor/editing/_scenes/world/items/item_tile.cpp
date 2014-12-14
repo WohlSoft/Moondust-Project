74312e224c5f1eb9845459ebe3c4337ffae1f802
Wohlstand <admin@wohlnet.ru>
2014-12-14 03:01:04 +0300

    Replacement of numbers with their #define-s
    This will make source more readable

--- Editor/editing/_scenes/world/items/item_tile.cpp
+++ Editor/editing/_scenes/world/items/item_tile.cpp
@@ -284,9 +284,8 @@ void ItemTile::setAnimator(long aniID)
                 scene->animates_Tiles[aniID]->image().height()
                 );
 
-    this->setData(9, QString::number(qRound(imageSize.width())) ); //width
-    this->setData(10, QString::number(qRound(imageSize.height())) ); //height
-
+    this->setData(ITEM_WIDTH, QString::number(qRound(imageSize.width())) ); //width
+    this->setData(ITEM_HEIGHT, QString::number(qRound(imageSize.height())) ); //height
     //WriteToLog(QtDebugMsg, QString("Tile Animator ID: %1").arg(aniID));
 
     animatorID = aniID;
