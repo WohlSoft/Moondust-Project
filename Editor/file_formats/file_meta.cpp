75455ba3932ee1f334fc4353e03d24b97d0238ed
Wohlstand <admin@wohlnet.ru>
2014-12-21 16:31:13 +0300

    Experimental implemention of new PGE-X parser

--- Editor/file_formats/file_meta.cpp
+++ Editor/file_formats/file_meta.cpp
@@ -20,13 +20,7 @@
 
 MetaData FileFormats::ReadNonSMBX64MetaData(QString RawData, QString filePath)
 {
-
-    FileStringList in;
-    in.addData( RawData );
-
     int str_count=0;        //Line Counter
-    int i;                  //counters
-    //int file_format=0;        //File format number
     QString line;           //Current Line data
 
 
@@ -35,117 +29,91 @@ MetaData FileFormats::ReadNonSMBX64MetaData(QString RawData, QString filePath)
 
     QString errorString;
 
-    typedef QPair<QString, QStringList> PGEXSct;
-    PGEXSct PGEXsection;
-
-    QList<PGEXSct > PGEXTree;
-
     ///////////////////////////////////////Begin file///////////////////////////////////////
-    //Read Sections
-    bool sectionOpened=false;
+    PGEFile pgeX_Data(RawData);
+    pgeX_Data.setRawData( RawData );
 
-    //Read PGE-X Tree
-    while(!in.atEnd())
+    if( !pgeX_Data.buildTreeFromRaw() )
     {
-        PGEXsection.first = in.readLine();
-        PGEXsection.second.clear();
-
-        if(QString(PGEXsection.first).remove(' ').isEmpty()) continue; //Skip empty strings
-
-        sectionOpened=true;
-        QString data;
-        while(!in.atEnd())
-        {
-            data = in.readLine();
-            if(data==PGEXsection.first+"_END") {sectionOpened=false; break;} // Close Section
-            PGEXsection.second.push_back(data);
-        }
-        PGEXTree.push_back(PGEXsection);
-
-        // WriteToLog(QtDebugMsg, QString("Section %1, lines %2, %3")
-        //        .arg(PGEXsection.first)
-        //        .arg(PGEXsection.second.size())
-        //        .arg(sectionOpened?"opened":"closed")
-        //        );
-    }
-
-    if(sectionOpened)
-    {
-        errorString=QString("Section [%1] is not closed").arg(PGEXsection.first);
+        errorString = pgeX_Data.lastError();
         goto badfile;
     }
 
-    foreach(PGEXSct sct, PGEXTree) //look sections
+    for(int section=0; section<pgeX_Data.dataTree.size(); section++) //look sections
     {
-
-        //WriteToLog(QtDebugMsg, QString("Section %1")
-        //          .arg(sct.first) );
-            bool good;
-            for(i=0; i<sct.second.size();i++) //Look Entries
+        if(pgeX_Data.dataTree[section].name=="JOKES")
+        {
+            #ifndef PGE_ENGINE
+            if(!pgeX_Data.dataTree[section].data.isEmpty())
+                if(!pgeX_Data.dataTree[section].data[0].values.isEmpty())
+                    QMessageBox::information(nullptr, "Jokes",
+                            pgeX_Data.dataTree[section].data[0].values[0].value,
+                            QMessageBox::Ok);
+            #endif
+        }
+        else
+        if(pgeX_Data.dataTree[section].name=="META_BOOKMARKS")
+        {
+            if(pgeX_Data.dataTree[section].type!=PGEFile::PGEX_Struct)
             {
-                if(QString(sct.second[i]).remove(' ').isEmpty()) continue; //Skip empty strings
+                errorString=QString("Wrong data syntax, section [%1]").arg(pgeX_Data.dataTree[section].name);
+                goto badfile;
+            }
 
-                if(sct.first=="JOKES")
+            for(int sdata=0;sdata<pgeX_Data.dataTree[section].data.size();sdata++)
+            {
+                if(pgeX_Data.dataTree[section].data[sdata].type!=PGEFile::PGEX_Struct)
                 {
-                    #ifndef PGE_ENGINE
-                    QMessageBox::information(nullptr, "Jokes", sct.second[i], QMessageBox::Ok);
-                    #endif
-                    continue;
-                }
-
-                QList<QStringList > sectData = PGEFile::splitDataLine(sct.second[i], &good);
-                line = sct.second[i];
-
-                if(!good)
-                {
-                    errorString=QString("Wrong data string format [%1]").arg(sct.second[i]);
+                    errorString=QString("Wrong data syntax, section [%1], data line %2")
+                            .arg(pgeX_Data.dataTree[section].name)
+                            .arg(sdata);
                     goto badfile;
                 }
-                errorString=QString("Wrong value data type");
 
+                PGEFile::PGEX_Item x = pgeX_Data.dataTree[section].data[sdata];
 
-                //Scan values
-                if(sct.first=="META_BOOKMARKS") // Bookmarks
+                Bookmark meta_bookmark;
+                meta_bookmark.bookmarkName = "";
+                meta_bookmark.x = 0;
+                meta_bookmark.y = 0;
+
+                for(int sval=0;sval<x.values.size();sval++) //Look markers and values
                 {
-                    Bookmark meta_bookmark;
-                    meta_bookmark.bookmarkName = "";
-                    meta_bookmark.x = 0;
-                    meta_bookmark.y = 0;
+                    PGEFile::PGEX_Val v = x.values[sval];
+                    errorString=QString("Wrong data syntax, section [%1], data line %2, marker %3")
+                            .arg(pgeX_Data.dataTree[section].name)
+                            .arg(sdata)
+                            .arg(sval);
 
-                    foreach(QStringList value, sectData) //Look markers and values
-                    {
-                          if(value[0]=="BM") //Bookmark name
-                          {
-                              if(PGEFile::IsQStr(value[1]))
-                                  meta_bookmark.bookmarkName = PGEFile::X2STR(value[1]);
-                              else
-                                  goto badfile;
-                          }
+                      if(v.marker=="BM") //Bookmark name
+                      {
+                          if(PGEFile::IsQStr(v.value))
+                              meta_bookmark.bookmarkName = PGEFile::X2STR(v.value);
                           else
-                          if(value[0]=="X") // Position X
-                          {
-                              if(PGEFile::IsIntS(value[1]))
-                                  meta_bookmark.x = value[1].toInt();
-                              else
-                                  goto badfile;
-                          }
+                              goto badfile;
+                      }
+                      else
+                      if(v.marker=="X") // Position X
+                      {
+                          if(PGEFile::IsIntS(v.value))
+                              meta_bookmark.x = v.value.toInt();
                           else
-                          if(value[0]=="Y") //Position Y
-                          {
-                              if(PGEFile::IsIntS(value[1]))
-                                  meta_bookmark.y = value[1].toInt();
-                              else
-                                  goto badfile;
-                          }
-                    }
-
-                    FileData.bookmarks.push_back(meta_bookmark);
-                }//Bookmarks
-
+                              goto badfile;
+                      }
+                      else
+                      if(v.marker=="Y") //Position Y
+                      {
+                          if(PGEFile::IsIntS(v.value))
+                              meta_bookmark.y = v.value.toInt();
+                          else
+                              goto badfile;
+                      }
+                }
+                FileData.bookmarks.push_back(meta_bookmark);
             }
+        }
     }
-
-
+    errorString="";
     ///////////////////////////////////////EndFile///////////////////////////////////////
 
     return FileData;
