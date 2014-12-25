7c0a38e7e3dcf438cd656f3b86057c6367ed0ba3
Wohlstand <admin@wohlnet.ru>
2014-12-25 05:51:11 +0300

    More works with custom counters system

--- Editor/main_window/dock/debugger.cpp
+++ Editor/main_window/dock/debugger.cpp
@@ -16,8 +16,15 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <QSettings>
+
+#include <common_features/app_path.h>
+#include <common_features/util.h>
+#include <tools/debugger/custom_counter_gui.h>
+
 #include "../../mainwindow.h"
 #include <ui_mainwindow.h>
+
 
 void MainWindow::on_debuggerBox_visibilityChanged(bool visible)
 {
@@ -78,16 +85,64 @@ void MainWindow::on_DEBUG_GotoPoint_clicked()
     ui->DEBUG_GotoY->setText("");
 }
 
+namespace mainwindow_debugger_box
+{
+    QVector<CustomCounter > customCounters;
+    bool isLoaded=false;
+}
 
 
 void MainWindow::Debugger_loadCustomCounters()
 {
+    using namespace mainwindow_debugger_box;
+    QSettings cCounters(ApplicationPath+"/pge_editor.ini", QSettings::IniFormat);
+    cCounters.setIniCodec("UTF-8");
 
+    cCounters.beginGroup("custom-counters");
+        int cntn = cCounters.value("total", 0).toInt();
+    cCounters.endGroup();
+
+    customCounters.clear();
+
+    for(;cntn>0;cntn--)
+    {
+        bool valid=true;
+        CustomCounter counter;
+        QStringList items;
+        cCounters.beginGroup(QString("custom-counter-%1").arg(cntn));
+            counter.name = cCounters.value("name", "").toString();
+                if(!counter.name.isEmpty()) valid=false;
+            counter.type = (ItemTypes::itemTypes)cCounters.value("type", 0).toInt();
+            counter.windowType = cCounters.value("window-type", 0).toInt();
+            QString tmp = cCounters.value("items", "").toString();
+                if(!tmp.isEmpty()) {valid=false; goto skip;}
+                items = tmp.split(',');
+                foreach(QString x, items)
+                {
+                    counter.items.push_back(x.toLong());
+                }
+                skip:
+        cCounters.endGroup();
+        if(valid) customCounters.push_back(counter);
+    }
 }
 
 
 void MainWindow::Debugger_saveCustomCounters()
 {
+    using namespace mainwindow_debugger_box;
+    int win = activeChildWindow();
+
+    util::memclear(ui->DEBUG_CustomCountersList);
+    for(int i=0;i<customCounters.size();i++)
+    {
+        if(customCounters[i].windowType!=win) continue;
+        QListWidgetItem *item;
+        item = new QListWidgetItem(ui->DEBUG_CustomCountersList);
+        item->setData(Qt::UserRole, QVariant(i));
+        item->setText(QString("%1\t%2").arg(0).arg(customCounters[i].name));
+        ui->DEBUG_CustomCountersList->addItem(item);
+    }
 
 }
 
@@ -95,24 +150,40 @@ void MainWindow::Debugger_saveCustomCounters()
 
 void MainWindow::on_DEBUG_AddCustomCounter_clicked()
 {
+    using namespace mainwindow_debugger_box;
+    CustomCounterGUI customCounterGui;
+    customCounterGui.setWindowFlags (Qt::Window | Qt::WindowTitleHint | Qt::WindowCloseButtonHint);
+    customCounterGui.setGeometry(QStyle::alignedRect(Qt::LeftToRight, Qt::AlignCenter, customCounterGui.size(), qApp->desktop()->availableGeometry()));
 
+    if(customCounterGui.exec()==QDialog::Accepted)
+    {
+        qApp->setActiveWindow(this);
+        customCounterGui.counterData.windowType = activeChildWindow();
+        customCounters.push_back(customCounterGui.counterData);
+        on_DEBUG_RefreshCoutners_clicked();
+    }
 }
 
 
 void MainWindow::on_DEBUG_RefreshCoutners_clicked()
 {
+    using namespace mainwindow_debugger_box;
+    if(!isLoaded)
+        Debugger_loadCustomCounters();
 
 }
 
 
 void MainWindow::on_DEBUG_CustomCountersList_itemClicked(QListWidgetItem *item)
 {
+    using namespace mainwindow_debugger_box;
 
 }
 
 
 void MainWindow::on_DEBUG_CustomCountersList_customContextMenuRequested(const QPoint &pos)
 {
+    using namespace mainwindow_debugger_box;
 
 }
 
