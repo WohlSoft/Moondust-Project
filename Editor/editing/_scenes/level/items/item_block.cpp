46350aca1bb110f2df1f2c7b926bbedf6ebaec6e
Wohlstand <admin@wohlnet.ru>
2014-12-27 02:32:44 +0300

    Don't collide NPC's with triangle blocks

--- Editor/editing/_scenes/level/items/item_block.cpp
+++ Editor/editing/_scenes/level/items/item_block.cpp
@@ -567,6 +567,7 @@ void ItemBlock::setBlockData(LevelBlock inD, obj_block *mergedSet, long *animato
 
             setData(ITEM_BLOCK_IS_SIZABLE, "standart" );
         }
+        setData(ITEM_BLOCK_SHAPE, localProps.phys_shape);
     }
 
     if(animator)
