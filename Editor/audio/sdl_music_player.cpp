dc1e627ce500fbe69ebe5b276dfeb00025cc71bb
Wohlstand <admin@wohlnet.ru>
2014-12-29 23:23:47 +0300

    Tiny fix

--- Editor/audio/sdl_music_player.cpp
+++ Editor/audio/sdl_music_player.cpp
@@ -102,6 +102,7 @@ void PGE_MusPlayer::setSampleRate(int sampleRate=44100)
     sRate=sampleRate;
     Mix_CloseAudio();
     Mix_OpenAudio(sRate, AUDIO_S16, 2, 4096);
+    Mix_AllocateChannels(16);
 }
 int PGE_MusPlayer::sampleRate()
 {
