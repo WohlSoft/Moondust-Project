b198378c0af35f0a87969c39f8fab4f98ba9e92e
KevinW1998 <kevin.waldock@gmail.com>
2015-01-18 10:06:15 +0100

    First working History class :D

--- Editor/editing/_components/history/historyelementmodification.h
+++ Editor/editing/_components/history/historyelementmodification.h
@@ -12,29 +12,55 @@ class HistoryElementModification : public QObject, public IHistoryElement
 
 public:
     explicit HistoryElementModification(const LevelData &oldData, const LevelData &newData, QObject *parent = 0);
+    explicit HistoryElementModification(const WorldData &oldData, const WorldData &newData, QObject *parent = 0);
     virtual ~HistoryElementModification();
     virtual QString getHistoryName();
     virtual void undo();
     virtual void redo();
 
 
-    LevelData oldData() const;
-    void setOldData(const LevelData &oldData);
-
-    LevelData newData() const;
-    void setNewData(const LevelData &newData);
+    void processReplacement(const LevelData &toRemoveData, const LevelData &toPlaceData);
 
     QString customHistoryName() const;
     void setCustomHistoryName(const QString &customHistoryName);
+
+    LevelData oldLvlData() const;
+    void setOldLvlData(const LevelData &oldLvlData);
+
+    LevelData newLvlData() const;
+    void setNewLvlData(const LevelData &newLvlData);
+
+    WorldData oldWldData() const;
+    void setOldWldData(const WorldData &oldWldData);
+
+    WorldData newWldData() const;
+    void setNewWldData(const WorldData &newWldData);
 
 signals:
 
 public slots:
 
+private slots:
+    void processBlock(const LevelBlock &sourceBlock, QGraphicsItem* item);
+    void processBGO(const LevelBGO &sourceBGO, QGraphicsItem* item);
+    void processNPC(const LevelNPC &sourceNPC, QGraphicsItem* item);
+    void processPhysEnv(const LevelPhysEnv &sourcePhysEnv, QGraphicsItem* item);
+    void processDoor(const LevelDoors &sourceDoor, QGraphicsItem* item);
+    void processPlayerPoint(const PlayerPoint &sourcePayerPoint, QGraphicsItem* item);
+
+    //Overworld
+    void processTile(const WorldTiles &sourceTile, QGraphicsItem* item);
+    void processScenery(const WorldScenery &sourceScenery, QGraphicsItem* item);
+    void processPath(const WorldPaths &sourcePath, QGraphicsItem* item);
+    void processLevel(const WorldLevels &sourceLevel, QGraphicsItem* item);
+    void processMusicbox(const WorldMusic &sourceMusic, QGraphicsItem* item);
+
 
 private:
-    LevelData m_oldData;
-    LevelData m_newData;
+    LevelData m_oldLvlData;
+    LevelData m_newLvlData;
+    WorldData m_oldWldData;
+    WorldData m_newWldData;
     QString m_customHistoryName;
 };
 
