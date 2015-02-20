49c50e4c07904a5e3e32681e8a505386afbe895c
Wohlstand <admin@wohlnet.ru>
2015-02-21 01:16:48 +0400

    OOps Forgot to fix same bug also for NPC.txt writer :P

--- Editor/editing/edit_npc/npctxt_files_io.cpp
+++ Editor/editing/edit_npc/npctxt_files_io.cpp
@@ -113,7 +113,7 @@ bool NpcEdit::saveAs()
 bool NpcEdit::saveFile(const QString &fileName, const bool addToRecent)
 {
     QFile file(fileName);
-    if (!file.open(QFile::WriteOnly | QFile::Text)) {
+    if (!file.open(QFile::WriteOnly)) {
         QMessageBox::warning(this, tr("File save error"),
                              tr("Cannot save file %1:\n%2.")
                              .arg(fileName)
