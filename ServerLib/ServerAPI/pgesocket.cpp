6be9dda355b400943eecd3eacd72be69b2e854ce
KevinW1998 <kevin.waldock@gmail.com>
2015-04-12 22:36:58 +0200

    WIP more constructors and functions for ServerAPI

--- ServerLib/ServerAPI/pgesocket.cpp
+++ ServerLib/ServerAPI/pgesocket.cpp
@@ -2,14 +2,30 @@
 
 
 
-PGESocket::PGESocket(QObject *parent) :
-    QThread(parent)
-{}
+PGESocket::PGESocket(QObject *parent) : PGESocket(PGESocketType::PGEServer, parent) {}
+PGESocket::PGESocket(PGESocket::PGESocketType type, QObject *parent) :
+    QThread(parent),
+    m_socketType(type),
+    m_server(0),
+    m_socket(0)
+{
+    init();
+}
 
-PGESocket::PGESocket(const QMap<PGEPackets, QMetaObject *> &toRegisterPackets, QObject *parent) :
-    QThread(parent)
+PGESocket::PGESocket(const QMap<PGEPackets, QMetaObject *> &toRegisterPackets, QObject *parent) : PGESocket(toRegisterPackets, PGESocketType::PGEServer, parent) {}
+PGESocket::PGESocket(const QMap<PGEPackets, QMetaObject *> &toRegisterPackets, PGESocket::PGESocketType type, QObject *parent) :
+    QThread(parent),
+    m_socketType(type),
+    m_server(0),
+    m_socket(0)
 {
     registerPackets(toRegisterPackets);
+    init();
+}
+
+void PGESocket::init()
+{
+    //Here the init code
 }
 
 void PGESocket::registerPackets(const QMap<PGEPackets, QMetaObject *> &toRegisterPackets)
@@ -21,5 +37,39 @@ void PGESocket::registerPackets(const QMap<PGEPackets, QMetaObject *> &toRegiste
 
 void PGESocket::registerPacket(QMetaObject *packetMetaObject, PGEPackets packetType)
 {
-    m_registeredPackets[packetType] = packetMetaObject;
+    if(isBaseOfPacket(packetMetaObject))
+        m_registeredPackets[packetType] = packetMetaObject;
 }
+
+void PGESocket::run()
+{
+
+}
+
+
+
+void PGESocket::dispatchPacket(Packet *packet)
+{
+    QString packetData = packet->serializePacket();
+
+
+}
+
+void PGESocket::execAllPacket()
+{
+
+}
+
+bool PGESocket::isBaseOfPacket(QMetaObject *possiblePacket)
+{
+    const QMetaObject* superObj = possiblePacket;
+    while((superObj = superObj->superClass())){
+        if(QString(superObj->className()) == "Packet"){
+            return true;
+        }
+    }
+
+    qWarning() << possiblePacket->className() << " doesn't belong to the Packet-Class!";
+    return false;
+}
+
