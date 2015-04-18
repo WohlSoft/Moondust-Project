8c1703a16c8a0e71e15f30b4c7152a6d7e7bf1f0
Vitaly Novichkov <Wohlstand@users.noreply.github.com>
2015-04-18 08:58:34 +0100

    Update generate_paths_travis.sh

--- generate_paths_travis.sh
+++ generate_paths_travis.sh
@@ -6,7 +6,7 @@ echo "#=========================================================================
 echo "QMAKESPEC=/home/travis/Qt/Qt-5.4_static/mkspecs/linux-g++" >> _paths.sh
 echo "QT_INSTALL_PREFIX=/home/travis/Qt/Qt-5.4_static/" >> _paths.sh
 echo "QT_INSTALL_DATA=/home/travis/Qt/Qt-5.4_static/" >> _paths.sh
-echo "export QMAKESPEC" >> _paths.sh
+echo "export QMAKESPEC QT_INSTALL_PREFIX QT_INSTALL_DATA" >> _paths.sh
 echo "QMake=\"/home/travis/Qt/Qt-5.4_static/bin/qmake\"; #" >> _paths.sh
 echo "" >> _paths.sh
 echo "LRelease=\"/home/travis/Qt/Qt-5.4_static/bin/lrelease\"; #" >> _paths.sh
