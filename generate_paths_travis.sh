8a02c3bece1cc84d8ae5e449df77d85573644dd5
Vitaly Novichkov <Wohlstand@users.noreply.github.com>
2015-04-18 08:31:36 +0100

    Update generate_paths_travis.sh

--- generate_paths_travis.sh
+++ generate_paths_travis.sh
@@ -3,6 +3,8 @@ cd $PWD
 echo "#===============================================================================================" > _paths.sh
 echo "#=================PLEASE SET UP THE ABLSOLUTE PATHS TO QMAKE AND TO LRELEASE====================" >> _paths.sh
 echo "#===============================================================================================" >> _paths.sh
+echo "QMAKESPEC=/home/travis/Qt/Qt-5.4_static/mkspecs/linux-g++" >> _paths.sh
+echo "export QMAKESPEC" >> _paths.sh
 echo "QMake=\"/home/travis/Qt/Qt-5.4_static/bin/qmake\"; #" >> _paths.sh
 echo "" >> _paths.sh
 echo "LRelease=\"/home/travis/Qt/Qt-5.4_static/bin/lrelease\"; #" >> _paths.sh
