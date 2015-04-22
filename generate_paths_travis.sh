4eb2a6cb960433febaa283ee8b6d198f2575a46f
Vitaly Novichkov <Wohlstand@users.noreply.github.com>
2015-04-22 10:10:40 +0100

    Update generate_paths_travis.sh

--- generate_paths_travis.sh
+++ generate_paths_travis.sh
@@ -3,8 +3,8 @@ cd $PWD
 echo "#===============================================================================================" > _paths.sh
 echo "#=================PLEASE SET UP THE ABLSOLUTE PATHS TO QMAKE AND TO LRELEASE====================" >> _paths.sh
 echo "#===============================================================================================" >> _paths.sh
-#    QMAKE_SPEC - the shortname of the host mkspec that is resolved and stored in the QMAKESPEC variable during a host build
-#echo "QMAKESPEC=/home/travis/Qt/Qt-5.4_static/mkspecs/linux-g++" >> _paths.sh
+    QMAKE_SPEC - the shortname of the host mkspec that is resolved and stored in the QMAKESPEC variable during a host build
+echo "QMAKESPEC=/home/travis/Qt/Qt-5.4_static/mkspecs/linux-g++" >> _paths.sh
 #/home/travis/Qt/Qt-5.4_static/bin/qmake -set QMAKESPEC /home/travis/Qt/Qt-5.4_static/mkspecs/linux-g++
 #    QT_INSTALL_PREFIX - default prefix for all paths
 echo "QT_INSTALL_PREFIX=/home/travis/Qt/Qt-5.4_static/" >> _paths.sh
@@ -20,10 +20,10 @@ echo "QT_HOST_BINS=/home/travis/Qt/Qt-5.4_static/bin" >> _paths.sh
 echo "QT_HOST_DATA=/home/travis/Qt/Qt-5.4_static" >> _paths.sh
 /home/travis/Qt/Qt-5.4_static/bin/qmake -set QT_HOST_DATA /home/travis/Qt/Qt-5.4_static
 #    QT_INSTALL_CONFIGURATION - location for Qt settings. Not applicable on Windows
-echo "QMAKESPEC=$PWD/_Libs/qmake.conf" >> _paths.sh
-echo "QMAKESPEC=$PWD/_Libs/qmake.conf"
+echo "QT_INSTALL_CONFIGURATION=/home/travis/Qt/Qt-5.4_static/bin" >> _paths.sh
+cp $PWD/_Libs/qmake.conf /home/travis/Qt/Qt-5.4_static/bin
 
-echo "export QT_INSTALL_CONFIGURATION" >> _paths.sh
+echo "export QMAKESPEC QT_INSTALL_CONFIGURATION QT_INSTALL_PREFIX" >> _paths.sh
 
 echo "QMake=\"/home/travis/Qt/Qt-5.4_static/bin/qmake\"; #" >> _paths.sh
 echo "" >> _paths.sh
