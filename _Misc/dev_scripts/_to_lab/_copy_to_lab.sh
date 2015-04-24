4704ef610fe2e9a90403e5c4360e38023e9368e3
Wohlstand <admin@wohlnet.ru>
2015-04-24 02:22:33 +0100

    Small fix of script

--- _Misc/dev_scripts/_to_lab/_copy_to_lab.sh
+++ _Misc/dev_scripts/_to_lab/_copy_to_lab.sh
@@ -6,12 +6,15 @@ CurDir=~+
 
 echo "Copying data..."
 if [ -f "$SOURCEDIR/Editor/android-build/bin/QtApp-release-signed.apk" ]; then
+	echo "copy $SOURCEDIR/Editor/android-build/bin/QtApp-release-signed.apk..."
 	cp "$SOURCEDIR/Editor/android-build/bin/QtApp-release-signed.apk" "$SiteRootNFS/$LabDir/pge_editor_dev.apk"
 fi
 
-if [ -f "$SOURCEDIR/bin/_packed/*.tar.gz" ]; then
-	cp "$SOURCEDIR/bin/_packed/*.tar.gz" "$SiteRootNFS/$LabDir"
+if [ -f "$SOURCEDIR/bin/_packed/pge-project-dev-linux-mint.tar.gz" ]; then
+	echo "copy $SOURCEDIR/bin/_packed/pge-project-dev-linux-mint.tar.gz..."
+	cp "$SOURCEDIR/bin/_packed/pge-project-dev-linux-mint.tar.gz" "$SiteRootNFS/$LabDir"
 fi
+echo "Done!"
 
 if [ -f "$SiteRootNFS/$LabDir/send_to_gna.sh" ]; then
 	echo "Uploading everything to the GNA"
