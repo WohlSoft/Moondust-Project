de6b2a84e8de6a0db121fa8f150f544327617fe2
Wohlstand <admin@wohlnet.ru>
2015-05-09 23:01:21 +0100

    Goose replaced with duck

--- Engine/scenes/level/lvl_player.h
+++ Engine/scenes/level/lvl_player.h
@@ -144,33 +144,19 @@ class LVL_Player :
         /******************Attack*******************/
 
         /************************************************
-                  ,;MMMM..
-               ,;:MM"MMMMM.
-            ,;.MM::M.MMMMMM:
-            ""::.;'MMMMMMMMM
-                   "'""MMMMM;
-                       ':MMMM.
-                        'MMMM;
-                         :MMMM;.
-                          MMMMMM;...
-                          MMMMMMMMMMMMM;.;..
-                          MMMMMMMMMMMMMMMMMMM...
-                          MMMMMM:MMMMMMMMMMMMMMM;...       ..:
-                          MMMMMM;MMMMMMMMMMMMM:MMMMMMM:MMMM:M
-                          :MMMMMM:MMMMMMMMMMMMMMM.:::;:::;;:'
-                          ':MMMMMMM:MMMM:;MM:M;.MMM:';::M:'
-                           ':MMMMMM;M;;MM;::M;;::::;MM:""
-                             'MMMMMMMM;M;:::MMMMMMMMMM"
-                              ''MMMMMMMMMMMMMMMMMMMMM"
-                                 ':MMMMMMMMMMMMMMMM"'
-                                   '':MMMMMMMMMMM"'
-               -Duck-                 ':MMMMMM""'
-                                         .
-                                         :
-                                        ::
-                                   ,..;.M'
-                                  ,;;MM:'
-                                    '"'
+                                    __
+                                  /` ,\__
+                                 |    ).-'
+                                / .--'
+                               / /
+                 ,      _.==''`  \
+               .'(  _.='         |
+              {   ``  _.='       |
+               {    \`     ;    /
+                `.   `'=..'  .='
+                  `=._    .='
+               duck '-`\\`__
+                        `-._{
         ************************************************/
         bool duck_allow;
         bool ducking;
