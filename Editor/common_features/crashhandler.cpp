e4f8d58785e4d4eb2fde0ac7e2d8f002108d0b11
KevinW1998 <kevin.waldock@gmail.com>
2015-05-11 20:51:46 +0200

    Improve Crash Handler with StackWalker

--- Editor/common_features/crashhandler.cpp
+++ Editor/common_features/crashhandler.cpp
@@ -38,31 +38,9 @@
 QString CrashHandler::getStacktrace()
 {
     #ifdef _WIN32
-        unsigned int   i;
-        void         * stack[ 100 ];
-        unsigned short frames;
-        SYMBOL_INFO  * symbol;
-        HANDLE         process;
-
-        process = GetCurrentProcess();
-
-        SymInitialize( process, NULL, TRUE );
-
-        frames               = CaptureStackBackTrace( 0, 100, stack, NULL );
-        symbol               = ( SYMBOL_INFO * )calloc( sizeof( SYMBOL_INFO ) + 256 * sizeof( char ), 1 );
-        symbol->MaxNameLen   = 255;
-        symbol->SizeOfStruct = sizeof( SYMBOL_INFO );
-
-        QString bkTrace("");
-        for( i = 0; i < frames; i++ )
-        {
-            char buf[500];
-            SymFromAddr( process, ( DWORD64 )( stack[ i ] ), 0, symbol );
-
-            sprintf(buf, "%i: %s - 0x%0llx\n", frames - i - 1, symbol->Name, symbol->Address );
-            bkTrace += buf;
-        }
-        return bkTrace;
+        StackTracer tracer;
+        tracer.runStackTracerForAllThreads();
+        return tracer.theOutput;
     #elif (__linux__ && !(__ANDROID__))
         void *array[400];
         size_t size;
