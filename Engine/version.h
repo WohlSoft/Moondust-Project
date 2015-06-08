5356863a96a761daaaf5179d30632e620bd236a3
Wohlstand <admin@wohlnet.ru>
2015-06-08 06:33:17 +0100

    Engine 0.0.11

--- Engine/version.h
+++ Engine/version.h
@@ -23,13 +23,12 @@
 #define EDITOR_VERSION_H
 
 //Version of this program
-#define _FILE_VERSION "0.0.10.1"
+#define _FILE_VERSION "0.0.11"
 #define _FILE_RELEASE " pre-alpha"
 
 #define _VF1 0
 #define _VF2 0
-#define _VF3 10
-#define _VF4 1
+#define _VF3 11
 
 #define _FILE_DESC "Game Engine"
 
