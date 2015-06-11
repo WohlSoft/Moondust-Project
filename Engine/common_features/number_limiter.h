57bd66cfab81c4ad0c3f536b63cf96b4905e0517
Wohlstand <admin@wohlnet.ru>
2015-06-11 17:56:16 +0100

    Fix NumberLimiter

--- Engine/common_features/number_limiter.h
+++ Engine/common_features/number_limiter.h
@@ -27,8 +27,67 @@
 class NumberLimiter
 {
 public:
+
     template<typename T>
-    static void apply(T &value, T min = SHRT_MIN, T max = SHRT_MAX){
+    static T minVal()
+    {
+        if(std::is_same<T, char>::value)
+            return CHAR_MIN;
+        else if(std::is_same<T, unsigned char>::value)
+            return 0;
+        if(std::is_same<T, short>::value)
+            return SHRT_MIN;
+        else if(std::is_same<T, unsigned short>::value)
+            return 0;
+        if(std::is_same<T, int>::value)
+            return INT_MIN;
+        else if(std::is_same<T, unsigned int>::value)
+            return 0;
+        if(std::is_same<T, long>::value)
+            return LONG_MIN;
+        else if(std::is_same<T, unsigned long>::value)
+            return 0;
+        if(std::is_same<T, long long>::value)
+            return LLONG_MIN;
+        else if(std::is_same<T, unsigned long long>::value)
+            return 0;
+        else if(std::is_same<T, float>::value)
+            return FLT_MIN;
+        else if(std::is_same<T, double>::value)
+            return DBL_MIN;
+    }
+
+    template<typename T>
+    static T maxVal()
+    {
+        if(std::is_same<T, char>::value)
+            return CHAR_MAX;
+        else if(std::is_same<T, unsigned char>::value)
+            return UCHAR_MAX;
+        if(std::is_same<T, short>::value)
+            return SHRT_MAX;
+        else if(std::is_same<T, unsigned short>::value)
+            return USHRT_MAX;
+        if(std::is_same<T, int>::value)
+            return INT_MAX;
+        else if(std::is_same<T, unsigned int>::value)
+            return UINT_MAX;
+        if(std::is_same<T, long>::value)
+            return LONG_MAX;
+        else if(std::is_same<T, unsigned long>::value)
+            return ULONG_MAX;
+        if(std::is_same<T, long long>::value)
+            return LLONG_MIN;
+        else if(std::is_same<T, unsigned long long>::value)
+            return ULLONG_MAX;
+        else if(std::is_same<T, float>::value)
+            return FLT_MAX;
+        else if(std::is_same<T, double>::value)
+            return DBL_MAX;
+    }
+
+    template<typename T>
+    static void apply(T &value, T min = minVal<T>(), T max = maxVal<T>()){
         static_assert(std::is_arithmetic<T>::value, "The value for \"apply\" must be arithemtic");
         if(value<min)
             value=min;
@@ -37,7 +96,7 @@ public:
     }
 
     template<typename T>
-    static void applyD(T &value, T defvalue, T min=SHRT_MIN, T max=SHRT_MAX){
+    static void applyD(T &value, T min = minVal<T>(), T max = maxVal<T>()){
         static_assert(std::is_arithmetic<T>::value, "The value for \"applyD\" must be arithemtic");
         if((value<min)||(value>max))
         {
@@ -45,6 +104,7 @@ public:
             apply(value, min, max);
         }
     }
+
 
 };
 
