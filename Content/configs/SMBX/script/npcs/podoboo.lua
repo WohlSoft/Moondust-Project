dfb072ab984c332cb9faf8b6fbe5f40ced330b2e
KevinW1998 <kevin.waldock@gmail.com>
2015-06-19 17:28:51 +0200

    More detail and more propeties for podoboo.lua

--- Content/configs/SMBX/script/npcs/podoboo.lua
+++ Content/configs/SMBX/script/npcs/podoboo.lua
@@ -5,38 +5,56 @@ local AI_FALLING = 1
 local AI_IDLE = 2
 
 function initProps(thenpc)
+
     -- Animation properties
-    thenpc.animateJump = {0, 1}
-    thenpc.animateFall = {2, 3}
+    -- [[AI_JUMPING]] --
+    thenpc.animateJump = {0, 1}                     -- Animation set for jumping/flying up
+    -- [[AI_FALLING]] --
+    thenpc.animateFall = {2, 3}                     -- Animation set for falling
     thenpc.animation_flying = false
+    
     -- Sound
     thenpc.soundPlayed = false
+    
     -- Currents
-    thenpc.cur_jumpingUpTicks = 0          -- The tick counter from 0 to 30 when forcing
-    thenpc.cur_idleTicks = 0               -- The idle timer from 0 to 150
-    thenpc.cur_mode = AI_JUMPING           -- The current "AI"-Mode
-    thenpc.npc_obj.gravity = 0
-    thenpc.npc_obj.y = thenpc.startingY
-    thenpc.npc_obj:setSequence(thenpc.animateJump)
-    thenpc.animation_flying = true
+    -- [[AI_JUMPING]] --
+    thenpc.cur_jumpingUpTicks = 0                   -- The tick counter from 0 to 30 when forcing
+    -- [[AI_FALLING]] --
+    thenpc.cur_idleTicks = 0                        -- The idle timer from 0 to 150
+    -- [[AI_IDLE]] --
+    thenpc.npc_obj.gravity = 0                      -- The gravity of the npc object 
+    thenpc.npc_obj.y = thenpc.startingY             -- The starting position for AI_JUMPING
+    thenpc.npc_obj:setSequence(thenpc.animateJump)  -- The Animation for AI_JUMPING
+    thenpc.animation_flying = true                  -- True, if the "flying" animation should be drawn
+    
 
+    -- [[AI_JUMPING]] --
     thenpc.left_sparks = thenpc.def_sparks;
     thenpc.next_spark = thenpc.def_sparks_time;
     thenpc.spark_rand_offset = math.random(4);
+    
+    thenpc.cur_mode = AI_JUMPING           -- The current "AI"-Mode
 end
 
 function podoboo:__init(npc_obj)
     self.npc_obj = npc_obj
     -- Config
-    self.def_init_posY = self.npc_obj.y
-    self.def_sparks = 5
-    self.def_sparks_time = smbx_utils.ticksToTime(5)
+    self.def_init_posY = self.npc_obj.y                         -- The position of the podoboo (default is the one set on the item)
+    self.def_sparks = 5                                         -- The number of sparkling effects
+    self.def_sparks_time = smbx_utils.ticksToTime(5)            -- The time delay between every sparkling effect
 
-    self.def_jumpingUpTicks = smbx_utils.ticksToTime(30)         -- The ticks where podoboo def_jumpingUpSpeed is beeing forced. (jumping up)
-    self.def_idleTicks = smbx_utils.ticksToTime(150)             -- The ticks where podoboo is idleing
-    self.def_gravity = 0.73              -- The gravity which is used for the podoboo
-    self.def_jumpingUpSpeed = -6         -- The speed when the podoboo is jumping up.
-    self.startingY = self.def_init_posY+48.0   -- The starting y position, this is needed for detecting, when idleing is needed.
+    self.def_jumpingUpTicks = smbx_utils.ticksToTime(30)        -- The ticks where podoboo def_jumpingUpSpeed is beeing forced. (jumping up)
+    self.def_idleTicks = smbx_utils.ticksToTime(150)            -- The ticks where podoboo is idleing
+    self.def_gravity = 0.73                                     -- The gravity which is used for the podoboo
+    self.def_jumpingUpSpeed = -6                                -- The speed when the podoboo is jumping up.
+    self.startingY = self.def_init_posY+48.0                    -- The starting y position, this is needed for detecting, when idleing is needed.
+
+    -- Audio
+    self.def_podobooSoundID = 16
+    
+    -- Effect
+    self.def_podobooSplashEffectID = 13
+    self.def_sparkleEffectID = 12
 
     initProps(self)
 end
@@ -47,10 +65,6 @@ end
 
 function podoboo:onLoop(tickTime)
     local cur_npc = self.npc_obj
-    --Renderer.printText("podo ticks: "..tickTime.." left Up"..self.cur_jumpingUpTicks, 20, 440)
-    --Renderer.printText("podo y: "..self.startingY, 20, 440)
-    --Renderer.printText("podo cy: "..self.npc_obj.y, 20, 480)
-    --Renderer.printText("podo cy < y: "..tostring(self.startingY < self.npc_obj.y), 20, 520)
     if(self.cur_mode == AI_JUMPING)then
         -- Renderer.printText("AI_JUMPING", 20, 400)
         if(self.cur_jumpingUpTicks <= 0)then
@@ -58,8 +72,8 @@ function podoboo:onLoop(tickTime)
         end
         -- Play sound
         if((self.soundPlayed == false) and (cur_npc.y <= self.def_init_posY+16)) then
-            Audio.playSound(16)
-            Effect.runStaticEffectCentered(13, cur_npc.center_x, cur_npc.bottom-16)
+            Audio.playSound(self.def_podobooSoundID)
+            Effect.runStaticEffectCentered(self.def_podobooSplashEffectID, cur_npc.center_x, cur_npc.bottom-16)
             self.soundPlayed=true
             self.left_sparks = self.def_sparks;
             self.next_spark =  self.def_sparks_time - smbx_utils.ticksToTime(4);
@@ -69,7 +83,7 @@ function podoboo:onLoop(tickTime)
         if(self.left_sparks >= 0)then
             self.next_spark = self.next_spark - tickTime
             if(self.next_spark <= 0) then
-                Effect.runStaticEffectCentered(12, cur_npc.center_x + math.sin( (self.spark_rand_offset+cur_npc.y)*8)*5, cur_npc.bottom+8)
+                Effect.runStaticEffectCentered(self.def_sparkleEffectID, cur_npc.center_x + math.sin( (self.spark_rand_offset+cur_npc.y)*8)*5, cur_npc.bottom+8)
                 self.next_spark = self.def_sparks_time
                 self.left_sparks = self.left_sparks-1
             end
@@ -85,7 +99,6 @@ function podoboo:onLoop(tickTime)
             end
         end
     elseif(self.cur_mode == AI_FALLING)then
-        --Renderer.printText("AI_FALLING", 20, 400)
         self.cur_idleTicks = self.cur_idleTicks + tickTime
 
         -- Toggle animation when Podoboo do falling
@@ -95,8 +108,8 @@ function podoboo:onLoop(tickTime)
         end
         -- Play sound
         if((self.soundPlayed==false) and (cur_npc.y >= self.def_init_posY-16)) then
-            Audio.playSound(16)
-            Effect.runStaticEffectCentered(13, cur_npc.center_x, cur_npc.bottom)
+            Audio.playSound(self.def_podobooSoundID)
+            Effect.runStaticEffectCentered(self.def_podobooSplashEffectID, cur_npc.center_x, cur_npc.bottom)
             self.soundPlayed=true
         end
 
@@ -107,7 +120,6 @@ function podoboo:onLoop(tickTime)
             self.cur_mode = AI_IDLE
         end
     elseif(self.cur_mode == AI_IDLE)then
-        --Renderer.printText("AI_IDLE", 20, 400)
         if(self.cur_idleTicks >= self.def_idleTicks)then
             self.cur_idleTicks = 0
             cur_npc:setSequence(self.animateJump)
