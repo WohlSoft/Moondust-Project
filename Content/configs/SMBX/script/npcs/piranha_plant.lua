1e401ed6c5da8c07a0d4aa7b09fa7e9d46b2cc3e
Wohlstand <admin@wohlnet.ru>
2015-06-20 22:47:14 +0100

    Completely comment testing code

--- Content/configs/SMBX/script/npcs/piranha_plant.lua
+++ Content/configs/SMBX/script/npcs/piranha_plant.lua
@@ -105,18 +105,18 @@ function piranha_plant:onLoop(tickTime)
             self.cur_hidingIdleTicks = self.cur_hidingIdleTicks + tickTime
         else
             --local players = Player.get()
-            local goUp = true
+            -- local goUp = true
             --for _, player in pairs(players) do
             --    if(math.abs(player.center_x - self.npc_obj.center_x) <= 44)then
             --        goUp = false
             --        self.cur_hidingIdleTicks = 0 -- NOTE: Unknown if it resets
             --    end
             --end
-            if(goUp)then
+            -- if(goUp)then
                 self.cur_mode = AI_SHOWING_UP
                 self.npc_obj.paused_physics = false
                 self.cur_hidingIdleTicks = 0
-            end
+            --end
         end
     end
 end
