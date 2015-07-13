99ca1de507df14396ffad38c704dc8566d90eac6
Wohlstand <admin@wohlnet.ru>
2015-07-13 23:46:54 +0100

    Config Pack Update checker plan

--- Manager/mainwindow.cpp
+++ Manager/mainwindow.cpp
@@ -20,3 +20,22 @@ void MainWindow::on_actionRepositories_triggered()
     ConfPacksRepos box;
     box.exec();
 }
+
+void MainWindow::on_refresh_clicked()
+{
+    //disable "update" button
+
+    //run QtConcurrent updateConfigPacksList()
+}
+
+
+void MainWindow::updateConfigPacksList()
+{
+    //Try to get index file from each repo
+
+    //try to get each index file of each config pack (invalid or not exist config packs must be skiped)
+
+    //find already downloaded config packs and compare update time
+
+    //enable "update" button
+}
