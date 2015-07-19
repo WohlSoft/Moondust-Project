3ad70bb612a41b733cc2b3271c810d90dd3f994e
Wohlstand <admin@wohlnet.ru>
2015-07-19 23:39:11 +0100

    Working HTTP downloader HTTP Downloader will download defined file, just an index file, repo settings, target file, etc.

--- Manager/mainwindow.h
+++ Manager/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <http_downloader/http_downloader.h>
+#include <QQueue>
 
 namespace Ui {
 class MainWindow;
@@ -14,12 +16,44 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    HttpDownloader downloader;
+    QString tempDir;
+
+    enum ActionQueueEnum
+    {
+        ACT_DOWNLOAD_FILE=0,
+        ACT_PARSE_XML,
+        ACT_UNPACK_ZIP,
+        ACT_LOCK_CONFIG_PAGE,
+        ACT_UNLOCK_CONFIG_PAGE,
+        ACT_SHOWMSG
+    };
+
+    struct ActionQueueItem
+    {
+        ActionQueueEnum type;
+        QString param1;
+        QString param2;
+        QString param3;
+        QString param4;
+    };
+
+    QQueue<ActionQueueItem > queue;
+    ActionQueueItem          currentAct;
+    int curstep;
+    int totalSteps;
 
 private slots:
     void on_actionRepositories_triggered();
     void on_refresh_clicked();
     void updateConfigPacksList();
     void on_actionExit_triggered();
+
+    void queueStepBegin();
+    void downloadSuccess();
+    void downloadAborted();
+    void downloadFailed(QString reason);
+    void setProgress(qint64 bytesRead, qint64 totalBytes);
 
 protected:
     void closeEvent(QCloseEvent* e);
