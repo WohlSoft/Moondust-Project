27dd5b885e8af0f9695b1bdce7dff1d95e700ac4
Mike Santiago <miketheripper1@gmail.com>
2015-08-01 17:44:57 -0400

    More work done on the XmlTester

---
+++ Manager.mono/XmlTester/ConfigPackInformation.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace XmlTester
+{
+    public class FilesStruct
+    {
+        public string Folder {get;set;}
+        public string URL {get;set;}
+
+    }
+
+    public class AuthorStruct
+    {
+        public string Author { get; set;}
+        public string Email { get; set;}
+        public string Website {get;set;}
+        public string Comment {get;set;}
+
+
+        public AuthorStruct()
+        {
+        }
+    }
+
+    public class ConfigPackInformation
+    {
+        public string PackName {get;set;}
+        public string Description {get;set;}
+        public string IconURL { get; set;}
+        public string SplashURL { get; set;}
+        public bool IsSMBX64 { get; set;}
+        public string License {get;set;}
+
+        public List<KeyValuePair<string, AuthorStruct[]>> CreditsParts = new List<KeyValuePair<string, AuthorStruct[]>>();
+
+        public List<FilesStruct> FilesParts = new List<FilesStruct>();
+
+        //Configurations creators
+        public AuthorStruct[] ConfigCreators { get; set;}
+
+        //Graphics pack creators
+        public AuthorStruct[] GraphicsCreators {get;set;}
+
+        //Music and Sound packs creators
+        public AuthorStruct[] AudioCreators {get;set;}
+
+        public bool IsNull()
+        {
+            if (Description == null && IconURL == null && ConfigCreators == null && SplashURL == null && License == null)
+                return true;
+            else
+                return false;
+        }
+    }
+}
+
--- Manager.mono/XmlTester/Program.cs
+++ Manager.mono/XmlTester/Program.cs
@@ -6,6 +6,7 @@ namespace XmlTester
 {
     class MainClass
     {
+        private static string CONFIGDIR = "http://download.gna.org/pgewohlstand/configs/";
         private static string CONFIGSINDEX = "http://download.gna.org/pgewohlstand/configs/configs.index";
         static List<ConfigPack> ConfigList = new List<ConfigPack>();
 
@@ -15,13 +16,122 @@ namespace XmlTester
 
             TestRead2();
 
-            foreach (var cfg in ConfigList)
+            Console.WriteLine("Please choose the number of the config pack you want to view");
+    AskAgain:
+            for (int i = 0; i < ConfigList.Count; i++)
             {
-                Console.WriteLine("-{0}, URL: {1}, upd: {2}", cfg.FriendlyName, cfg.URL, cfg.upd);
+                Console.WriteLine("({0}) {1}", i, ConfigList[i].FriendlyName, ConfigList[i].URL, ConfigList[i].upd);
             }
+
+            int selection = int.Parse(Console.ReadLine());
+
+            if (selection > ConfigList.Count)
+                goto AskAgain;
+
+            ConfigPackInformation cpi = TryReadPackInformation(ConfigList[selection]);
+            PrintInformation(cpi);
 
             Console.WriteLine("\nDone :)");
             Console.ReadLine();
+        }
+
+        private static void PrintInformation(ConfigPackInformation cp)
+        {
+            Console.WriteLine(cp.PackName);
+            Console.WriteLine(cp.Description);
+            Console.WriteLine(cp.License);
+            foreach (var thing in cp.CreditsParts)
+            {
+                Console.WriteLine("---{0}---", thing.Key);
+                foreach (var author in thing.Value)
+                {
+                    string print = "-" + author.Author;
+
+                    if (author.Email != null)
+                        print += ", Email: " + author.Email;
+                    if (author.Website != null)
+                        print += ", Website: " + author.Website;
+                    if (author.Comment != null)
+                        print += "(" + author.Comment + ")";
+                }
+            }
+
+            Console.WriteLine(cp.IconURL + " & " + cp.SplashURL);
+            Console.WriteLine("SMBX64: " + cp.IsSMBX64);
+
+            Console.WriteLine("\n\nDone:)");
+            Console.ReadLine();
+        }
+
+        private static ConfigPackInformation TryReadPackInformation(ConfigPack selected)
+        {
+            XmlTextReader reader = new XmlTextReader(CONFIGDIR + selected.URL + "pge_cpack.xml");
+
+            ConfigPackInformation PackInformation = new ConfigPackInformation();
+
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        
+                        if (reader.Name == "config")
+                            PackInformation.PackName = reader.GetAttribute("name");
+                        if (reader.Name == "description")
+                            PackInformation.Description = reader.Value;
+                        if (reader.Name == "icon")
+                            PackInformation.IconURL = CONFIGDIR + selected.URL + "/" + reader.GetAttribute("img");
+                        if (reader.Name == "splash")
+                            PackInformation.SplashURL = CONFIGDIR + selected.URL + "/" + reader.GetAttribute("img");
+                        if (reader.Name == "smbx64")
+                            PackInformation.IsSMBX64 = (reader.GetAttribute("true") == "1") ? true : false;
+                        if (reader.Name == "license")
+                            PackInformation.License = reader.Value;
+                        if (reader.Name == "part")
+                        {
+                            string key = reader.GetAttribute("name");
+                            List<AuthorStruct> values = new List<AuthorStruct>();
+                            if (reader.ReadToDescendant("author"))
+                            {
+                                do
+                                {
+                                    if (reader.Name == "author")
+                                    {
+                                        AuthorStruct auth = new AuthorStruct();
+
+                                        if (reader.GetAttribute("email") != null)
+                                            auth.Email = reader.GetAttribute("email");
+                                        if (reader.GetAttribute("url") != null)
+                                            auth.Website = reader.GetAttribute("url");
+                                        if (reader.GetAttribute("comment") != null)
+                                            auth.Comment = reader.GetAttribute("comment");
+                                        auth.Author = reader.Value;
+
+                                        values.Add(auth);
+                                    }
+                                }
+                                while(reader.ReadToNextSibling("author"));
+                            }
+
+
+                            PackInformation.CreditsParts.Add(new KeyValuePair<string, AuthorStruct[]>(key, values.ToArray()));
+                        }
+                        if (reader.Name == "files")
+                        {
+                            List<FilesStruct> files = new List<FilesStruct>();
+                            if (reader.ReadToDescendant("file"))
+                            {
+                                FilesStruct fs = new FilesStruct();
+                                if (reader.GetAttribute("folder") != null)
+                                    fs.Folder = reader.GetAttribute("folder");
+                                fs.URL = reader.Value;
+                            }
+                        }
+                        break;
+
+                }
+            }
+            return PackInformation;
         }
 
         private static void TestRead2()
