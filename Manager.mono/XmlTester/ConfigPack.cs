16e47b7fe7f38f2e5e28c3a9ccb8cd72e7bf49c4
Mike Santiago <miketheripper1@gmail.com>
2015-08-01 15:32:22 -0400

    Added a small test project

---
+++ Manager.mono/XmlTester/ConfigPack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace XmlTester
+{
+	public class ConfigPack
+	{
+		public string FriendlyName {get;set;}
+		public string URL { get; set;}
+		public int upd { get; set; }
+
+		public ConfigPack ()
+		{
+		}
+	}
+}
+
