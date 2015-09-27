2441dc122ae939191e991facf63434a8d314c780
Wohlstand <admin@wohlnet.ru>
2015-09-27 21:37:35 +0300

    Fix MIDI player and SDL2 mixer's post-link script for Windows

--- _Misc/SDLMusPlayer/main.cpp
+++ _Misc/SDLMusPlayer/main.cpp
@@ -3,17 +3,21 @@
 
 #undef main
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_mixer.h>
+#include <SDL2/SDL_mixer_ext.h>
 #undef main
+
+#include <QtDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-
     SDL_Init(SDL_INIT_AUDIO);
-    //Mix_Init(MIX_INIT_FLAC | MIX_INIT_MOD | MIX_INIT_MP3 | MIX_INIT_OGG );
-    Mix_OpenAudio(44100, AUDIO_S16SYS, 2, 4096);
+    Mix_Init(MIX_INIT_FLAC | MIX_INIT_MOD | MIX_INIT_MP3 | MIX_INIT_OGG | MIX_INIT_MODPLUG );
+    qDebug() << QString(a.applicationDirPath()+"/timidity/");
+    MIX_Timidity_addToPathList(QString(a.applicationDirPath()+"/timidity/").toLocal8Bit().data());
+    Mix_OpenAudio(44100, AUDIO_S16, 2, 4096);
+    Mix_AllocateChannels(16);
 
     MainWindow w;
     w.show();
