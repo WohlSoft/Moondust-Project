d6a58df44f48f3fdcf4fa7de471cb9f3a7324adb
lighthouse64 <9pipbubbles@Macforkimfamily.home>
2015-10-11 10:06:15 -0400

    Fixed build of SDL Mixer X on OS  X

--- _Libs/SDL2_mixer_modified/native_midi/native_midi_macosx.c
+++ _Libs/SDL2_mixer_modified/native_midi/native_midi_macosx.c
@@ -24,7 +24,7 @@
 
 #include <SDL2/SDL_config.h>
 
-#if __MACOSX__
+#if __MACOSX__ && USE_NATIVE_MIDI
 
 #include <CoreServices/CoreServices.h>      /* ComponentDescription */
 #include <AudioUnit/AudioUnit.h>
