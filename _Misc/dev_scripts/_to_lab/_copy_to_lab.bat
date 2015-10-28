e8769c7bf30ee2eece0a6d75897f8453a3e04cf5
Wohlstand <admin@wohlnet.ru>
2015-10-28 09:03:44 +0300

    Ouch

--- _Misc/dev_scripts/_to_lab/_copy_to_lab.bat
+++ _Misc/dev_scripts/_to_lab/_copy_to_lab.bat
@@ -3,7 +3,7 @@ set InitDir=%CD%
 cd ..
 call _paths.bat
 
-copy "%SOURCEDIR%\bin\_packed\*.zip" "%LabDir%"
+copy "%SOURCEDIR%\bin-w32\_packed\*.zip" "%LabDir%"
 
 for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set ldt=%%j
 set ldt=%ldt:~0,4%-%ldt:~4,2%-%ldt:~6,2% %ldt:~8,2%:%ldt:~10,2%:%ldt:~12,2%
