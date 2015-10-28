7f5c2567b1661a8b53e1518bd98e2135a826c8f5
Wohlstand <admin@wohlnet.ru>
2015-10-28 05:34:16 +0300

    Better way to detect script's directory

--- _Misc/dev_scripts/_to_lab/_copy_to_lab.sh
+++ _Misc/dev_scripts/_to_lab/_copy_to_lab.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 InitDir=~+
+#=============Detect directory that contains script=====================
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  CurDir="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+CurDir="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+#=======================================================================
 cd ..
+CurDir=$CurDir/../
 source ./_paths.sh
-CurDir=~+
 
 echo "Copying data..."
 if [ -f "$SOURCEDIR/bin/_packed/pge-project-dev-linux-mint.tar.gz" ]; then
