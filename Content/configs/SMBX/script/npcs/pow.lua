07d88a7ec33b598a57e5e4ac519c423c0b66ce4f
Wohlstand <admin@wohlnet.ru>
2015-10-30 06:22:07 +0300

    More scripts and config updates

---
+++ Content/configs/SMBX/script/npcs/pow.lua
@@ -0,0 +1,69 @@
+class 'powBrick'
+
+function powBrick:initProps()
+    self.recent_speedY = 0
+end
+
+function powBrick:__init(npc_obj)
+    self.npc_obj = npc_obj
+    self.egg_color = {0}
+    self.recent_speedY = 0
+    
+    self.def_ThwompSnd = 37
+
+    self:initProps()
+end
+
+function powBrick:onActivated()
+    self:initProps()
+end
+
+function powBrick:onLoop(TickTime)
+    if ((self.npc_obj.onGround) and (self.recent_speedY > 3)) then
+        self.npc_obj:kill(2);
+        local activeNPCs = NPC.getActive()
+        for K,Goo in pairs(activeNPCs) do
+            if(self:isCoin(Goo.id))then
+                Goo.gravity=1.0
+                Goo.direction=0 --if set direction to 0, it will be randomely replaced with 1 or -1
+                Goo.speedX=0.5*Goo.direction
+                Goo.collideWithBlocks=true
+            end
+        end
+        local players = Player.get()
+        for K,Plr in pairs(players) do
+            if(Plr.onGround)then
+                Plr:bump(true, 6, 0)
+                Audio.playSound(2)
+            end
+        end
+        Audio.playSound(self.def_ThwompSnd)
+    end
+    self.recent_speedY=self.npc_obj.speedY
+end
+
+function powBrick:isCoin(npcID)
+    if(npcID==10)then
+        return true
+    elseif(npcID==33)then
+        return true
+    elseif(npcID==88)then
+        return true
+    elseif(npcID==103)then
+        return true
+    elseif(npcID==138)then
+        return true
+    elseif(npcID==152)then
+        return true
+    elseif((npcID>=251) and (npcID<=253))then
+        return true
+    elseif(npcID==258)then
+        return true
+    elseif(npcID==274)then
+        return true
+    end
+    return false
+end
+
+return powBrick
+
