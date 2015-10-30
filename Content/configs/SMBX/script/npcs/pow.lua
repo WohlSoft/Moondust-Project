946a1910c1ad52fd1024502ee82d4f838d854029
Wohlstand <admin@wohlnet.ru>
2015-10-30 06:40:58 +0300

    Fix kill even in the pow.lua

--- Content/configs/SMBX/script/npcs/pow.lua
+++ Content/configs/SMBX/script/npcs/pow.lua
@@ -21,25 +21,28 @@ end
 function powBrick:onLoop(TickTime)
     if ((self.npc_obj.onGround) and (self.recent_speedY > 3)) then
         self.npc_obj:kill(2);
-        local activeNPCs = NPC.getActive()
-        for K,Goo in pairs(activeNPCs) do
-            if(self:isCoin(Goo.id))then
-                Goo.gravity=1.0
-                Goo.direction=0 --if set direction to 0, it will be randomely replaced with 1 or -1
-                Goo.speedX=0.5*Goo.direction
-                Goo.collideWithBlocks=true
-            end
-        end
-        local players = Player.get()
-        for K,Plr in pairs(players) do
-            if(Plr.onGround)then
-                Plr:bump(true, 6, 0)
-                Audio.playSound(2)
-            end
-        end
-        Audio.playSound(self.def_ThwompSnd)
     end
     self.recent_speedY=self.npc_obj.speedY
+end
+
+function powBrick:onKill(reason)
+    local activeNPCs = NPC.getActive()
+    for K,Goo in pairs(activeNPCs) do
+        if(self:isCoin(Goo.id))then
+            Goo.gravity=1.0
+            Goo.direction=0 --if set direction to 0, it will be randomely replaced with 1 or -1
+            Goo.speedX=(math.random(1, 5)/10)*Goo.direction
+            Goo.collideWithBlocks=true
+        end
+    end
+    local players = Player.get()
+    for K,Plr in pairs(players) do
+        if(Plr.onGround)then
+            Plr:bump(true, 6, 0)
+            Audio.playSound(2)
+        end
+    end
+    Audio.playSound(self.def_ThwompSnd)
 end
 
 function powBrick:isCoin(npcID)
