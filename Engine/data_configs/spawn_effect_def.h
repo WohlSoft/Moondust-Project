8b43299a73d138ea3a117136b513e4c0ae55c36c
Wohlstand <admin@wohlnet.ru>
2015-11-13 10:01:30 +0300

    Spawn effect def class This class will be used to spawn effect with flexible configuring and wilth full access to effect's properties

--- Engine/data_configs/spawn_effect_def.h
+++ Engine/data_configs/spawn_effect_def.h
@@ -1,22 +1,31 @@
 #ifndef SPAWNEFFECTDEF_H
 #define SPAWNEFFECTDEF_H
 
+#include <QString>
+#include <QSettings>
+#include <QList>
+
 class SpawnEffectDef
 {
 public:
     SpawnEffectDef();
+    SpawnEffectDef(const SpawnEffectDef &c);
+    void fill(QString prefix, QSettings *setup);
+
     long id;
     float startX;
     float startY;
     int animationLoops;
     int delay;
+    int framespeed;
+    QList<int> frame_sequence;
     float velocityX;
     float velocityY;
     float gravity;
     int direction;
 
-    float mix_vel_x;
-    float mix_vel_y;
+    float min_vel_x;
+    float min_vel_y;
     float max_vel_x;
     float max_vel_y;
     float decelerate_x;
