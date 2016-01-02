3e5d2fb0bc5df5e9abeb43a4f4acc57cba9f64e9
Wohlstand <admin@wohlnet.ru>
2016-01-03 01:46:37 +0300

    Refresh clear_version.* scripts

--- _Misc/dev_scripts/clear_version.sh
+++ _Misc/dev_scripts/clear_version.sh
@@ -2,20 +2,37 @@
 source ./_paths.sh
 CurDir=~+
 
-rm $SOURCEDIR/bin/_build/editor/_release/.obj/aboutdialog.o
-rm $SOURCEDIR/bin/_build/editor/_release/.obj/pge_editor_res.o
-rm $SOURCEDIR/bin/_build/_dynamic/editor/_release/.obj/aboutdialog.o
-rm $SOURCEDIR/bin/_build/_dynamic/editor/_release/.obj/pge_editor_res.o
+## declare an array variable
+declare -a arr=("$SOURCEDIR/bin/_build/" "$SOURCEDIR/bin/_build/_dynamic/" "$SOURCEDIR/bin/_build_x32/" "$SOURCEDIR/bin/_build_x32/_dynamic/" "$SOURCEDIR/bin/_build_x64/" "$SOURCEDIR/bin/_build_x64/" "$SOURCEDIR/bin/_build_x64/_dynamic/")
 
-rm $SOURCEDIR/bin/_build/engine/_release/.obj/qrc_engine.o
-rm $SOURCEDIR/bin/_build/engine/_release/.obj/engine_res.o
-rm $SOURCEDIR/bin/_build/_dynamic/engine/_release/.obj/qrc_engine.o
-rm $SOURCEDIR/bin/_build/_dynamic/engine/_release/.obj/engine_res.o
+killf()
+{
+    ## now loop through the above array
+    for i in "${arr[@]}"
+    do
+        if [ -f $i$1 ]; then
+            rm $i$1
+            echo "kill -> $i$1"
+        fi
+    done
+}
 
-rm $SOURCEDIR/bin/_build/calibrator/_release/.obj/caltool_res.o
-rm $SOURCEDIR/bin/_build/calibrator/_release/.obj/qrc_images.o
-rm $SOURCEDIR/bin/_build/_dynamic/calibrator/_release/.obj/caltool_res.o
-rm $SOURCEDIR/bin/_build/_dynamic/calibrator/_release/.obj/qrc_images.o
+killf pge_editor/_release/.obj/aboutdialog.o
+killf pge_editor/_release/.obj/check_updates.o
+killf pge_editor/_release/.obj/moc_aboutdialog.o
+killf pge_editor/_release/.obj/moc_check_updates.o
+killf pge_editor/_release/.obj/pge_editor_res.o
+killf pge_engine/_release/.obj/qrc_engine.o
+killf pge_engine/_release/.obj/engine_res.o
+killf pge_engine/_release/.obj/main.o
+killf pge_calibrator/_release/.obj/caltool_res.o
+killf pge_calibrator/_release/.obj/qrc_images.o
+killf png2gifs/_release/.obj/qrc_png2gifs.o
+killf png2gifs/_release/.obj/png2gifs.o
+killf GIFs2PNG/_release/.obj/qrc_gifs2png.o
+killf GIFs2PNG/_release/.obj/gifs2png.o
+killf LazyFixTool/_release/.obj/qrc_lazyfix_tool.o
+killf LazyFixTool/_release/.obj/LazyFixTool.o
 
 echo "Removed all objects which contains version numbers"
 read -n 1
