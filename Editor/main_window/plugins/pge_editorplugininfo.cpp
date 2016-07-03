9de5110025114f7aa104bfde48620a7c17478dcf
KevinW1998 <kevin.waldock@gmail.com>
2016-07-03 15:58:15 +0200

    React to plugins, which fails to load

--- Editor/main_window/plugins/pge_editorplugininfo.cpp
+++ Editor/main_window/plugins/pge_editorplugininfo.cpp
@@ -9,11 +9,16 @@ PGE_EditorPluginInfo::PGE_EditorPluginInfo(PGE_EditorPluginManager *pluginManage
     ui->setupUi(this);
 
     for(unsigned int i = 0; i < pluginManager->getPluginCount(); ++i){
-        const PGE_EditorPluginItem& nextItem = pluginManager->getPluginInfo(i);
-        QListWidgetItem* nextDisplayItem = new QListWidgetItem(nextItem.pluginName());
+        const PGE_EditorPluginItem* nextItem = pluginManager->getPluginInfo(i);
+        QString nextDisplayName = (nextItem->isValid() ? nextItem->pluginName() : QString("Package: ") + nextItem->packageName());
+        QListWidgetItem* nextDisplayItem = new QListWidgetItem(nextDisplayName);
         nextDisplayItem->setData(UserRolePluginIndex, QVariant(i));
         nextDisplayItem->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEnabled | Qt::ItemIsUserCheckable);
         nextDisplayItem->setCheckState(Qt::Checked);
+        if(nextItem->isValid())
+            nextDisplayItem->setForeground(Qt::blue);
+        else
+            nextDisplayItem->setForeground(Qt::red);
         ui->pluginList->addItem(nextDisplayItem);
     }
 
@@ -23,11 +28,19 @@ PGE_EditorPluginInfo::PGE_EditorPluginInfo(PGE_EditorPluginManager *pluginManage
 
     connect(ui->pluginList, &QListWidget::currentItemChanged,
             [this](QListWidgetItem* current, QListWidgetItem*){
-        const PGE_EditorPluginItem& pluginDetails = m_pluginManager->getPluginInfo(current->data(UserRolePluginIndex).toInt());
-        ui->textInfo->setText(tr("Name: ") + pluginDetails.pluginName() + "\n"
-                              + tr("Author: ") + pluginDetails.authorName() + "\n"
-                              + tr("Version: ") + QString::number(pluginDetails.pluginVersionNum()) + "\n"
-                              + tr("Description: ") + pluginDetails.description());
+        const PGE_EditorPluginItem* pluginDetails = m_pluginManager->getPluginInfo(current->data(UserRolePluginIndex).toInt());
+        if(pluginDetails->isValid())
+        {
+            ui->textInfo->setText(tr("Name: ") + pluginDetails->pluginName() + "\n"
+                                  + tr("Author: ") + pluginDetails->authorName() + "\n"
+                                  + tr("Version: ") + QString::number(pluginDetails->pluginVersionNum()) + "\n"
+                                  + tr("Description: ") + pluginDetails->description());
+        }
+        else
+        {
+            ui->textInfo->setText(tr("Package ") + pluginDetails->packageName() + tr(" failed to load!\n")
+                                  + tr("Error description: \n") + pluginDetails->errorMessage());
+        }
     });
 }
 
--- Editor/main_window/plugins/pge_editorpluginitem.cpp
+++ Editor/main_window/plugins/pge_editorpluginitem.cpp
@@ -1,10 +1,28 @@
 #include "pge_editorpluginitem.h"
+#include "pge_editorpluginmanager.h"
 
-PGE_EditorPluginItem::PGE_EditorPluginItem(const QString &pluginName, const QString &authorName, const QString &description, int pluginVersionNumber) :
+PGE_EditorPluginItem::PGE_EditorPluginItem(PGE_EditorPluginManager *manager, const QString &packageName, const QString &errorMessage) :
+    QObject(manager),
+    m_pluginManager(manager),
+    m_packageName(packageName),
+    m_pluginName(""),
+    m_authorName(""),
+    m_description(""),
+    m_pluginVersionNum(0),
+    m_isValid(false),
+    m_errorMessage(errorMessage)
+{}
+
+PGE_EditorPluginItem::PGE_EditorPluginItem(PGE_EditorPluginManager* manager, const QString& packageName, const QString &pluginName, const QString &authorName, const QString &description, int pluginVersionNumber) :
+    QObject(manager),
+    m_pluginManager(manager),
+    m_packageName(packageName),
     m_pluginName(pluginName),
     m_authorName(authorName),
     m_description(description),
-    m_pluginVersionNum(pluginVersionNumber)
+    m_pluginVersionNum(pluginVersionNumber),
+    m_isValid(true),
+    m_errorMessage("")
 {}
 
 QString PGE_EditorPluginItem::pluginName() const
@@ -25,5 +43,35 @@ int PGE_EditorPluginItem::pluginVersionNum() const
 QString PGE_EditorPluginItem::description() const
 {
     return m_description;
+}
+
+QString PGE_EditorPluginItem::packageName() const
+{
+    return m_packageName;
+}
+
+void PGE_EditorPluginItem::setPackageName(const QString &packageName)
+{
+    m_packageName = packageName;
+}
+
+bool PGE_EditorPluginItem::isValid() const
+{
+    return m_isValid;
+}
+
+void PGE_EditorPluginItem::setIsValid(bool isValid)
+{
+    m_isValid = isValid;
+}
+
+QString PGE_EditorPluginItem::errorMessage() const
+{
+    return m_errorMessage;
+}
+
+void PGE_EditorPluginItem::setErrorMessage(const QString &errorMessage)
+{
+    m_errorMessage = errorMessage;
 }
 
