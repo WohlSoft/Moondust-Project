d387501bdce9819ff6efeb775af43d6c911c9abb
Wohlstand <admin@wohlnet.ru>
2016-08-22 02:01:51 +0300

    Added some event functions into templates

--- Editor/_resources/lunalua/templates/lunadll.lua
+++ Editor/_resources/lunalua/templates/lunadll.lua
@@ -2,31 +2,56 @@
 -- Level code
 -- Created @DATETIME@
 --------------------------------------------------
+@ONLOAD{
 
+-- Run code on level loading by game engine before initalizing stuff
+-- (CAREFULLY USE IT! Use it to initialize own variables
+-- and don't process any operations with any objects and states of the level or world map!
+-- Use onStart() for those purposes instead this).
+function onLoad()
+    --Your code here
+end
+@}
 @ONSTART{
+
 -- Run code on level start
 function onStart()
     --Your code here
 end
 @}
+@ONLOOP{
 
+-- Immediately at the start of each frame while the game runs onLoop is called.
+-- Note: In LunaLua ≥ v0.7.3, you should usually use onTick in place of where you'd use onLoop in the past
+function onLoop()
+    --Your code here
+end
+@}
 @ONTICK{
+
 -- Run code every frame (~1/65 second)
 -- (code will be executed before game logic will be processed)
 function onTick()
     --Your code here
 end
 @}
-
 @ONTICKEND{
+
 -- Run code every frame (~1/65 second)
 -- (code will be executed after game logic will be processed)
 function onTickEnd()
     --Your code here
 end
 @}
+@ONDRAW{
 
+-- This is called just before drawing each frame
+function onDraw()
+    --Your code here
+end
+@}
 @ONEVENT{
+
 -- Run code when internal event of the SMBX Engine has been triggered
 -- eventName - name of triggered event
 function onEvent(eventName)
--- Editor/_resources/lunalua/templates/lunaworld.lua
+++ Editor/_resources/lunalua/templates/lunaworld.lua
@@ -2,27 +2,53 @@
 -- Episode code for every level
 -- Created @DATETIME@
 --------------------------------------------------
+@ONLOAD{
 
+-- Run code on level loading by game engine before initalizing stuff
+-- (CAREFULLY USE IT! Use it to initialize own variables
+-- and don't process any operations with any objects and states of the level or world map!
+-- Use onStart() for those purposes instead this).
+function onLoad()
+    --Your code here
+end
+@}
 @ONSTART{
+
 -- Run code on the first frame
 function onStart()
     --Your code here
 end
 @}
-
 @ONTICK{
+
 -- Run code every frame (~1/65 second)
 -- (code will be executed before game logic will be processed)
 function onTick()
     --Your code here
 end
 @}
-
 @ONTICKEND{
+
 -- Run code every frame (~1/65 second)
 -- (code will be executed after game logic will be processed)
 function onTickEnd()
     --Your code here
 end
 @}
+@ONDRAW{
+
+-- This is called just before drawing each frame
+function onDraw()
+    --Your code here
+end
+@}
+@ONEVENT{
+
+-- Run code when internal event of the SMBX Engine has been triggered
+-- eventName - name of triggered event
+function onEvent(eventName)
+    --Your code here
+end
+@}
+
 
