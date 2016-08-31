f1a9cd4c4e4204d4839bacb8cdca388e36dfa57f
Wohlstand <admin@wohlnet.ru>
2016-08-31 04:24:32 +0300

    Add a full conversion of the config pack from nested into split

--- Content/configs/SMBX/nest2split.php
+++ Content/configs/SMBX/nest2split.php
@@ -1,5 +1,6 @@
 <?php
 
+
 function needQuotes($value)
 {
     if($value == (string)intval($value))
@@ -26,14 +27,38 @@ function splitIni($iniFile, $targetDirectory, $sectionName)
     foreach($ini_array as $key => &$section)
     {
         //echo $key;
-        $out_data = "[".$sectionName."]\n";
+        if(!EndsWith($key, "-main"))
+            $out_data = "[".$sectionName."]\n";
+        else
+            $out_data = "[".$key."]\n";
         foreach($section as $param => &$value)
         {
             $out_data .= $param . " = " . needQuotes($value) . "\n";
         }
         if(!EndsWith($key, "-main"))
-            file_put_contents($targetDirectory . $key . ".ini", $out_data);
+            file_put_contents($targetDirectory . "/" . $key . ".ini", $out_data);
+        else
+        {
+            $out_data .= "config-dir = " . needQuotes($targetDirectory) . "\n";
+            file_put_contents($iniFile, $out_data);
+        }
     }
 }
 
+echo "\n";
+
+if(!is_dir("items"))
+    mkdir("items");
+
+splitIni("lvl_blocks.ini", "items/blocks", "block");
+splitIni("lvl_bgo.ini", "items/bgo", "background");
+splitIni("lvl_npc.ini", "items/npc", "npc");
+splitIni("lvl_bkgrd.ini", "items/backgrounds", "background2");
+
+splitIni("wld_tiles.ini", "items/terrain", "tile");
+splitIni("wld_scenery.ini", "items/scenery", "scenery");
+splitIni("wld_paths.ini", "items/paths", "path");
+splitIni("wld_levels.ini", "items/levels", "level");
+
+echo "\n";
 
