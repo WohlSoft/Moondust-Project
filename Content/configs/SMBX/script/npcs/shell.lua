9e5a144499fb1ee7aa1888a5f95b3355f6c34ee4
Wohlstand <admin@wohlnet.ru>
2016-09-14 22:32:55 +0300

    Shooted shells are will in go in running mode!

--- Content/configs/SMBX/script/npcs/shell.lua
+++ Content/configs/SMBX/script/npcs/shell.lua
@@ -10,9 +10,16 @@ function shell:initProps()
     -- Currents
     self.cur_mode=AI_IDLING
     self.npc_obj.motionSpeed = 0
-    self.npc_obj.speedX = 0
-    self.npc_obj.speedY = 0
+    if(self.npc_obj.spawnedGenType == BaseNPC.SPAWN_APPEAR) then
+        self.npc_obj.speedX = 0
+        self.npc_obj.speedY = 0
+    end
     self.npc_obj:setSequence({0})
+    if(self.npc_obj.spawnedGenType == BaseNPC.SPAWN_PROJECTILE) then
+            if self.npc_obj.spawnedGenDirection == BaseNPC.SPAWN_LEFT    then self.npc_obj.direction = -1; self:setRunning()
+        elseif self.npc_obj.spawnedGenDirection == BaseNPC.SPAWN_RIGHT   then self.npc_obj.direction = 1;  self:setRunning()
+        end
+    end
 end
 
 function shell:__init(npc_obj)
@@ -30,7 +37,7 @@ function shell:onLoop(tickTime)
         if(self.contacts:detected())then
             local Blocks= self.contacts:getBlocks()
             for K,Blk in pairs(Blocks) do
-                if(Blk.collide_npc == PhysBase.COLLISION_ANY)then
+                if(Blk.collide_npc == PhysBase.COLLISION_ANY and (self.npc_obj.blockedAtLeft or self.npc_obj.blockedAtRight) )then
                     if (Blk.top+2 < self.npc_obj.bottom) and (Blk.bottom-2 > self.npc_obj.top) then
                         Blk:hit(2)
                     end
@@ -71,18 +78,26 @@ function shell:onKill(killEvent)
     end
 end
 
+function shell:setRunning()
+    self.cur_mode=AI_RUNNING
+    self.npc_obj.motionSpeed = 7.1
+    self.npc_obj.frameDelay = 32
+    self.npc_obj:setSequence({0,1,2,3})
+end
+
+function shell:setIdling()
+    self.cur_mode=AI_IDLING
+    self.npc_obj.motionSpeed = 0
+    self.npc_obj.speedX = 0
+    self.npc_obj:setSequence({0})
+end
+
 function shell:toggleState()
     if(self.cur_mode == AI_IDLING)then
-        self.cur_mode=AI_RUNNING
-        self.npc_obj.motionSpeed = 7.1
-        self.npc_obj.frameDelay = 32
-        self.npc_obj:setSequence({0,1,2,3})
+        self:setRunning()
         Audio.playSoundByRole(SoundRoles.PlayerKick)
     else
-        self.cur_mode=AI_IDLING
-        self.npc_obj.motionSpeed = 0
-        self.npc_obj.speedX = 0
-        self.npc_obj:setSequence({0})
+        self:setIdling()
     end    
 end
 
