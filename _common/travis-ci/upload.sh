93390c1c8d6ef66800898e1be42458ccc2c23104
Wohlstand <admin@wohlnet.ru>
2016-10-14 14:08:58 +0300

    Attempt to fix Travis-CI builder

--- _common/travis-ci/upload.sh
+++ _common/travis-ci/upload.sh
@@ -2,16 +2,21 @@
 
 if [ $TRAVIS_OS_NAME == linux ];
 then
-    if [ -d /home/runner/PGE-Project/bin/_packed ]; 
-    then
-        cd /home/runner/PGE-Project/Content/configs
-        zip -9 -r /home/runner/PGE-Project/bin/_packed/SMBX-Config-Patch.zip SMBX
-    fi
 
-    if [ -d /home/runner/PGE-Project/bin/_packed ]; 
-    then 
-        lftp -e "put -O ./ubuntu-14-04/ /home/runner/PGE-Project/bin/_packed/pge-project-dev-linux-mint.tar.gz; put -O ./_common/ /home/runner/PGE-Project/bin/_packed/SMBX-Config-Patch.zip; exit" -u $FTP_USER,$FTP_PASSWORD $FTP_SERVER
-        lftp -e "put -O ./_versions/ /home/runner/build_date_dev_linux.txt; put -O ./_versions/ /home/runner/PGE-Project/bin/versions/editor.txt; put -O ./_versions/ /home/runner/PGE-Project/bin/versions/editor_stable.txt; put -O ./_versions/ /home/runner/PGE-Project/bin/versions/engine.txt; put -O ./_versions/ /home/runner/PGE-Project/bin/versions/engine_stable.txt; exit" -u $FTP_USER,$FTP_PASSWORD $FTP_SERVER
+    #Skip deploy on Travis-CI, since it done on Semaphore-CI
+    if [[ $(whoami) != "travis" ]];
+    then
+        if [ -d /home/runner/PGE-Project/bin/_packed ]; 
+        then
+            cd /home/runner/PGE-Project/Content/configs
+            zip -9 -r /home/runner/PGE-Project/bin/_packed/SMBX-Config-Patch.zip SMBX
+        fi
+
+        if [ -d /home/runner/PGE-Project/bin/_packed ]; 
+        then 
+            lftp -e "put -O ./ubuntu-14-04/ /home/runner/PGE-Project/bin/_packed/pge-project-dev-linux-mint.tar.gz; put -O ./_common/ /home/runner/PGE-Project/bin/_packed/SMBX-Config-Patch.zip; exit" -u $FTP_USER,$FTP_PASSWORD $FTP_SERVER
+            lftp -e "put -O ./_versions/ /home/runner/build_date_dev_linux.txt; put -O ./_versions/ /home/runner/PGE-Project/bin/versions/editor.txt; put -O ./_versions/ /home/runner/PGE-Project/bin/versions/editor_stable.txt; put -O ./_versions/ /home/runner/PGE-Project/bin/versions/engine.txt; put -O ./_versions/ /home/runner/PGE-Project/bin/versions/engine_stable.txt; exit" -u $FTP_USER,$FTP_PASSWORD $FTP_SERVER
+        fi
     fi
 
 elif [ $TRAVIS_OS_NAME == osx ];
