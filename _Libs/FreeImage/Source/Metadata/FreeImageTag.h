624a9c1d114baba7b9ca696ecf3e8fe664c19e0a
Wohlstand <admin@wohlnet.ru>
2016-11-02 16:38:31 +0400

    Tiny change

--- _Libs/FreeImage/Source/Metadata/FreeImageTag.h
+++ _Libs/FreeImage/Source/Metadata/FreeImageTag.h
@@ -2,7 +2,7 @@
 // Tag manipulation functions
 //
 // Design and implementation by
-// - Hervé Drolon <drolon@infonie.fr>
+// - Herve Drolon <drolon@infonie.fr>
 //
 // This file is part of FreeImage 3
 //
