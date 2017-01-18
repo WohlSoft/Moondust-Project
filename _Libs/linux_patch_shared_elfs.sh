7eb88004c63d780dfd541849d06713549df3a1b6
Wohlstand <admin@wohlnet.ru>
2017-01-19 00:07:33 +0300

    Notify about not installed "patchelf" utility in the patch script [ci skip]

--- _Libs/linux_patch_shared_elfs.sh
+++ _Libs/linux_patch_shared_elfs.sh
@@ -3,6 +3,22 @@
 echo "This script adds into rpath the path to shared Qt libraries."
 echo "That need if applications are can't work with pre-installed or failing with .so not found or version QT_5 is not same"
 
+if [ ! -f /usr/bin/patchelf ]; then
+    echo "ERROR! Can't use this script without patchelf"
+    echo "Install it first!"
+    if [  -f /usr/bin/apt-get ]; then
+        echo "   sudo apt-get install patchelf"
+    elif [  -f /usr/bin/yum ]; then
+        echo "   sudo yum install patchelf"
+    elif [  -f /usr/bin/pacman ]; then
+        echo "   sudo pacman install patchelf"
+    fi
+    echo ""
+
+    if [[ "$1" != "nopause" ]]; then echo "Press any key to continue..."; read -n 1; fi
+    exit 1
+fi
+
 source ../_paths.sh
 BIN_PATH=../bin
 
@@ -19,4 +35,5 @@ patchBin pge_manager
 patchBin pge_maintainer
 patchBin pge_musplay
 
+if [[ "$1" != "nopause" ]]; then echo "Press any key to continue..."; read -n 1; fi
 
