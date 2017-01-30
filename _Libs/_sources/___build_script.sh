aa41177ab2935439a6dddfa958d6876af7ad9786
Wohlstand <admin@wohlnet.ru>
2017-01-31 01:19:52 +0300

    Dependencies: organizing of the build script - breaking long argument sets - added optional build of libsamplerate (support experimentally) - experiment with the GLEW build script (bit later is planned to use GLEW, but need to setup it's building on all platforms)

--- _Libs/_sources/___build_script.sh
+++ _Libs/_sources/___build_script.sh
@@ -63,7 +63,13 @@ BuildSrc()
     cd $1
     #Build debug version of SDL
     #CFLAGS='-O0 -g' ./configure $2
+
+    echo ----------------------------------------------------------------------
+    echo "./configure $2"
+    echo ----------------------------------------------------------------------
+
     ./configure $2
+
     if [ $? -eq 0 ]
     then
         printf "\n[Configure completed]\n\n"
@@ -93,6 +99,21 @@ BuildSrc()
 
 #############################Build libraries#####################
 
+BuildSampleRate()
+{
+    CURRENT_TARBALL="libSampleRate"
+    UnArch 'libsamplerate-0.1.9'
+    printf "=========\E[37;42mlibSampleRate\E[0m===========\n"
+    lSRC_ARGS="${lSRC_ARGS} --prefix=${InstallTo}"
+    lSRC_ARGS="${lSRC_ARGS} --includedir=${InstallTo}/include"
+    lSRC_ARGS="${lSRC_ARGS} --libdir=${InstallTo}/lib"
+    lSRC_ARGS="${lSRC_ARGS} CFLAGS=-fPIC CXXFLAGS=-fPIC"
+    lSRC_ARGS="${lSRC_ARGS} --enable-static=yes --enable-shared=no"
+    BuildSrc 'libsamplerate-0.1.9' "${lSRC_ARGS}"
+    # use libSampleRate with SDL:
+    SDL_ARGS="${SDL_ARGS} --enable-libsamplerate=yes --disable-libsamplerate-shared "
+}
+
 BuildSDL()
 {
     CURRENT_TARBALL="SDL2"
@@ -115,7 +136,12 @@ BuildSDL()
     #cd ..
     if [[ "$OurOS" != "macos" ]]; then
         #on any other OS'es build via autotools
-        BuildSrc $LatestSDL $SDL_ARGS'--prefix='$InstallTo' --includedir='$InstallTo'/include --libdir='$InstallTo'/lib'
+        export CFLAGS="-I${InstallTo}/include"
+        export LDFLAGS="-L${InstallTo}/lib"
+        SDL_ARGS="${SDL_ARGS} --prefix=${InstallTo}"
+        SDL_ARGS="${SDL_ARGS} --includedir=${InstallTo}/include"
+        SDL_ARGS="${SDL_ARGS} --libdir=${InstallTo}/lib"
+        BuildSrc $LatestSDL "${SDL_ARGS}"
     else
         #on Mac OS X build via X-Code
         cd $LatestSDL
@@ -149,11 +175,27 @@ BuildFluidSynth()
 {
     CURRENT_TARBALL="FluidSynth"
     UnArch 'fluidsynth-1.1.6'
-    ###########MODPLUG###########
     printf "=========\E[37;42mFLUIDSYNTH\E[0m===========\n"
     #Build minimalistic FluidSynth version to just generate raw audio output to handle in the SDL Mixer X
-    #./configure CFLAGS=-fPIC --prefix=/home/vitaly/_git_repos/PGE-Project/_Libs/_builds/linux/ --disable-dbus-support --disable-pulse-support --disable-alsa-support --disable-portaudio-support --disable-oss-support --disable-jack-support --disable-midishare --disable-coreaudio --disable-coremidi --disable-dart --disable-lash --disable-ladcca --enable-static=yes --enable-shared=no
-    BuildSrc 'fluidsynth-1.1.6' '--prefix='$InstallTo' --includedir='$InstallTo'/include --libdir='$InstallTo'/lib CFLAGS=-fPIC CXXFLAGS=-fPIC --disable-dbus-support --disable-pulse-support --disable-alsa-support --disable-portaudio-support --disable-oss-support --disable-jack-support --disable-midishare --disable-coreaudio --disable-coremidi --disable-dart --disable-lash --disable-ladcca --without-readline --enable-static=yes --enable-shared=no'
+    FLUID_ARGS="${FLUID_ARGS} --prefix=${InstallTo}"
+    FLUID_ARGS="${FLUID_ARGS} --includedir=${InstallTo}/include"
+    FLUID_ARGS="${FLUID_ARGS} --libdir=${InstallTo}/lib"
+    FLUID_ARGS="${FLUID_ARGS} CFLAGS=-fPIC CXXFLAGS=-fPIC"
+    FLUID_ARGS="${FLUID_ARGS} --disable-dbus-support"
+    FLUID_ARGS="${FLUID_ARGS} --disable-pulse-support"
+    FLUID_ARGS="${FLUID_ARGS} --disable-alsa-support"
+    FLUID_ARGS="${FLUID_ARGS} --disable-portaudio-support"
+    FLUID_ARGS="${FLUID_ARGS} --disable-oss-support"
+    FLUID_ARGS="${FLUID_ARGS} --disable-jack-support"
+    FLUID_ARGS="${FLUID_ARGS} --disable-midishare"
+    FLUID_ARGS="${FLUID_ARGS} --disable-coreaudio"
+    FLUID_ARGS="${FLUID_ARGS} --disable-coremidi"
+    FLUID_ARGS="${FLUID_ARGS} --disable-dart"
+    FLUID_ARGS="${FLUID_ARGS} --disable-lash"
+    FLUID_ARGS="${FLUID_ARGS} --disable-ladcca"
+    FLUID_ARGS="${FLUID_ARGS} --without-readline"
+    FLUID_ARGS="${FLUID_ARGS} --enable-static=yes --enable-shared=no"
+    BuildSrc 'fluidsynth-1.1.6' "${FLUID_ARGS}"
 }
 
 BuildLUAJIT()
@@ -165,7 +207,7 @@ BuildLUAJIT()
     printf "=========\E[37;42mLuaJIT\E[0m===========\n"
     cd LuaJIT
     echo "Building..."
-    make -s --jobs=2 PREFIX=$InstallTo BUILDMODE=static
+    make -s --jobs=2 PREFIX="${InstallTo}" BUILDMODE=static
     if [ $? -eq 0 ]
     then
         echo "[good]"
@@ -174,7 +216,7 @@ BuildLUAJIT()
     fi
 
     echo "Installing..."
-    make -s install PREFIX=$InstallTo BUILDMODE=static
+    make -s install PREFIX="${InstallTo}" BUILDMODE=static
     if [ $? -eq 0 ]
     then
         echo "[good]"
@@ -183,8 +225,8 @@ BuildLUAJIT()
     fi
 
     if [[ "$OurOS" == "macos" ]]; then
-        cp -a ./src/libluajit.a $InstallTo/lib/libluajit.a
-        cp -a ./src/libluajit.a $InstallTo/lib/libluajit-5.1.a
+        cp -a ./src/libluajit.a "${InstallTo}/lib/libluajit.a"
+        cp -a ./src/libluajit.a "${InstallTo}/lib/libluajit-5.1.a"
     fi
     cd ..
 }
@@ -192,24 +234,31 @@ BuildLUAJIT()
 BuildGLEW()
 {
     CURRENT_TARBALL="GLEW"
-    UnArch 'glew-1.13.0'
+    UnArch 'glew-2.0.0'
 
     ###########GLEW###########
     printf "=========\E[37;42mGLEW\E[0m===========\n"
-    cd glew-1.13.0
-    make GLEW_PREFIX=$InstallTo GLEW_DEST=$InstallTo CFLAGS.EXTRA="-DGLEW_STATIC -fPIC" GLEW_NO_GLU="-DGLEW_NO_GLU"
+    cd glew-2.0.0
+    GLEW_ARGS="${GLEW_ARGS} GLEW_PREFIX=\"${InstallTo}\""
+    GLEW_ARGS="${GLEW_ARGS} LIBDIR=\"${InstallTo}/lib\""
+    GLEW_ARGS="${GLEW_ARGS} GLEW_DEST=\"${InstallTo}\""
+    GLEW_ARGS="${GLEW_ARGS} GLEW_NO_GLU=\"-DGLEW_NO_GLU\""
+    echo ------------------------------------------------------------
+    echo make ${GLEW_ARGS} CFLAGS.EXTRA="-fPIC"
+    echo ------------------------------------------------------------
+    make ${GLEW_ARGS} CFLAGS.EXTRA="-fPIC"
     if [ $? -eq 0 ]
     then
-      echo "[good]"
+        echo "[good]"
     else
-      errorofbuild
+        errorofbuild
     fi
-        make install GLEW_PREFIX=$InstallTo GLEW_DEST=$InstallTo CFLAGS.EXTRA="-DGLEW_STATIC" GLEW_NO_GLU="-DGLEW_NO_GLU"
+        make install $GLEW_ARGS
         if [ $? -eq 0 ]
         then
-          echo "[good]"
+            echo "[good]"
         else
-          errorofbuild
+            errorofbuild
         fi
     cd ..
 }
@@ -224,8 +273,8 @@ fi
 cd $CACHE_DIR
 
 BuildLUAJIT
+#BuildSampleRate
 BuildSDL
-
 #BuildFluidSynth
 #BuildGLEW
 
