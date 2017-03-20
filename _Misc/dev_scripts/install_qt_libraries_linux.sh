c75537e806f0da21ee53c9514fc00f19d31d2560
Wohlstand <admin@wohlnet.ru>
2017-03-20 23:02:43 +0300

    DevScripts: Add the Qt into ldconfig environment instead of coping SOs That would be fine on Debian/Ubuntu/CentOS, but will destroy OpenSUSE because of own set of Qt libraries are very important for the KDE

--- _Misc/dev_scripts/install_qt_libraries_linux.sh
+++ _Misc/dev_scripts/install_qt_libraries_linux.sh
@@ -10,14 +10,24 @@ else
 	exit 1
 fi
 
-if [[ $(whoami) != "root" ]]
+iamuser=$(whoami)
+
+if [[ "$iamuser" != "root" ]]
 then
 	echo ""
 	echo "Root is needed to copy a tonn of Qt's SO's into /usr/lib/x86_64-linux-gnu/ folder"
 	exit 1
 fi
 
-cp -n -a "$QT_PATH"'../lib/'*'.so'* /usr/lib/x86_64-linux-gnu/
+# cp -n -a "$QT_PATH"'../lib/'*'.so'* /usr/lib/x86_64-linux-gnu/
+
+ldconf_file=/etc/ld.so.conf.d/libqt5.pge-project.custom.conf
+
+echo "# Custom installed Qt 5, used by PGE Project" >  $ldconf_file
+echo ${QT_LIB_PATH} >> $ldconf_file
+
+ldconfig
+
 echo "DONE!"
 read -n 1
 
