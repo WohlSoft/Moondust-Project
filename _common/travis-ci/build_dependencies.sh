8cc5049f3ab06ff99d61db172aafc23831a859be
Wohlstand <admin@wohlnet.ru>
2017-03-21 13:55:13 +0300

    Travis-CI: Try out the caching thing to boost up build process

--- _common/travis-ci/build_dependencies.sh
+++ _common/travis-ci/build_dependencies.sh
@@ -2,13 +2,20 @@
 
 if [ $TRAVIS_OS_NAME == linux ];
 then
-    bash ./build_deps.sh no-pause use-ccache
+    # Don't build dependencies if there are cached
+    if [ ! -d _Libs/_builds/linux ];
+    then
+        bash ./build_deps.sh no-pause use-ccache
+    fi
     sudo cp -a _Libs/_builds/linux/lib/*.so /usr/lib
 
 elif [ $TRAVIS_OS_NAME == osx ];
 then
     source _common/travis-ci/_osx_env.sh
-    bash ./build_deps.sh no-pause
-
+    # Don't build dependencies if there are cached
+    if [ ! -d _Libs/_builds/macos ];
+    then
+        bash ./build_deps.sh no-pause
+    fi
 fi
 
