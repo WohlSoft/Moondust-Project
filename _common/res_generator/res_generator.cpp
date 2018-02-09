c347e4f8af35a89165a4ea6383d3e4941d65483c
Vitaly Novichkov <admin@wohlnet.ru>
2018-02-09 04:15:58 +0300

    RES Generator: Added MIT license into the header

--- _common/res_generator/res_generator.cpp
+++ _common/res_generator/res_generator.cpp
@@ -1,3 +1,28 @@
+/*
+ * RES_Generator - A small resource generator which converts list of files
+ * into C++ arrays are can be accessible via friendly functions
+ *
+ * Copyright (c) 2018 Vitaly Novichkov <admin@wohlnet.ru>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the “Software”), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #include <cstdio>
 #include <string>
 #include <vector>
@@ -21,7 +46,7 @@ int main(int argc, char**argv)
     IniProcessing ini;
     std::string folderAt = "../../Engine/_resources/";
     std::string iniFile = "resource.ini";
-    
+
     ini.open(folderAt + iniFile);
     unsigned int count = 0;
     ini.beginGroup("resources");
@@ -158,3 +183,4 @@ int main(int argc, char**argv)
 
     return 0;
 }
+
