71fc970ef190a4beca9853c1a11f840d187138b7
Wohlstand <admin@wohlnet.ru>
2018-02-16 06:57:08 +0300

    CMake-UNIX2DOS: Fixed initial warning about parentness of a variable

--- _common/unix2dos_copy.cmake
+++ _common/unix2dos_copy.cmake
@@ -1,6 +1,6 @@
-if(WIN32)
-    set(UNIX2DOS_LIST PARENT_SCOPE)
-endif()
+# A small function to pass text files through UNIX2DOS utiltiy on Windows or install them by the regular way
+# Requires a custom build of UNIX2DOS utility!
+
 
 function(InstallTextFile)
     cmake_parse_arguments(_installfile
@@ -16,15 +16,24 @@ function(InstallTextFile)
 
     if(WIN32)
         message("File ${_installfile_FILE} will be UNIX2DOS-ed")
-        set(UNIX2DOS_LIST "${UNIX2DOS_LIST};${_installfile_FILE}?${CMAKE_INSTALL_PREFIX}/${_installfile_DESTINATION}?${_installfile_RENAME}" PARENT_SCOPE)
+        set(UNIX2DOS_ELEMENT ${_installfile_FILE}?${CMAKE_INSTALL_PREFIX}/${_installfile_DESTINATION}?${_installfile_RENAME})
+        if(UNIX2DOS_LIST)
+            # Add next elements as first element is already added
+            set(UNIX2DOS_LIST "${UNIX2DOS_LIST};${UNIX2DOS_ELEMENT}" PARENT_SCOPE)
+        else()
+            # Add first element to avoid empty element in begin
+            set(UNIX2DOS_LIST "${UNIX2DOS_ELEMENT}" PARENT_SCOPE)
+        endif()
     else()
         install(FILES "${_installfile_FILE}" RENAME "${_installfile_RENAME}" DESTINATION "${_installfile_DESTINATION}")
     endif()
 endfunction()
 
-
+# Put this function into end of config without any arguments.
+# Thus function initializes the installer script which will be ran on install step
 function(InstallTextFiles)
     if(WIN32)
+        # message("== UNIX2DOS List is ${UNIX2DOS_LIST}")
         set(custom_script ${CMAKE_BINARY_DIR}/unix2dos_list.cmake)
         configure_file(_common/unix2dos.cmake.in ${custom_script} @ONLY)
         install(SCRIPT ${custom_script})
