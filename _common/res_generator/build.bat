ecf3b28071afde7bc79f3da336ea1c6feecae1ac
Wohlstand <admin@wohlnet.ru>
2018-02-17 20:11:08 +0300

    ResGenerator: A small polishing to prepare it be integarted with CMake

--- _common/res_generator/build.bat
+++ _common/res_generator/build.bat
@@ -3,12 +3,13 @@ call ..\..\_paths.bat
 PATH=%MinGW%;%PATH%
 
 md bin
-g++ -std=c++11 -c -I../IniProcessor/ res_generator.cpp -o bin/res_generator.o -Wall
+g++ -std=c++11 -c -I../IniProcessor/ -I../Utf8Main/ res_generator.cpp -o bin/res_generator.o -Wall
 g++ -std=c++11 -c -I../IniProcessor/ ../IniProcessor/ini_processing.cpp -o bin/ini_processing.o -Wall
+g++ -std=c++11 -c -I../Utf8Main/ ../Utf8Main/utf8main_win32.cpp -o bin/utf8main_win32.o -Wall
 
-g++ bin/ini_processing.o bin/res_generator.o -o bin/res_generator
+g++ bin/ini_processing.o bin/res_generator.o bin/utf8main_win32.o -o bin/res_generator
 
 rem # Build resources!
-bin\res_generator.exe
+bin\res_generator.exe --config resource.ini "../../Engine/_resources/"
 
 rd /S /Q bin
