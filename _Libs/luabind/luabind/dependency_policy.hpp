9dfbc758cd2802e29963cd250c25d4a71091f7a6
Vitaly Novichkov <admin@wohlnet.ru>
2018-06-12 23:26:39 +0300

    LuaBind: Tiny fix of the "tabs vs spaces" issue [ci skip]

--- _Libs/luabind/luabind/dependency_policy.hpp
+++ _Libs/luabind/luabind/dependency_policy.hpp
@@ -38,7 +38,7 @@ namespace luabind {
 		struct dependency_policy
 		{
 			template< unsigned int... StackIndices >
-            static void postcall(lua_State* L, int /*results*/, meta::index_list<StackIndices...>)
+			static void postcall(lua_State* L, int /*results*/, meta::index_list<StackIndices...>)
 			{
 				object_rep* nurse = static_cast<object_rep*>(lua_touserdata(L, meta::get<meta::index_list<StackIndices...>, A>::value));
 
