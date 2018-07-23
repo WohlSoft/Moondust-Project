8bd46ccea5544d78d815534f0853d11ec72d1276
Wohlstand <admin@wohlnet.ru>
2018-07-23 03:54:46 +0300

    Travis-CI: Prepare OSX to use CMake build and deploy

--- _common/travis-ci/build_dependencies.sh
+++ _common/travis-ci/build_dependencies.sh
@@ -14,12 +14,13 @@ then
 elif [ $TRAVIS_OS_NAME == osx ];
 then
     source _common/travis-ci/_osx_env.sh
+    # == Dependencies pre-build removed since CMake build is in use ==
     # Don't build dependencies if there are cached
-    if [ ! -d _Libs/_builds/macos/lib ];
-    then
-        bash ./build_deps.sh no-pause
-    else
-        echo "Pre-built dependencies are already cached! Do nothing!"
-    fi
+    #if [ ! -d _Libs/_builds/macos/lib ];
+    #then
+    #    bash ./build_deps.sh no-pause
+    #else
+    #    echo "Pre-built dependencies are already cached! Do nothing!"
+    #fi
 fi
 
