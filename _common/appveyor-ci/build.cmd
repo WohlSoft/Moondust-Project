6f0e6a5ea076d2a410471a9d6b170920d0a3636b
Vitaly Novichkov <admin@wohlnet.ru>
2018-11-07 02:36:42 +0300

    AppVeyor: Build fix attempt №6 Deployment is missing! (reason: exit /B 0 from external bat file avoid any next code to be ran)

--- _common/appveyor-ci/build.cmd
+++ _common/appveyor-ci/build.cmd
@@ -22,33 +22,3 @@ echo CMakeDir=%CMakeDir%
 echo =============================
 
 build.bat nopause cmake-it ninja deploy
-
-if [%APPVEYOR_PULL_REQUEST_NUMBER%]==[] (
-    echo "Prepating artifacts..."
-    md %BIN_DIR%
-    md %BIN_DIR%\_packed
-    move bin-cmake-release\_packed\*.zip %BIN_DIR%\_packed
-) else (
-    echo "Pull-request detected, skipping artifacts..."
-)
-
-rem  == Disabled due next error: ==
-rem  'appveyor' is not recognized as an internal or external command,
-rem  operable program or batch file.
-rem  ==============================
-rem # Upload artifacts
-rem   - if [%APPVEYOR_PULL_REQUEST_NUMBER%]==[] appveyor PushArtifact "%BIN_DIR%\_packed\pge-project-%APPVEYOR_REPO_BRANCH%-%WINXX_ARCH%.zip"
-rem   - if [%APPVEYOR_PULL_REQUEST_NUMBER%]==[] appveyor PushArtifact "%BIN_DIR%\_packed\install-pge-common-%APPVEYOR_REPO_BRANCH%-%WINXX_ARCH%.zip"
-rem   - if [%APPVEYOR_PULL_REQUEST_NUMBER%]==[] appveyor PushArtifact "%BIN_DIR%\_packed\install-pge-editor-%APPVEYOR_REPO_BRANCH%-%WINXX_ARCH%.zip"
-rem   - if [%APPVEYOR_PULL_REQUEST_NUMBER%]==[] appveyor PushArtifact "%BIN_DIR%\_packed\install-pge-engine-%APPVEYOR_REPO_BRANCH%-%WINXX_ARCH%.zip"
-rem   - if [%APPVEYOR_PULL_REQUEST_NUMBER%]==[] appveyor PushArtifact "%BIN_DIR%\_packed\install-pge-tools-%APPVEYOR_REPO_BRANCH%-%WINXX_ARCH%.zip"
-rem #Standalone package of PGE Maintainer (work outside PGE)
-rem   - if [%APPVEYOR_PULL_REQUEST_NUMBER%]==[] appveyor PushArtifact "%BIN_DIR%\_packed\pge-maintainer-%APPVEYOR_REPO_BRANCH%-%WINXX_ARCH%.zip"
-rem #Standalone package of PGE MusPlay (work outside PGE)
-rem   - if [%APPVEYOR_PULL_REQUEST_NUMBER%]==[] appveyor PushArtifact "%BIN_DIR%\_packed\pge-musplay-%APPVEYOR_REPO_BRANCH%-%WINXX_ARCH%.zip"
-rem #Standalone package of Playable Character calibration tool (work outside PGE)
-rem   - if [%APPVEYOR_PULL_REQUEST_NUMBER%]==[] appveyor PushArtifact "%BIN_DIR%\_packed\pge-calibrator-%APPVEYOR_REPO_BRANCH%-%WINXX_ARCH%.zip"
-rem   - if [%APPVEYOR_PULL_REQUEST_NUMBER%]==[] appveyor PushArtifact "%BIN_DIR%\_packed\GIFs2PNG-%APPVEYOR_REPO_BRANCH%-%WINXX_ARCH%.zip"
-rem   - if [%APPVEYOR_PULL_REQUEST_NUMBER%]==[] appveyor PushArtifact "%BIN_DIR%\_packed\PNG2GIFs-%APPVEYOR_REPO_BRANCH%-%WINXX_ARCH%.zip"
-rem   - if [%APPVEYOR_PULL_REQUEST_NUMBER%]==[] appveyor PushArtifact "%BIN_DIR%\_packed\LazyFixTool-%APPVEYOR_REPO_BRANCH%-%WINXX_ARCH%.zip"
-rem   - if [%APPVEYOR_PULL_REQUEST_NUMBER%]==[] appveyor PushArtifact "build_date_%APPVEYOR_REPO_BRANCH%_%WINXX_ARCH%.txt"
