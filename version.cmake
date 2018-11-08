254a4015586fd9f1b54721cc38c597afd0d2cfda
Vitaly Novichkov <admin@wohlnet.ru>
2018-11-09 02:55:55 +0300

    Change PGE Project package version up to 0.4.2

--- version.cmake
+++ version.cmake
@@ -3,7 +3,7 @@ add_definitions(-DPGE_TOOLCHAIN_VERSION_1=0)
 # Minor
 add_definitions(-DPGE_TOOLCHAIN_VERSION_2=4)
 # Revision
-add_definitions(-DPGE_TOOLCHAIN_VERSION_3=1)
+add_definitions(-DPGE_TOOLCHAIN_VERSION_3=2)
 # Patch
 add_definitions(-DPGE_TOOLCHAIN_VERSION_4=0)
 
