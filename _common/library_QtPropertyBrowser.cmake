85fa9e087b5b141c3bc919064d394871f9a57d9c
Wohlstand <admin@wohlnet.ru>
2018-11-14 00:29:03 +0300

    Build: Pass CMAKE_PREFIX_PATH into QPB module QPB - QtPropertyBrowser

--- _common/library_QtPropertyBrowser.cmake
+++ _common/library_QtPropertyBrowser.cmake
@@ -8,6 +8,7 @@ if(PGE_ENABLE_QT) # Build if Qt support enabled
             DOWNLOAD_COMMAND ""
             SOURCE_DIR ${CMAKE_SOURCE_DIR}/_Libs/QtPropertyBrowser
             CMAKE_ARGS
+            "-DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}"
             "-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}"
             "-DCMAKE_INSTALL_PREFIX=${DEPENDENCIES_INSTALL_DIR}"
             "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
