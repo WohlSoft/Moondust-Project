f34d7fe732ca38bc7df960da3158448257e36242
Vitaly Novichkov <admin@wohlnet.ru>
2019-02-14 02:08:11 +0300

    Engine: Detect system language on Android

--- Engine/android-project/moondust/src/main/java/ru/wohlsoft/moondust/moondustActivity.java
+++ Engine/android-project/moondust/src/main/java/ru/wohlsoft/moondust/moondustActivity.java
@@ -1,6 +1,9 @@
 package ru.wohlsoft.moondust;
-
 import org.libsdl.app.SDLActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class moondustActivity extends SDLActivity
 {
@@ -10,5 +13,29 @@ public class moondustActivity extends SDLActivity
             "SDL2",
             "pge_engine"
         };
+    }
+
+    private String detectLanguage()
+    {
+        String lang = Locale.getDefault().toString();
+        String[] langD = lang.split("_");
+        if(langD.length >= 1)
+            return langD[0];
+        return "";
+    }
+
+    protected String[] getArguments()
+    {
+        List<String> args = new ArrayList<>();
+
+        // Detect current language of the system
+        String lang = detectLanguage();
+        if(lang.length() >= 1)
+            args.add("--lang=" + lang);
+
+        String[] argsOut = new String[args.size()];
+        args.toArray(argsOut);
+
+        return argsOut;
     }
 }
