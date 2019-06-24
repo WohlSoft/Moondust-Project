163d545f6b44309dff139eb25f76709172afeac0
Wohlstand <admin@wohlnet.ru>
2019-06-24 03:07:06 +0300

    Build: support for MSYS by bash builder `build.sh` and `generate_paths.sh` scripts is now possible to use from MSYS2 environment which previously didn't worked. Also, fixed the working of static Qt build on Windows.

--- generate_paths.sh
+++ generate_paths.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 cd $PWD
 
-if [[ "$OSTYPE" == "msys"* ]]; then
-   ./generate_paths.bat
-   exit 0
-fi
-
 MACHINE_TYPE=$(uname -m)
 GCC_ARCH=gcc_64
 CLANG_ARCH=clang_64
@@ -23,6 +18,7 @@ QT_VERSION=undefined/$GCC_ARCH
 QT_PATH=~/Qt/$QT_VERSION/bin/
 QT_LIB_PATH=~/Qt/$QT_VERSION/lib/
 AUTODETECTED=false
+STATIC_QT=false
 
 #At first detect globally installed Qt
 if [[ $QT_VERSION == "undefined/gcc_64" ]]; then
@@ -59,31 +55,44 @@ fi
 #Automatically detect manually installed Qt
 for var in 5.4 5.4.0 5.4.1 5.4.2 5.5 5.5.0 5.5.1 5.6 5.6.0 5.6.1 5.6.1-1 5.7 5.7.1 5.7.2 5.8 5.8.1 5.8.2 5.8.3 5.9 5.9.1 5.9.2 5.9.3 5.10 5.10.1 5.10.2 5.10.3 5.11 5.11.0 5.11.1 5.11.2 5.11.3 5.12 5.12.0 5.12.1 5.12.2 5.12.3 5.12.4 5.12.5 5.13 5.13.0 5.13.1 5.13.2 5.13.3 5.13.4 5.14 5.14.0 5.14.1 5.14.2 5.14.3 5.14.4 5.14.5 5.15 5.15.0 5.15.1 5.15.2 5.15.3 5.15.4 5.15.5 5.15.6
 do
-    if [ -f /opt/Qt/$var/$GCC_ARCH/bin/qmake ]; then
+    if [[ -f /opt/Qt/$var/$GCC_ARCH/bin/qmake ]]; then
         QT_VERSION=$var/$GCC_ARCH
         QT_PATH=~/Qt/$QT_VERSION/bin/
         QT_LIB_PATH=~/Qt/$QT_VERSION/lib/
         AUTODETECTED=true
-    fi
-    if [ -f ~/Qt/$var/$GCC_ARCH/bin/qmake ]; then
+    elif [[ -f ~/Qt/$var/$GCC_ARCH/bin/qmake ]]; then
         QT_VERSION=$var/$GCC_ARCH
         QT_PATH=~/Qt/$QT_VERSION/bin/
         QT_LIB_PATH=~/Qt/$QT_VERSION/lib/
         AUTODETECTED=true
-    fi
-    if [ -f /opt/Qt/$var/$CLANG_ARCH/bin/qmake ]; then
+    elif [[ -f /opt/Qt/$var/$CLANG_ARCH/bin/qmake ]]; then
         QT_VERSION=$var/$CLANG_ARCH
         QT_PATH=~/Qt/$QT_VERSION/bin/
         QT_LIB_PATH=~/Qt/$QT_VERSION/lib/
         AUTODETECTED=true
-    fi
-    if [ -f ~/Qt/$var/$CLANG_ARCH/bin/qmake ]; then
+    elif [[ -f ~/Qt/$var/$CLANG_ARCH/bin/qmake ]]; then
         QT_VERSION=$var/$CLANG_ARCH
         QT_PATH=~/Qt/$QT_VERSION/bin/
         QT_LIB_PATH=~/Qt/$QT_VERSION/lib/
         AUTODETECTED=true
     fi
 done
+
+if [[ "$OSTYPE" == "msys"* ]]; then
+    if [[ -f /mingw64/qt5-static/bin/qmake ]]; then
+        QT_VERSION=mingw64/qt5-static
+        QT_PATH=/mingw64/qt5-static/bin/
+        QT_LIB_PATH=/mingw64/qt5-static/lib/
+        AUTODETECTED=true
+        STATIC_QT=true
+    elif [[ -f /mingw32/qt5-static/bin/qmake ]]; then
+        QT_VERSION=mingw32/qt5-static
+        QT_PATH=/mingw32/qt5-static/bin/
+        QT_LIB_PATH=/mingw32/qt5-static/lib/
+        AUTODETECTED=true
+        STATIC_QT=true
+    fi
+fi
 
 OPEN_GEDIT=true
 for var in "$@"
@@ -103,6 +112,7 @@ do
                         QT_PATH=~/Qt/$QT_VERSION/bin/
                         QT_LIB_PATH=~/Qt/$QT_VERSION/lib/
                         AUTODETECTED=true
+                        STATIC_QT=true
                     fi
                 done
                 #QT_PATH=/home/runner/Qt/$QT_VERSION/bin/
@@ -129,6 +139,11 @@ echo "" >> _paths.sh
 echo "#---------lrelease executable name or full path------------" >> _paths.sh
 echo "LRelease=\"$LRELEASE_PATH\";" >> _paths.sh
 echo "" >> _paths.sh
+if $STATIC_QT; then
+    echo "#---------Force using of Static Qt build mode------------" >> _paths.sh
+    echo "flag_cmake_static_qt=true;" >> _paths.sh
+    echo "" >> _paths.sh
+fi
 echo "#===============================================================================================" >> _paths.sh
 echo "#===============================================================================================" >> _paths.sh
 echo "#===============================================================================================" >> _paths.sh
@@ -141,6 +156,9 @@ if $AUTODETECTED ; then
         $QMAKE_PATH -v
     elif [ -f $QT_PATH$QMAKE_PATH ]; then
         $QT_PATH$QMAKE_PATH -v
+    fi
+    if $STATIC_QT; then
+        echo " -- DETECTED STATIC BUILD --"
     fi
     echo "================================================================"
 
