57ef408aa9189d8c26ab77dc5e14ebfad8dc52b7
Wohlstand <admin@wohlnet.ru>
2019-09-23 12:56:38 +0300

    Build: Set "-no-pie" to all executables

--- _common/executable_build_props.cmake
+++ _common/executable_build_props.cmake
@@ -1,3 +1,15 @@
+include(CheckCXXCompilerFlag)
+
+if(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX)
+    check_cxx_compiler_flag("-no-pie" HAS_NO_PIE)
+endif()
+
+function(pge_set_nopie _target)
+    if(HAS_NO_PIE AND CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX)
+        set_property(TARGET ${_target} APPEND_STRING PROPERTY LINK_FLAGS " -no-pie")
+    endif()
+endfunction()
+
 # WORKAROUND: Avoid ImpLib file creation on a non-library project
 if(CYGWIN OR MINGW)
     # Prune --out-implib from executables. It doesn't make sense even
@@ -7,3 +19,4 @@ if(CYGWIN OR MINGW)
     string(REGEX REPLACE "-Wl,--out-implib,[^ ]+ " " "
         CMAKE_CXX_LINK_EXECUTABLE "${CMAKE_CXX_LINK_EXECUTABLE}")
 endif()
+
