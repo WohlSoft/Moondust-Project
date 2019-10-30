6325c58909d3506db25f46b93e963c7da384fa6c
Wohlstand <admin@wohlnet.ru>
2019-10-30 14:31:41 +0300

    Build: Use CMAKE_*_VERSION for PGE package version number

--- version.cmake
+++ version.cmake
@@ -1,14 +1,20 @@
+set(CMAKE_MAJOR_VERSION 0)
+set(CMAKE_MINOR_VERSION 4)
+set(CMAKE_PATCH_VERSION 2)
+set(CMAKE_TWEAK_VERSION 0)
+
 # Major
-add_definitions(-DPGE_TOOLCHAIN_VERSION_1=0)
+add_definitions(-DPGE_TOOLCHAIN_VERSION_1=${CMAKE_MAJOR_VERSION})
 # Minor
-add_definitions(-DPGE_TOOLCHAIN_VERSION_2=4)
+add_definitions(-DPGE_TOOLCHAIN_VERSION_2=${CMAKE_MINOR_VERSION})
 # Revision
-add_definitions(-DPGE_TOOLCHAIN_VERSION_3=2)
+add_definitions(-DPGE_TOOLCHAIN_VERSION_3=${CMAKE_PATCH_VERSION})
 # Patch
-add_definitions(-DPGE_TOOLCHAIN_VERSION_4=0)
+add_definitions(-DPGE_TOOLCHAIN_VERSION_4=${CMAKE_TWEAK_VERSION})
 
 # Config pack API version
 add_definitions(-DPGE_CONFIG_PACK_API_VERSION=41)
 
 # Type of version: "-alpha","-beta","-dev", or "" aka "release"
 add_definitions(-DPGE_TOOLCHAIN_VERSION_REL="")
+
