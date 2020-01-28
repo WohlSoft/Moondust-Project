51989b9365ebdcc92ab29c272025c5856aa43c1c
Wohlstand <admin@wohlnet.ru>
2020-01-29 01:03:01 +0300

    Maintainer: Initial work on CaseFixer worker

--- Maintainer/EpisodeCvt/case_fixer.cpp
+++ Maintainer/EpisodeCvt/case_fixer.cpp
@@ -1,11 +1,98 @@
 #include "case_fixer.h"
 #include "ui_case_fixer.h"
+#include <QtConcurrent>
+#include <QMessageBox>
+
+
+
+CaseFixerWorker::CaseFixerWorker(QObject *parent):
+    QObject(parent),
+    m_mode(MODE_TOLOWER),
+    m_jobRunning(false),
+    m_currentValue(0),
+    m_isFine(false)
+{
+
+}
+
+CaseFixerWorker::~CaseFixerWorker()
+{
+
+}
+
+bool CaseFixerWorker::initJob(QString configPack, QString episodePath, bool recursive, int targetMode)
+{
+    m_filesToConvert.clear();
+    m_errorString.clear();
+    m_episode.setPath(episodePath);
+    m_mode = targetMode;
+    if(!m_episode.setCurrent(episodePath))
+    {
+        m_errorString = tr("Can't resolve path \"%1\"").arg(episodePath);
+        return false;
+    }
+
+    m_episodeBox.openEpisode(episodePath, recursive);
+
+    if(m_episodeBox.totalElements()==0)
+    {
+        m_errorString = tr("No files to convert");
+        return false;
+    }
+
+    emit totalElements(m_episodeBox.totalElements());
+
+    return true;
+}
+
+bool CaseFixerWorker::runJob()
+{
+    m_jobRunning = true;
+    m_isFine = false;
+
+    try
+    {
+        // TODO: Put code HERE
+    }
+    catch(QString err)
+    {
+        m_errorString = "Error ocouped while conversion process: " + err;
+        emit workFinished(false);
+        m_isFine = false;
+    }
+
+    m_jobRunning = false;
+    qDebug() << "Work done, exiting";
+    emit workFinished(true);
+    return m_isFine;
+}
+
+QString CaseFixerWorker::errorString()
+{
+    return m_errorString;
+}
+
+
+
 
 CaseFixer::CaseFixer(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::CaseFixer)
 {
     ui->setupUi(this);
+
+    ui->progress->reset();
+
+    connect(&m_progressWatcher, &QTimer::timeout,
+                   this, &CaseFixer::refreshProgressBar,
+                   Qt::QueuedConnection);
+
+    connect(&m_worker, &CaseFixerWorker::totalElements,
+                   ui->progress, &QProgressBar::setMaximum,
+                   Qt::QueuedConnection);
+
+    connect(&m_worker, &CaseFixerWorker::workFinished,
+            this, &CaseFixer::workFinished, Qt::QueuedConnection);
 }
 
 CaseFixer::~CaseFixer()
@@ -21,4 +108,25 @@ void CaseFixer::on_episodeBrowse_clicked()
 void CaseFixer::on_configPackBrowse_clicked()
 {
 
+}
+
+void CaseFixer::workFinished(bool isFine)
+{
+    m_process.waitForFinished();
+    m_progressWatcher.stop();
+    ui->progress->reset();
+    emit setLocked(false);
+
+    if(isFine)
+        QMessageBox::information(this, tr("Work finished"), tr("Case sensitive fixing work has been converted!"));
+    else
+        QMessageBox::warning(this,
+                             tr("Error occouped while work process"),
+                             tr("Case fixer was aborted with an error: %1")
+                             .arg(m_worker.errorString()));
+}
+
+void CaseFixer::refreshProgressBar()
+{
+    ui->progress->setValue(m_worker.m_currentValue);
 }
--- Maintainer/EpisodeCvt/case_fixer.h
+++ Maintainer/EpisodeCvt/case_fixer.h
@@ -2,10 +2,55 @@
 #define CASE_FIXER_H
 
 #include <QDialog>
+#include <QTimer>
+#include <QFuture>
+#include <QDir>
+#include <QQueue>
+#include <QAtomicInteger>
+
+#include <files/episode_box.h>
 
 namespace Ui {
 class CaseFixer;
 }
+
+class CaseFixer;
+class CaseFixerWorker : public QObject
+{
+    Q_OBJECT
+    QDir                m_episode;
+    QString             m_errorString;
+    QQueue<QString >    m_filesToConvert;
+    int                 m_mode;
+    EpisodeBox          m_episodeBox;
+    QAtomicInteger<bool>m_jobRunning;
+
+public:
+    QAtomicInteger<int> m_currentValue;
+    bool                m_isFine;
+
+    enum Mode
+    {
+        MODE_TOLOWER = 0,
+        MODE_MATCH_FS,
+        MODE_MATCH_SETUP
+    };
+
+    explicit CaseFixerWorker(QObject*parent = nullptr);
+    ~CaseFixerWorker();
+
+    bool initJob(QString configPack,
+                 QString episodePath,
+                 bool recursive,
+                 int targetMode);
+    bool runJob();
+
+    QString errorString();
+
+signals:
+    void workFinished(bool isFine);
+    void totalElements(int max);
+};
 
 class CaseFixer : public QDialog
 {
@@ -18,9 +63,17 @@ public:
 private slots:
     void on_episodeBrowse_clicked();
     void on_configPackBrowse_clicked();
+    void workFinished(bool isFine);
+    void refreshProgressBar();
+
+signals:
+    void setLocked(bool);
 
 private:
     Ui::CaseFixer *ui;
+    QFuture<bool>   m_process;
+    CaseFixerWorker m_worker;
+    QTimer          m_progressWatcher;
 };
 
 #endif // CASE_FIXER_H
