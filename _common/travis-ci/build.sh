6cede17106b01083229e18ea16a66d08a3ecd7b8
Wohlstand <admin@wohlnet.ru>
2020-05-25 00:43:37 +0300

    Build: Store build date file at the REAL home to don't confuse an upload script

--- _common/travis-ci/build.sh
+++ _common/travis-ci/build.sh
@@ -15,7 +15,7 @@ then
     #Don't do duplicated build! (it already proceeded by Coverity plugin!)
     if [[ "${IS_COVERITY_SCAN}" == "" ]] ;
     then
-        TZ=Europe/Moscow date +"%Y-%m-%d %H:%M:%S" > /home/runner/build_date_${GIT_BRANCH}_${UPLOAD_VERSION_SUFFIX}.txt
+        TZ=Europe/Moscow date +"%Y-%m-%d %H:%M:%S" > $HOME/build_date_${GIT_BRANCH}_${UPLOAD_VERSION_SUFFIX}.txt
         bash build.sh no-pause ninja use-ccache deploy static-qt portable
     fi
 elif [[ "$TRAVIS_OS_NAME" == "osx" ]];
