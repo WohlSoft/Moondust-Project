70fd8d7bef61d36d3cea7008e43fd72fc9dacf49
Wohlstand <admin@wohlnet.ru>
2020-05-28 00:45:28 +0300

    Build: Fixed the LFTP deb download URL

--- _common/travis-ci/before-script.sh
+++ _common/travis-ci/before-script.sh
@@ -29,7 +29,7 @@ then
 
     if [[ "${CI_NEED_LFTP_U1604}" != "" ]]; then
         echo "Downloading a custom lftp build for Ubuntu 16.04..."
-        wget --quiet http://wohlsoft.ru/www/docs/Software/lftp_4.9.1-1_amd64-ubuntu1604.deb -O /home/runner/lftp_4.9.1-1_amd64.deb
+        wget --quiet http://wohlsoft.ru/docs/Software/lftp_4.9.1-1_amd64-ubuntu1604.deb -O /home/runner/lftp_4.9.1-1_amd64.deb
         echo "Installing custom lftp..."
         sudo dpkg -i /home/runner/lftp_4.9.1-1_amd64.deb
     fi
