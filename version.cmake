59223f6b75049eb7bf4d7dd26b8a8cccc866fad3
Wohlstand <admin@wohlnet.ru>
2021-03-30 21:26:59 +0300

    Build: Fixed the conflict with the latest SDL2

--- version.cmake
+++ version.cmake
@@ -1,16 +1,16 @@
-set(CMAKE_MAJOR_VERSION 0)
-set(CMAKE_MINOR_VERSION 4)
-set(CMAKE_PATCH_VERSION 3)
-set(CMAKE_TWEAK_VERSION 1)
+set(Moondust_MAJOR_VERSION 0)
+set(Moondust_MINOR_VERSION 4)
+set(Moondust_PATCH_VERSION 3)
+set(Moondust_TWEAK_VERSION 1)
 
 # Major
-add_definitions(-DPGE_TOOLCHAIN_VERSION_1=${CMAKE_MAJOR_VERSION})
+add_definitions(-DPGE_TOOLCHAIN_VERSION_1=${Moondust_MAJOR_VERSION})
 # Minor
-add_definitions(-DPGE_TOOLCHAIN_VERSION_2=${CMAKE_MINOR_VERSION})
+add_definitions(-DPGE_TOOLCHAIN_VERSION_2=${Moondust_MINOR_VERSION})
 # Revision
-add_definitions(-DPGE_TOOLCHAIN_VERSION_3=${CMAKE_PATCH_VERSION})
+add_definitions(-DPGE_TOOLCHAIN_VERSION_3=${Moondust_PATCH_VERSION})
 # Patch
-add_definitions(-DPGE_TOOLCHAIN_VERSION_4=${CMAKE_TWEAK_VERSION})
+add_definitions(-DPGE_TOOLCHAIN_VERSION_4=${Moondust_TWEAK_VERSION})
 
 # Config pack API version
 add_definitions(-DPGE_CONFIG_PACK_API_VERSION=41)
