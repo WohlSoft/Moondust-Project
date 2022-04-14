fc3cfa7f2537d6d827081a3814e33db8662dbaa3
迎春心情 <lovebodhi@foxmail.com>
2022-04-14 10:03:47 +0000

    Translations: Weblate side updates (Chinese (Simplified))
    Currently translated at 100.0% (2128 of 2128 strings)
    
    Translation: Moondust Project/Editor
    Translate-URL: https://hosted.weblate.org/projects/moondust-project/editor/zh_Hans/

--- Editor/languages/editor_zh.ts
+++ Editor/languages/editor_zh.ts
@@ -10523,5 +10523,41 @@ Right-click to remove!</source>
 </context>
 <context>
     <name></name>
+    <message>
+        <source>Can&apos;t start the utility, Moondust Maintainer is not found: 
+%1
+Please, choose the Moondust Maintainer application yourself!</source>
+        <translation>未找到月尘引擎管理器，无法启动工具：
+%1
+请选择月尘引擎管理器文件夹！</translation>
+    </message>
+    <message>
+        <source>While making a report, please explain what you did to cause a crash error.
+- Please give a list of actions you did to cause a crash. 
+- If a crash happened with editing a specific file, please attach it to the report.
+- What operating system you are using?</source>
+        <translation>发送报告之前，请简述您是如何操作造成的程序异常终止。
+- 请列出造成程序异常终止的行为。
+- 如果是编辑指定文件造成的异常终止，请附上相关文件。
+- 您使用的操作系统？</translation>
+    </message>
+    <message>
+        <source>We&apos;re sorry, but PGE Editor has crashed. 
+Reason: %1
+
+</source>
+        <translation>非常抱歉，月尘引擎编辑器遭到异常终止。
+原因：%1
+
+</translation>
+    </message>
+    <message>
+        <source>Show a blank screen instead of loading screen.
+In the Legacy Engine level loading screen, it would otherwise 
+show the current playable character(s) and number of lives.</source>
+        <translation>显示黑屏而非加载关卡时的界面。
+在 SMBX 1.3 引擎中，加载关卡时的界面将
+显示当前人物和剩余生命数。</translation>
+    </message>
 </context>
 </TS>
