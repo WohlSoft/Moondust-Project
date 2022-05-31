652042a4f015f58519b1134c6baed1d9c299374e
Hosted Weblate <hosted@weblate.org>
2022-05-31 09:13:28 +0200

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: Moondust Project/Editor
    Translate-URL: https://hosted.weblate.org/projects/moondust-project/editor/

--- Editor/languages/editor_zh.ts
+++ Editor/languages/editor_zh.ts
@@ -10523,13 +10523,5 @@ Right-click to remove!</source>
 </context>
 <context>
     <name></name>
-    <message>
-        <source>Show a blank screen instead of loading screen.
-In the Legacy Engine level loading screen, it would otherwise 
-show the current playable character(s) and number of lives.</source>
-        <translation>显示黑屏而非加载关卡时的界面。
-在 SMBX 1.3 引擎中，加载关卡时的界面将
-显示当前人物和剩余生命数。</translation>
-    </message>
 </context>
 </TS>
