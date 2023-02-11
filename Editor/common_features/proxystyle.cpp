c048ac1c1d2387312beb5827f015c74153cac0db
Wohlstand <admin@wohlnet.ru>
2023-02-11 06:49:06 +0300

    Editor: Beautify the proxystyle.cpp

--- Editor/common_features/proxystyle.cpp
+++ Editor/common_features/proxystyle.cpp
@@ -19,93 +19,97 @@
 #include <QDockWidget>
 #include <QAbstractItemView>
 #include <QComboBox>
+#include <QStyleFactory>
 
 #include "proxystyle.h"
 
-PGE_ProxyStyle::PGE_ProxyStyle(QStyle *style) : QProxyStyle(style) {}
+PGE_ProxyStyle::PGE_ProxyStyle(QStyle* style) : QProxyStyle(style) {}
 
-PGE_ProxyStyle::PGE_ProxyStyle(const QString &key) : QProxyStyle(key) {}
+PGE_ProxyStyle::PGE_ProxyStyle(const QString& key) : QProxyStyle(key) {}
 
 PGE_ProxyStyle::~PGE_ProxyStyle() {}
 
-int PGE_ProxyStyle::styleHint(QStyle::StyleHint hint, const QStyleOption *option, const QWidget *widget, QStyleHintReturn *returnData) const
+int PGE_ProxyStyle::styleHint(QStyle::StyleHint hint, const QStyleOption* option, const QWidget* widget, QStyleHintReturn* returnData) const
 {
     switch(hint)
     {
-        //Combobox fix by Guilherme Nascimento:
-        //http://stackoverflow.com/questions/20554940/qcombobox-pop-up-expanding-and-qtwebkit
-        case QStyle::SH_ComboBox_Popup:
+    //Combobox fix by Guilherme Nascimento:
+    //http://stackoverflow.com/questions/20554940/qcombobox-pop-up-expanding-and-qtwebkit
+    case QStyle::SH_ComboBox_Popup:
+    {
+        const QComboBox* combo = (QComboBox*) widget; //convert qwidget in QComboBox
+        const QObjectList a = combo->children();
+        const int j = a.count();
+        QAbstractItemView* view = 0;
+        QString className;
+        bool hasView = false;
+
+        /*
+            at this point I have not used combo->view() because he "crash" the application without explanation
+            so I had to make a loop to find the "view"
+            */
+        for(int i = 0; i < j; ++i)
         {
-            const QComboBox *combo = (QComboBox *) widget;//convert qwidget in QComboBox
-            const QObjectList a = combo->children();
-            const int j = a.count();
-            QAbstractItemView *view = 0;
-            QString className = "";
-            bool hasView = false;
+            const QObjectList b = a.at(i)->children();
+            const int y = b.count();
 
-            /*
-                at this point I have not used combo->view() because he "crash" the application without explanation
-                so I had to make a loop to find the "view"
-                */
-            for(int i = 0; i < j; ++i)
+            for(int x = 0; x < y; ++x)
             {
-                const QObjectList b = a.at(i)->children();
-                const int y = b.count();
+                className = b.at(x)->metaObject()->className();
 
-                for(int x = 0; x < y; ++x)
+                if(className == "QComboBoxListView")
                 {
-                    className = b.at(x)->metaObject()->className();
-                    if(className == "QComboBoxListView") {
-                        view = (QAbstractItemView *) b.at(x);
-                        hasView = true;
-                        break;
-                    }
-                }
-
-                if (hasView == true)
-                {
+                    view = (QAbstractItemView*) b.at(x);
+                    hasView = true;
                     break;
                 }
             }
 
-            if(hasView == true)
+            if(hasView)
+                break;
+        }
+
+        if(hasView)
+        {
+            const int iconSize = combo->iconSize().width();
+            const QFontMetrics fontMetrics1 = view->fontMetrics();
+            const QFontMetrics fontMetrics2 = combo->fontMetrics();
+            const int j = combo->count();
+            int width = combo->width(); //default width
+
+            for(int i = 0; i < j; ++i)
             {
-                const int iconSize = combo->iconSize().width();
-                const QFontMetrics fontMetrics1 = view->fontMetrics();
-                const QFontMetrics fontMetrics2 = combo->fontMetrics();
-                const int j = combo->count();
-                int width = combo->width(); //default width
-
-                for(int i=0; i < j; ++i)
-                {
 #if QT_VERSION >= QT_VERSION_CHECK(5, 11, 0)
-                    const int textWidth = qMax(
-                                fontMetrics1.horizontalAdvance(combo->itemText(i) + "WW"),
-                                fontMetrics2.horizontalAdvance(combo->itemText(i) + "WW")
-                                );
+                const int textWidth = qMax(
+                                          fontMetrics1.horizontalAdvance(combo->itemText(i) + "WW"),
+                                          fontMetrics2.horizontalAdvance(combo->itemText(i) + "WW")
+                                      );
 #else
-                    const int textWidth = qMax(
-                                fontMetrics1.width(combo->itemText(i) + "WW"),
-                                fontMetrics2.width(combo->itemText(i) + "WW")
-                                );
+                const int textWidth = qMax(
+                                          fontMetrics1.width(combo->itemText(i) + "WW"),
+                                          fontMetrics2.width(combo->itemText(i) + "WW")
+                                      );
 #endif
-                    if(combo->itemIcon(i).isNull())
-                        width = qMax(width, textWidth);
-                    else
-                        width = qMax(width, textWidth + iconSize);
-                }
 
-                view->setMinimumWidth(width);
+                if(combo->itemIcon(i).isNull())
+                    width = qMax(width, textWidth);
+                else
+                    width = qMax(width, textWidth + iconSize);
             }
+
+            view->setMinimumWidth(width);
+        }
         } /*fallthrough*/
-        //Combobox fix end
 
-        case QStyle::SH_MenuBar_AltKeyNavigation:
-            return 0;//Prevent menu bar steal focus
+    //Combobox fix end
 
-        default:
-            break;
+    case QStyle::SH_MenuBar_AltKeyNavigation:
+        return 0;//Prevent menu bar steal focus
+
+    default:
+        break;
     }
+
     return QProxyStyle::styleHint(hint, option, widget, returnData);
 }
 
