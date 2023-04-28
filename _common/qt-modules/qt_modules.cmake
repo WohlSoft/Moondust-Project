317e3575cfc18ea6da6d0afa2eb92f47adf9376c
Wohlstand <admin@wohlnet.ru>
2023-04-28 21:57:27 +0300

    Make MsgBoxPreview being shared module

--- _common/qt-modules/qt_modules.cmake
+++ _common/qt-modules/qt_modules.cmake
@@ -1,10 +1,12 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 include(${CMAKE_CURRENT_LIST_DIR}/file_list_browser/file_list_browser.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/custom_music_setup/custom_music_setup.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/msg_box_preview/msg_box_preview.cmake)
 
 set(MOONDUST_QT_MODULES_SRC
     ${FILE_LIST_BROWSER_SRC}
     ${CUSTOM_MUSIC_SETUP_SRC}
+    ${MSG_BOX_PREVIEW_SRC}
 )
 
 set(MOONDUST_QT_MODULES_UI
