7b7dd3b8fc01807a28501ddf602545b38cc88d90
Wohlstand <admin@wohlnet.ru>
2023-08-09 01:26:36 +0300

    Editor: Clean-up of the "Tip of Day" dialogue
    - Repleaced qrand() with C++11 random
    - Fixed the code style

--- Editor/main_window/tip_of_day/tip_of_day.cpp
+++ Editor/main_window/tip_of_day/tip_of_day.cpp
@@ -1,3 +1,21 @@
+/*
+ * Platformer Game Engine by Wohlstand, a free platform for game making
+ * Copyright (c) 2014-2023 Vitaly Novichkov <admin@wohlnet.ru>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "tip_of_day.h"
 #include "ui_tip_of_day.h"
 #include <QFile>
@@ -6,28 +24,35 @@
 #include <QDateTime>
 #include <main_window/global_settings.h>
 
-TipOfDay::TipOfDay(QWidget *parent) :
+
+TipOfDay::TipOfDay(QWidget* parent) :
     QDialog(parent),
+    m_mt(m_rd()),
+    m_rand(0, RAND_MAX),
     ui(new Ui::TipOfDay)
 {
-    qsrand(QDateTime::currentMSecsSinceEpoch());
     ui->setupUi(this);
+
     QFile ftips(":/tips/tips/tips_en.html");
-    ftips.open(QIODevice::ReadOnly|QIODevice::Text);
+
+    ftips.open(QIODevice::ReadOnly | QIODevice::Text);
     QTextStream ts(&ftips);
     QString allTipsRaw = ts.readAll();
     ftips.close();
+
     ui->showEachStart->setChecked(GlobalSettings::ShowTipOfDay);
-    curTip=0;
+    curTip = 0;
+
     tips.clear();
     tips = allTipsRaw.split("<hr>\n");
+
     ui->prevTip->setEnabled(false);
     on_nextTip_clicked();
 }
 
 TipOfDay::~TipOfDay()
 {
-    GlobalSettings::ShowTipOfDay=ui->showEachStart->isChecked();
+    GlobalSettings::ShowTipOfDay = ui->showEachStart->isChecked();
     delete ui;
 }
 
@@ -36,28 +61,31 @@ void TipOfDay::on_prevTip_clicked()
     curTip--;
     ui->tipText->setText(tips_viewed[curTip]);
 
-    ui->prevTip->setEnabled(curTip>0);
-    ui->nextTip->setEnabled(  (curTip<tips_viewed.size()-1) || (!tips.isEmpty()) );
+    ui->prevTip->setEnabled(curTip > 0);
+    ui->nextTip->setEnabled((curTip < tips_viewed.size() - 1) || (!tips.isEmpty()));
 }
 
 void TipOfDay::on_nextTip_clicked()
 {
     QString tip;
-    if((curTip>=(tips_viewed.size()-1))||tips_viewed.isEmpty())
+
+    if((curTip >= (tips_viewed.size() - 1)) || tips_viewed.isEmpty())
     {
         int random_selector;
         int n = tips.size();
-        random_selector = (qrand()%n);
-        tip=tips[random_selector];
+        random_selector = (m_rand(m_mt) % n);
+        tip = tips[random_selector];
         ui->tipText->setText(tip);
         tips_viewed.append(tip);
-        curTip=tips_viewed.size()-1;
+        curTip = tips_viewed.size() - 1;
         tips.removeAt(random_selector);
-    } else {
+    }
+    else
+    {
         curTip++;
         ui->tipText->setText(tips_viewed[curTip]);
     }
 
-    ui->prevTip->setEnabled(curTip>0);
-    ui->nextTip->setEnabled(  (curTip<tips_viewed.size()-1) || (!tips.isEmpty()) );
+    ui->prevTip->setEnabled(curTip > 0);
+    ui->nextTip->setEnabled((curTip < tips_viewed.size() - 1) || (!tips.isEmpty()));
 }
--- Editor/main_window/tip_of_day/tip_of_day.h
+++ Editor/main_window/tip_of_day/tip_of_day.h
@@ -1,7 +1,26 @@
+/*
+ * Platformer Game Engine by Wohlstand, a free platform for game making
+ * Copyright (c) 2014-2023 Vitaly Novichkov <admin@wohlnet.ru>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #pragma once
 #ifndef TIP_OF_DAY_H
 #define TIP_OF_DAY_H
 
+#include <random>
 #include <QDialog>
 #include <QStringList>
 
@@ -13,8 +32,12 @@ class TipOfDay : public QDialog
 {
     Q_OBJECT
 
+    std::random_device m_rd;
+    std::mt19937 m_mt;
+    std::uniform_int_distribution<int> m_rand;
+
 public:
-    explicit TipOfDay(QWidget *parent = 0);
+    explicit TipOfDay(QWidget *parent = nullptr);
     ~TipOfDay();
 
 private slots:
