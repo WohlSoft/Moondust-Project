70b922ec58249bd2fc65dd9f9a6aed7e6e6296c5
Wohlstand <admin@wohlnet.ru>
2023-08-17 19:42:51 +0300

    Editor: Fixed digit font printing

--- Editor/common_features/graphics_funcs.cpp
+++ Editor/common_features/graphics_funcs.cpp
@@ -346,7 +346,7 @@ void GraphicsHelps::squareImageR(QPixmap &imageInOut, QSize targetSize)
 
 QPixmap GraphicsHelps::drawDegitFont(int number)
 {
-    QPixmap font = QPixmap(":/fonts/digits.png");
+    QPixmap font = QPixmap(":/fonts/font_digits.png");
     QString text = QString::number(number);
     QPixmap img(text.size() * 18, 16);
     img.fill(Qt::transparent);
