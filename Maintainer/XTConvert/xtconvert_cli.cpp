fd002d76fbd7d465923abc72065d8f7de9beece6
ds-sloth <72112344+ds-sloth@users.noreply.github.com>
2024-11-30 13:21:55 -0500

    XTConvert: add mus2qoa audio conversion

--- Maintainer/XTConvert/xtconvert_cli.cpp
+++ Maintainer/XTConvert/xtconvert_cli.cpp
@@ -3,10 +3,13 @@
 int main(int argc, char** argv)
 {
     XTConvert::Spec s;
-    s.target_platform = XTConvert::TargetPlatform::TPL;
+    s.target_platform = XTConvert::TargetPlatform::DSG;
+
+    s.package_type = XTConvert::PackageType::Episode;
 
     s.input_dir = argv[1];
-    s.destination = argv[2];
+    s.use_assets_dir = argv[2];
+    s.destination = argv[3];
 
     return !XTConvert::Convert(s);
 }
