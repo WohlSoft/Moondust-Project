90561c5390bb1ddac539b609d58db93eed86c9dd
ds-sloth <72112344+ds-sloth@users.noreply.github.com>
2025-01-12 21:45:55 -0500

    XTConvertUI: first worker thread implementation

--- Maintainer/XTConvert/xt_convert.cpp
+++ Maintainer/XTConvert/xt_convert.cpp
@@ -23,19 +23,102 @@
 #include <QMessageBox>
 #include <QCloseEvent>
 #include <common_features/app_path.h>
-#include <QtConcurrent>
 #include <QStandardPaths>
 
 #include <QtDebug>
 
 #include "qfile_dialogs_default_options.hpp"
 
+static void log_file_callback_ext(void* userdata, XTConvert::LogCategory log_category, const std::string& filename)
+{
+    ((XTConvert_Worker*)userdata)->log_file_callback(log_category, filename);
+}
+
+static int progress_callback_ext(void* userdata, int cur_stage, int stage_count, const std::string& stage_name, int cur_file, int file_count, const std::string& file_name)
+{
+    return ((XTConvert_Worker*)userdata)->progress_callback(cur_stage, stage_count, stage_name, cur_file, file_count, file_name);
+}
+
+void XTConvert_Worker::log_file_callback(XTConvert::LogCategory log_category, const std::string& filename)
+{
+    XTConvertUpdate res;
+    res.log_category = log_category;
+    res.file_name = QString::fromStdString(filename);
+
+    qInfo() << res.file_name;
+
+    emit status_update(res);
+}
+
+int XTConvert_Worker::progress_callback(int cur_stage, int stage_count, const std::string& stage_name, int cur_file, int file_count, const std::string& file_name)
+{
+    XTConvertUpdate res;
+
+    res.cur_stage = cur_stage;
+    res.stage_count = stage_count;
+    res.stage_name = QString::fromStdString(stage_name);
+    res.cur_file = cur_file;
+    res.file_count = file_count;
+    res.file_name = QString::fromStdString(file_name);
+
+    emit status_update(res);
+
+    QEventLoop loop;
+    loop.processEvents();
+
+    if(is_cancelled)
+        return -1;
+
+    return 0;
+}
+
+void XTConvert_Worker::do_cancel()
+{
+    if(is_running)
+        is_cancelled = true;
+    else
+        emit finish();
+}
+
+void XTConvert_Worker::do_run(XTConvert::Spec spec)
+{
+    if(is_running)
+        return;
+
+    is_running = true;
+    is_cancelled = false;
+
+    spec.log_file_callback = log_file_callback_ext;
+    spec.progress_callback = progress_callback_ext;
+    spec.callback_userdata = this;
+
+    XTConvert::Convert(spec);
+
+    emit finish();
+
+    is_running = false;
+}
 
 XTConvertUI::XTConvertUI(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::XTConvertUI)
 {
     ui->setupUi(this);
+
+    auto worker_p = new XTConvert_Worker();
+    worker.reset(worker_p);
+
+    qRegisterMetaType<XTConvert::Spec>();
+    qRegisterMetaType<XTConvertUpdate>();
+
+    connect(this, &XTConvertUI::do_run, worker_p, &XTConvert_Worker::do_run);
+    connect(this, &XTConvertUI::do_cancel, worker_p, &XTConvert_Worker::do_cancel);
+
+    connect(worker_p, &XTConvert_Worker::status_update, this, &XTConvertUI::on_status_update);
+    connect(worker_p, &XTConvert_Worker::finish, this, &XTConvertUI::on_finish);
+
+    worker->moveToThread(&process_thread);
+    process_thread.start();
 
     m_in_progress = false;
     m_recent_content_path = ApplicationPath;
@@ -47,6 +130,11 @@ XTConvertUI::XTConvertUI(QWidget *parent) :
 XTConvertUI::~XTConvertUI()
 {
     delete ui;
+
+    process_thread.quit();
+    process_thread.wait();
+
+    worker.reset();
 }
 
 void XTConvertUI::updateControls()
@@ -81,13 +169,42 @@ void XTConvertUI::start()
     ui->progress->setValue(-1);
 
     ui->progress->setFormat("Preparing...");
+
+    XTConvert::Spec spec;
+
+    QString content_path = ui->content_path->text();
+
+    if(content_path.endsWith("gameinfo.ini") || content_path.endsWith(".wld") || content_path.endsWith(".wldx"))
+        content_path = QFileInfo(content_path).dir().absolutePath();
+
+    spec.input_path = content_path.toStdString();
+    spec.destination = ui->output_path->text().toStdString();
+
+    if(m_target_asset_pack)
+        spec.package_type = XTConvert::PackageType::AssetPack;
+    else
+        spec.package_type = XTConvert::PackageType::Episode;
+
+    if(ui->target_platform->currentText() == "Mainline")
+        spec.target_platform = XTConvert::TargetPlatform::Desktop;
+    else if(ui->target_platform->currentText() == "3DS")
+        spec.target_platform = XTConvert::TargetPlatform::T3X;
+    else if(ui->target_platform->currentText() == "Wii")
+        spec.target_platform = XTConvert::TargetPlatform::TPL;
+    else if(ui->target_platform->currentText() == "DSi")
+        spec.target_platform = XTConvert::TargetPlatform::DSG;
+
+    // need to add to UI, and update default filenames...
+    spec.legacy_archives = true;
+
+    // need to add to UI...
+    spec.base_assets_path.clear();
+
+    emit do_run(spec);
 }
 
-void XTConvertUI::stop(bool do_abort)
+void XTConvertUI::on_finish()
 {
-    if(process_thread.isRunning())
-        process_thread.cancel();
-
     m_in_progress = false;
 
     ui->progress->setValue(0);
@@ -96,20 +213,33 @@ void XTConvertUI::stop(bool do_abort)
     ui->progress->setFormat("");
 
     updateControls();
+}
 
-    if(do_abort)
+void XTConvertUI::on_status_update(XTConvertUpdate update)
+{
+    if(update.log_category == XTConvert::LogCategory::ErrorMessage)
     {
-        // if(converter.state() == QProcess::Running)
-        //     converter.terminate();
-
-        // QMessageBox::warning(this, tr("SoX error"), tr("Operation cancaled"));
+        QMessageBox::warning(this, "XTConvert", update.file_name);
+        return;
     }
+
+    if(update.log_category == XTConvert::LogCategory::Category_Count)
+    {
+        // update progress bars
+        return;
+    }
+
+    // update file table
+    ui->textBrowser_3->append("\n");
+    ui->textBrowser_3->append(XTConvert::log_category[(int)update.log_category]);
+    ui->textBrowser_3->append(" - ");
+    ui->textBrowser_3->append(update.file_name);
 }
 
 void XTConvertUI::on_start_clicked()
 {
     if(m_in_progress)
-        stop(true);
+        emit do_cancel();
     else
         start();
 }
