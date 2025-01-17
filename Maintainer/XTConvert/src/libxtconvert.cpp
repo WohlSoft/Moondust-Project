3213dbfafdf100894cfa34f80dc1c20f7f2ab68a
ds-sloth <72112344+ds-sloth@users.noreply.github.com>
2025-01-16 22:38:56 -0500

    libxtconvert.cpp: refine filetypes again

--- Maintainer/XTConvert/src/libxtconvert.cpp
+++ Maintainer/XTConvert/src/libxtconvert.cpp
@@ -1312,7 +1312,8 @@ public:
             || filename.endsWith(".dll") || filename.endsWith(".exe")
             || filename.endsWith(".rar") || filename.endsWith(".zip") || filename.endsWith(".7z")
             || filename.endsWith(".xte") || filename.endsWith(".xta")
-            || filename.endsWith(".odt") || filename.endsWith(".pdf") || filename.endsWith(".md"))
+            || filename.endsWith(".odt") || filename.endsWith(".pdf") || filename.endsWith(".md") || filename.endsWith(".rtf")
+            || filename.endsWith(".tmp"))
         {
             // banned filenames
             log_file(LogCategory::SkippedUnused, in_path);
