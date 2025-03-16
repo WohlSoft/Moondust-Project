26a4be99714a33fc3e17323b1fb79fb717518e25
Wohlstand <admin@wohlnet.ru>
2025-03-16 04:35:38 +0300

    Make submodule branch script smarter
    Just parse .submodules file to obtain the true branch name rather than rely on mythical data from the repo itself that often causes weird behaviour because of heads instead of branches.

--- _common/submodule-update.sh
+++ _common/submodule-update.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
 
-reponame=$(basename `git rev-parse --show-toplevel`)
-dstbranch=$(git symbolic-ref refs/remotes/origin/HEAD --short | sed -n 's/origin\///p')
+projroot=$(dirname "$1")
+projroot_len=$((${#projroot}+1))
+cur_dir=$(pwd)
+repo_path=${cur_dir:$projroot_len}
+
 echo "-----------------------------------"
-echo "Updating repo $reponame, branch $dstbranch"
+# echo "Debug: projroot=$projroot"
+# echo "Debug: projroot_len=$projroot_len"
+# echo "Debug: cur_dir=$cur_dir"
+# echo "Debug: repo_path=$repo_path"
+
+dstbranch=""
+found=false
+
+while IFS= read -r line; do
+    line_clear=$(echo "$line" | sed 's/^[ \t]*//g')
+
+    if $found ; then
+        if [[ "$line_clear" == "branch = "* ]]; then
+            dstbranch=$(echo "$line_clear" | sed 's/^branch = *//g')
+            echo "Found branch name: $dstbranch"
+            break
+        fi
+    elif [[ "$line_clear" == "path = $repo_path" ]]; then
+        found=true
+    fi
+done < "$1"
+
+reponame=$(basename `git rev-parse --show-toplevel`)
+echo "Updating repo $reponame (path=$repo_path), branch $dstbranch"
 
 git checkout $dstbranch
 git pull origin $dstbranch
