ce25de3bdde8619d693c0592b42507eb30b967df
Wohlstand <admin@wohlnet.ru>
2025-03-16 03:25:28 +0300

    Simplify submodules handling script
    No more manual branch recognition!

--- _common/submodule-update.sh
+++ _common/submodule-update.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
 
 reponame=$(basename `git rev-parse --show-toplevel`)
+dstbranch=$(git branch | sed -n 's/^\* //p')
 echo "-----------------------------------"
-echo "Updating repo $reponame"
-if [[ "$reponame" == "LuaJIT" ]]; then
-    echo "Branch v2.1"
-    git checkout v2.1
-    git pull origin v2.1
-elif [[ "$reponame" == "libpng" ]]; then
-    echo "Branch moondust"
-    git checkout moondust
-    git pull origin moondust
-elif [[ "$reponame" == "harfbuzz" || "$reponame" == "libjpeg" ]]; then
-    echo "Branch main"
-    git checkout main
-    git pull origin main
-else
-    echo "Branch master"
-    git checkout master
-    git pull origin master
-fi
+echo "Updating repo $reponame, branch $dstbranch"
+
+git checkout $dstbranch
+git pull origin $dstbranch
