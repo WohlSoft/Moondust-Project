7f61e338cd347b3ef81d61118ef67abfeaebb8f1
Wohlstand <admin@wohlnet.ru>
2025-07-09 05:03:58 +0300

    Translations refresh

--- PlayableCalibrator/languages/calibrator_zh.ts
+++ PlayableCalibrator/languages/calibrator_zh.ts
@@ -627,17 +627,17 @@ Right button - Move the right-bottom crop edges.</source>
         <translation type="vanished">已保存</translation>
     </message>
     <message>
-        <location filename="../main/calibration_file.cpp" line="349"/>
+        <location filename="../main/calibration_file.cpp" line="358"/>
         <source>Configuration saved in file</source>
         <translation>设置已保存到文件</translation>
     </message>
     <message>
-        <location filename="../main/calibration_file.cpp" line="676"/>
+        <location filename="../main/calibration_file.cpp" line="685"/>
         <source>Bad data generated</source>
         <translation>数据生成错误</translation>
     </message>
     <message>
-        <location filename="../main/calibration_file.cpp" line="677"/>
+        <location filename="../main/calibration_file.cpp" line="686"/>
         <source>Some generated values are out of range, SMBX-38A may crash. File will not be patched.</source>
         <translation>某些生成的值超出范围，可能造成 SMBX-38A 异常，操作已中断。</translation>
     </message>
@@ -1150,6 +1150,55 @@ Right button - Move the right-bottom crop edges.</source>
         <source>Reset calibration</source>
         <translation>校正重置</translation>
     </message>
+    <message>
+        <location filename="../image_calibration/image_calibrator.cpp" line="110"/>
+        <source>Mirror SMBX sprite...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../image_calibration/image_calibrator.cpp" line="380"/>
+        <location filename="../image_calibration/image_calibrator.cpp" line="440"/>
+        <source>Incompatible sprite</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../image_calibration/image_calibrator.cpp" line="381"/>
+        <location filename="../image_calibration/image_calibrator.cpp" line="441"/>
+        <source>Can&apos;t perform the SMBX sprite mirror to incompatible sprite (the width and/or height of matrix are not equal to 10 and sprite is not equal to 1000x1000).</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../image_calibration/image_calibrator.cpp" line="388"/>
+        <source>Mirror frames on the SMBX sprite...</source>
+        <comment>Mirror SMBX frames</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../image_calibration/image_calibrator.cpp" line="391"/>
+        <source>Select the way how frames will be mirrored:</source>
+        <comment>Mirror SMBX frames</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../image_calibration/image_calibrator.cpp" line="394"/>
+        <source>Copy direction</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../image_calibration/image_calibrator.cpp" line="399"/>
+        <source>Left to right</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../image_calibration/image_calibrator.cpp" line="401"/>
+        <source>Right to left</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../image_calibration/image_calibrator.cpp" line="407"/>
+        <source>Confirm</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>Matrix</name>
@@ -1171,25 +1220,25 @@ To choose a frame for edit, push to the button over a necessary frame.</source>
 要选择编辑的帧，请在需要的帧按下按钮。</translation>
     </message>
     <message>
-        <location filename="../frame_matrix/matrix.cpp" line="127"/>
+        <location filename="../frame_matrix/matrix.cpp" line="129"/>
         <source>Click a frame by the left mouse button to choose it. Use the right mouse button to set or unset check on a frame. The settings of each unchecked frame will not be saved into the calibration file.</source>
         <comment>Frame selector&apos;s label, normal version</comment>
         <translation>单击以选择帧，右击以选择或取消选择帧。未选择的帧不会保存到人物校正文件。</translation>
     </message>
     <message>
-        <location filename="../frame_matrix/matrix.cpp" line="134"/>
+        <location filename="../frame_matrix/matrix.cpp" line="136"/>
         <source>Click a frame by the left mouse button to choose it. Use the right mouse button to set or unset check on a frame. The settings of each unchecked frame will not be saved into the calibration file. Use the middle mouse button to pick up the frame as a reference on the scene.</source>
         <comment>Frame selector&apos;s label, sprite editor version</comment>
         <translation>单击以选择帧，右击以选择或取消选择帧。未选择的帧不会保存到人物校正文件。点击鼠标中键以在画布中显示用于对照的帧。</translation>
     </message>
     <message>
-        <location filename="../frame_matrix/matrix.cpp" line="142"/>
+        <location filename="../frame_matrix/matrix.cpp" line="144"/>
         <source>Click a frame by the left mouse button to add it to the list.</source>
         <comment>Frame selector&apos;s label, animation editor, add action</comment>
         <translation>单击以添加帧到列表。</translation>
     </message>
     <message>
-        <location filename="../frame_matrix/matrix.cpp" line="147"/>
+        <location filename="../frame_matrix/matrix.cpp" line="149"/>
         <source>Click a frame by the left mouse button to take it to the list instead of selected.</source>
         <comment>Frame selector&apos;s label, animation editor, change action</comment>
         <translation>单击以设置所选的帧。</translation>
