5ec697d8ebbd9cd87cc2f28dad50ad1842dec1b5
Wohlstand <admin@wohlnet.ru>
2026-01-13 01:49:13 +0300

    Editor: Adjusted music warning messages

--- Editor/languages/editor_en.ts
+++ Editor/languages/editor_en.ts
@@ -4655,19 +4655,27 @@ Physical env. zones:	 %6
 &lt;br/&gt;&lt;br/&gt;
 - Learn how to make looping music of different formats: &lt;a href=&quot;%2&quot;&gt;%2&lt;/a&gt;&lt;br/&gt;- List of supported music formats and other documentation can be found here: &lt;a href=&quot;%3&quot;&gt;%3&lt;/a&gt;</source>
         <comment>Custom music notice dialogue message</comment>
-        <translation>Music file size of &quot;%1&quot; seems too large. Please don&apos;t use so called &quot;extended versions&quot; of the game music and don&apos;t use uncompressed formats such as WAV or AIFF, otherwise you making your levels and episode unfairly bloated. Please don&apos;t waste disk space that might be limited at end users. Please convert your music into FLAC or OGG Vorbis, and consider to use loop tags to set the song area that will loop infinitely. Or even better, consider to compose or find the chiptune version in the original format such as NSF, VGM, KSM, HES, GBS, etc. or use the MIDI file with one of built-in MIDI synthesisers. You can play it in chiptune style using libADLMIDI, libOPNMIDI or libEDMIDI, or play it using SoundFont wavetable banks with the FluidSynth synthesiser as you would like.
+        <translation>Music file size of &quot;%1&quot; seems too large. Please &lt;u&gt;don&apos;t use so called &quot;extended versions&quot;&lt;/u&gt; of the game music and &lt;u&gt;don&apos;t use uncompressed formats such as WAV or AIFF&lt;/u&gt;, otherwise you making your levels and episode unfairly bloated. Please don&apos;t waste disk space that might be limited at end users.
 &lt;br/&gt;&lt;br/&gt;
-- Learn how to make looping music of different formats: &lt;a href=&quot;%2&quot;&gt;%2&lt;/a&gt;&lt;br/&gt;
-- List of supported music formats and other documentation can be found here: &lt;a href=&quot;%3&quot;&gt;%3&lt;/a&gt;</translation>
+Please convert your music into &lt;b&gt;FLAC&lt;/b&gt; or &lt;b&gt;OGG Vorbis&lt;/b&gt;, and consider to use loop tags to set the song area that will loop infinitely. Or even better, consider to compose or find the chiptune version in the original format such as NSF, VGM, KSM, HES, GBS, etc. or use the MIDI file with one of built-in MIDI synthesisers. You can play it in chiptune style using libADLMIDI, libOPNMIDI or libEDMIDI, or play it using SoundFont wavetable banks with the FluidSynth synthesiser as you would like.
+&lt;ul&gt;
+&lt;li&gt;Learn how to make looping music of different formats: &lt;a href=&quot;%2&quot;&gt;%2&lt;/a&gt;&lt;/li&gt;
+&lt;li&gt;List of supported music formats and other documentation can be found here: &lt;a href=&quot;%3&quot;&gt;%3&lt;/a&gt;&lt;/li&gt;
+&lt;/ul&gt;</translation>
     </message>
     <message>
         <source>Your music file &quot;%1&quot; seems uses one of deprecated formats. These formats often result bad sounding quality, bloated package sizes, or lack of several features like looping tags. Please consider to use the music of the different format. For digital audio the best suggested format is the &lt;b&gt;OGG Vorbis&lt;/b&gt; which allows much better quality on strong compression levels and allows using looping tags to set the part of the song that will loop infinitely. Additionally, you can use chiptune (NSF, HES, VGM, HES, GBS, etc.) or tracker formats (MOD, IT, XM, S3M, etc.) natively. Additionally, you can use MIDI files with the customised sounding using chiptune style with libADLMIDI, libOPNMIDI, or libEDMIDI synthesisers, or with the wavetable style using SoundFont banks with the FluidSynth synthesiser.&lt;br/&gt;&lt;br/&gt;
 - Learn how to make looping music of different formats: &lt;a href=&quot;%2&quot;&gt;%2&lt;/a&gt;&lt;br/&gt;- List of supported music formats and other documentation can be found here: &lt;a href=&quot;%3&quot;&gt;%3&lt;/a&gt;</source>
         <comment>Custom music notice dialogue message</comment>
-        <translation>Your music file &quot;%1&quot; seems uses one of deprecated formats. These formats often result bad sounding quality, bloated package sizes, or lack of several features like looping tags. Please consider to use the music of the different format. For digital audio the best suggested format is the &lt;b&gt;OGG Vorbis&lt;/b&gt; which allows much better quality on strong compression levels and allows using looping tags to set the part of the song that will loop infinitely. Additionally, you can use chiptune (NSF, HES, VGM, HES, GBS, etc.) or tracker formats (MOD, IT, XM, S3M, etc.) natively. Additionally, you can use MIDI files with the customised sounding using chiptune style with libADLMIDI, libOPNMIDI, or libEDMIDI synthesisers, or with the wavetable style using SoundFont banks with the FluidSynth synthesiser.
+        <translation>Your music file &quot;%1&quot; seems uses one of &lt;b&gt;deprecated formats&lt;/b&gt;. These formats often result bad sounding quality, bloated package sizes, or lack of several features like &lt;u&gt;looping tags&lt;/u&gt;.
 &lt;br/&gt;&lt;br/&gt;
-- Learn how to make looping music of different formats: &lt;a href=&quot;%2&quot;&gt;%2&lt;/a&gt;&lt;br/&gt;
-- List of supported music formats and other documentation can be found here: &lt;a href=&quot;%3&quot;&gt;%3&lt;/a&gt;</translation>
+Please consider to use the music of the different format. For digital audio the best suggested format is the &lt;b&gt;OGG Vorbis&lt;/b&gt; which allows much better quality on strong compression levels and allows using looping tags to set the part of the song that will loop infinitely. 
+&lt;br/&gt;&lt;br/&gt;
+Additionally, you can use &lt;b&gt;chiptune&lt;/b&gt; (NSF, HES, VGM, HES, GBS, etc.) or &lt;b&gt;tracker&lt;/b&gt; formats (MOD, IT, XM, S3M, etc.) natively. Additionally, you can use &lt;b&gt;MIDI files&lt;/b&gt; with the customised sounding using chiptune style with libADLMIDI, libOPNMIDI, or libEDMIDI synthesisers, or with the wavetable style using SoundFont banks with the FluidSynth synthesiser.
+&lt;ul&gt;
+&lt;li&gt;Learn how to make looping music of different formats: &lt;a href=&quot;%2&quot;&gt;%2&lt;/a&gt;&lt;/li&gt;
+&lt;li&gt;List of supported music formats and other documentation can be found here: &lt;a href=&quot;%3&quot;&gt;%3&lt;/a&gt;&lt;/li&gt;
+&lt;/ul&gt;</translation>
     </message>
     <message>
         <source>Your music file &quot;%1&quot; seems incompatible to the original SMBX Engine. You see this warning since you edit your level file of the legacy format. If you don&apos;t target to the original engine, you can ignore this warning. Original SMBX Engine supports only &lt;b&gt;MP3&lt;/b&gt;, &lt;b&gt;WAV&lt;/b&gt;, &lt;b&gt;WMA&lt;/b&gt;, and &lt;b&gt;MID&lt;/b&gt; without arguments string.</source>
