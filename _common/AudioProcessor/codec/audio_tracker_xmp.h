39f08b4d434ca49e36dc7b61765848056a6afc91
Wohlstand <admin@wohlnet.ru>
2026-04-06 00:55:03 +0300

    AuidioProcessor: Added support for tracker music handling using libxmp libModPlug is not yet supported as a fallback for the supported formats.

---
+++ _common/AudioProcessor/codec/audio_tracker_xmp.h
@@ -0,0 +1,50 @@
+/*
+ * Moondust, a free game engine for platform game making
+ * Copyright (c) 2014-2025 Vitaly Novichkov <admin@wohlnet.ru>
+ *
+ * This software is licensed under a dual license system (MIT or GPL version 3 or later).
+ * This means you are free to choose with which of both licenses (MIT or GPL version 3 or later)
+ * you want to use this software.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You can see text of MIT license in the LICENSE.mit file you can see in Engine folder,
+ * or see https://mit-license.org/.
+ *
+ * You can see text of GPLv3 license in the LICENSE.gpl3 file you can see in Engine folder,
+ * or see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+#ifndef AUDIO_TRACKER_XMP_H
+#define AUDIO_TRACKER_XMP_H
+
+#include "../audio_file.h"
+
+struct MDAudioXMPPriv;
+
+class MDAudioXMP : public MDAudioFile
+{
+    MDAudioXMPPriv *m_p;
+    int init_level = 0;
+
+public:
+    MDAudioXMP();
+    virtual ~MDAudioXMP();
+
+    uint32_t getCodecSpec() const override;
+
+    bool openRead(SDL_RWops *file) override;
+    bool openWrite(SDL_RWops *file, const MDAudioFileSpec &dstSpec) override;
+
+    bool close() override;
+
+    bool readRewind() override;
+
+    size_t readChunk(uint8_t *out, size_t outSize, bool *spec_changed = nullptr) override;
+    size_t writeChunk(uint8_t *in, size_t inSize) override;
+};
+
+#endif // AUDIO_TRACKER_XMP_H
